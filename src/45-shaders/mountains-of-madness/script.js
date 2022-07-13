0d474ea7e4a57b3a33485503d179025dd0751d71
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-14 01:02:03 +0200

    m

--- src/45-shaders/mountains-of-madness/script.js
+++ src/45-shaders/mountains-of-madness/script.js
@@ -1,20 +1,12 @@
-
-let container
-let camera, scene, renderer
-
-container = document.getElementById('container')
-
-camera = new THREE.Camera()
-camera.position.z = 1
-
-scene = new THREE.Scene()
+import * as THREE from '/node_modules/three127/build/three.module.js'
+import { camera, scene, renderer } from '/utils/scene.js'
 
 const geometry = new THREE.PlaneBufferGeometry(2, 2)
 
 const uniforms = {
   u_time: { type: 'f', value: 1.0 },
   u_resolution: { type: 'v2', value: new THREE.Vector2() },
-  u_mouse: { type: 'v2', value: new THREE.Vector2() } }
+}
 
 const material = new THREE.ShaderMaterial({
   uniforms,
@@ -26,21 +18,10 @@ material.extensions.derivatives = true
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-renderer = new THREE.WebGLRenderer()
-container.appendChild(renderer.domElement)
-
 onWindowResize()
 window.addEventListener('resize', onWindowResize, false)
 
-document.addEventListener('pointermove', e => {
-  const ratio = window.innerHeight / window.innerWidth
-  uniforms.u_mouse.value.x = (e.pageX - window.innerWidth / 2) / window.innerWidth / ratio
-  uniforms.u_mouse.value.y = (e.pageY - window.innerHeight / 2) / window.innerHeight * -1
-
-  e.preventDefault()
-})
-
-function onWindowResize(event) {
+function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight)
   uniforms.u_resolution.value.x = renderer.domElement.width
   uniforms.u_resolution.value.y = renderer.domElement.height
