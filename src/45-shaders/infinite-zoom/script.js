4ace8d6f56837bb7f6458b1a81ad140f7731c61d
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-14 00:59:43 +0200

    pomalo

--- src/45-shaders/infinite-zoom/script.js
+++ src/45-shaders/infinite-zoom/script.js
@@ -1,68 +1,33 @@
 // https://codepen.io/shubniggurath/pen/JMzQRw
-/*
-Most of the stuff in here is just bootstrapping. Essentially it's just
-setting ThreeJS up so that it renders a flat surface upon which to draw
-the shader. The only thing to see here really is the uniforms sent to
-the shader. Apart from that all of the magic happens in the HTML view
-under the fragment shader.
-*/
+import * as THREE from '/node_modules/three127/build/three.module.js'
+import { camera, scene, renderer } from '/utils/scene.js'
 
-let container
-let camera, scene, renderer
-let uniforms
+const geometry = new THREE.PlaneBufferGeometry(2, 2)
 
-function init() {
-  container = document.getElementById('container')
-
-  camera = new THREE.Camera()
-  camera.position.z = 1
-
-  scene = new THREE.Scene()
-
-  const geometry = new THREE.PlaneBufferGeometry(2, 2)
-
-  uniforms = {
-    u_time: { type: 'f', value: 1.0 },
-    u_resolution: { type: 'v2', value: new THREE.Vector2() },
-    u_mouse: { type: 'v2', value: new THREE.Vector2() } }
-
-  const material = new THREE.ShaderMaterial({
-    uniforms,
-    vertexShader: document.getElementById('vertexShader').textContent,
-    fragmentShader: document.getElementById('fragmentShader').textContent })
-
-  const mesh = new THREE.Mesh(geometry, material)
-  scene.add(mesh)
-
-  renderer = new THREE.WebGLRenderer()
-  renderer.setPixelRatio(window.devicePixelRatio)
-
-  container.appendChild(renderer.domElement)
-
-  onWindowResize()
-  window.addEventListener('resize', onWindowResize, false)
-
-  document.onmousemove = function(e) {
-    uniforms.u_mouse.value.x = e.pageX
-    uniforms.u_mouse.value.y = e.pageY
-  }
+const uniforms = {
+  u_time: { type: 'f', value: 1.0 },
+  u_resolution: { type: 'v2', value: new THREE.Vector2() },
 }
 
-function onWindowResize(event) {
-  renderer.setSize(window.innerWidth, window.innerHeight)
+const material = new THREE.ShaderMaterial({
+  uniforms,
+  vertexShader: document.getElementById('vertexShader').textContent,
+  fragmentShader: document.getElementById('fragmentShader').textContent })
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+onWindowResize()
+
+function onWindowResize() {
   uniforms.u_resolution.value.x = renderer.domElement.width
   uniforms.u_resolution.value.y = renderer.domElement.height
 }
 
-function animate() {
+void function animate() {
   requestAnimationFrame(animate)
-  render()
-}
-
-function render() {
   uniforms.u_time.value += 0.05
   renderer.render(scene, camera)
-}
+}()
 
-init()
-animate()
+window.addEventListener('resize', onWindowResize, false)
