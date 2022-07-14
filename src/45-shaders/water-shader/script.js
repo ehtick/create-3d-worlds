0781821fbab50e0d35488e10f877c6fa0e640329
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-14 10:55:54 +0200

    primer

--- src/45-shaders/water-shader/script.js
+++ src/45-shaders/water-shader/script.js
@@ -1,80 +1,103 @@
-https:// codepen.io/knoland/pen/XKxrJb
+import * as THREE from '/node_modules/three127/build/three.module.js'
+import { OrbitControls } from '/node_modules/three127/examples/jsm/controls/OrbitControls.js'
+import { Water } from '/node_modules/three127/examples/jsm/objects/Water.js'
+import { Sky } from '/node_modules/three127/examples/jsm/objects/Sky.js'
 
-var container,
-  renderer,
-  scene,
-  camera,
-  mesh,
-  start = Date.now(),
-  fov = 30
+let container
+let camera, scene, renderer
+let controls, water, sun, mesh
 
-const clock = new THREE.Clock()
+init()
+animate()
 
-const timeUniform = {
-  iGlobalTime: {
-    type: 'f',
-    value: 0.1
-  },
-  iResolution: {
-    type: 'v2',
-    value: new THREE.Vector2()
-  }
-}
+function init() {
 
-timeUniform.iResolution.value.x = window.innerWidth
-timeUniform.iResolution.value.y = window.innerHeight
-
-window.addEventListener('load', () => {
   container = document.getElementById('container')
-  scene = new THREE.Scene()
-
-  camera = new THREE.PerspectiveCamera(
-    fov,
-    window.innerWidth / window.innerHeight,
-    1,
-    10000
-  )
-  camera.position.x = 20
-  camera.position.y = 10
-  camera.position.z = 20
-  camera.lookAt(scene.position)
-  scene.add(camera)
-
-  const axis = new THREE.AxisHelper(10)
-  scene.add (axis)
-
-  material = new THREE.ShaderMaterial({
-    uniforms: timeUniform,
-    vertexShader: document.getElementById('vertex-shader').textContent,
-    fragmentShader: document.getElementById('fragment-shader').textContent
-  })
-
-  const water = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(window.innerWidth, window.innerHeight, 40), material
-  )
-  scene.add(water)
-
-  const geometry = new THREE.SphereGeometry(10, 32, 32)
-  var material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
-  const sphere = new THREE.Mesh(geometry, material)
-  scene.add(sphere)
 
   renderer = new THREE.WebGLRenderer()
+  renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
-
+  renderer.toneMapping = THREE.ACESFilmicToneMapping
   container.appendChild(renderer.domElement)
 
-  render()
-})
+  scene = new THREE.Scene()
 
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-})
+  camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000)
+  camera.position.set(30, 30, 100)
 
-function render() {
-  timeUniform.iGlobalTime.value += clock.getDelta()
+  sun = new THREE.Vector3()
+
+  const waterGeometry = new THREE.PlaneGeometry(10000, 10000)
+  water = new Water(
+    waterGeometry,
+    {
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals: new THREE.TextureLoader().load('textures/waternormals.jpg', texture => {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+      }),
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffffff,
+      waterColor: 0x001e0f,
+      distortionScale: 3.7,
+      fog: scene.fog !== undefined
+    }
+  )
+
+  water.rotation.x = - Math.PI / 2
+  scene.add(water)
+
+  const sky = new Sky()
+  sky.scale.setScalar(10000)
+  scene.add(sky)
+
+  const skyUniforms = sky.material.uniforms
+
+  skyUniforms.turbidity.value = 10
+  skyUniforms.rayleigh.value = 2
+  skyUniforms.mieCoefficient.value = 0.005
+  skyUniforms.mieDirectionalG.value = 0.8
+
+  const parameters = {
+    elevation: 2,
+    azimuth: 180
+  }
+
+  function updateSun() {
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation)
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth)
+
+    sun.setFromSphericalCoords(1, phi, theta)
+
+    sky.material.uniforms.sunPosition.value.copy(sun)
+    water.material.uniforms.sunDirection.value.copy(sun).normalize()
+  }
+
+  updateSun()
+
+  const geometry = new THREE.BoxGeometry(30, 30, 30)
+  const material = new THREE.MeshStandardMaterial({ roughness: 0 })
+
+  mesh = new THREE.Mesh(geometry, material)
+  scene.add(mesh)
+
+  controls = new OrbitControls(camera, renderer.domElement)
+  controls.maxPolarAngle = Math.PI * 0.495
+  controls.target.set(0, 10, 0)
+  controls.minDistance = 40.0
+  controls.maxDistance = 200.0
+  controls.update()
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+  const time = performance.now() * 0.001
+
+  mesh.position.y = Math.sin(time) * 20 + 5
+  mesh.rotation.x = time * 0.5
+  mesh.rotation.z = time * 0.51
+
+  water.material.uniforms.time.value += 1.0 / 60.0
+
   renderer.render(scene, camera)
-  requestAnimationFrame(render)
 }
