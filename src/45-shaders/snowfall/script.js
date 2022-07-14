2f382c742904a149c6b47e84a3da79b8eb857097
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-14 10:36:42 +0200

    m

--- src/45-shaders/snowfall/script.js
+++ src/45-shaders/snowfall/script.js
@@ -1,65 +1,35 @@
-let container
-let camera, scene, renderer
-let uniforms
+import * as THREE from '/node_modules/three127/build/three.module.js'
+import { camera, scene, renderer } from '/utils/scene.js'
 
 const loader = new THREE.TextureLoader()
-let texture
-loader.setCrossOrigin('anonymous')
-loader.load('noise.png', tex => {
-  texture = tex
-  texture.wrapS = THREE.RepeatWrapping
-  texture.wrapT = THREE.RepeatWrapping
-  texture.minFilter = THREE.LinearFilter
-  init()
-  animate()
-})
 
-function init() {
-  container = document.getElementById('container')
+const texture = await loader.loadAsync('noise.png')
+texture.wrapS = THREE.RepeatWrapping
+texture.wrapT = THREE.RepeatWrapping
 
-  camera = new THREE.Camera()
-  camera.position.z = 1
+const geometry = new THREE.PlaneBufferGeometry(2, 2)
 
-  scene = new THREE.Scene()
-
-  const geometry = new THREE.PlaneBufferGeometry(2, 2)
-
-  uniforms = {
-    u_time: { type: 'f', value: 1.0 },
-    u_resolution: { type: 'v2', value: new THREE.Vector2() },
-    u_noise: { type: 't', value: texture },
-  }
-
-  const material = new THREE.ShaderMaterial({
-    uniforms,
-    vertexShader: document.getElementById('vertexShader').textContent,
-    fragmentShader: document.getElementById('fragmentShader').textContent })
-
-  material.extensions.derivatives = true
-
-  const mesh = new THREE.Mesh(geometry, material)
-  scene.add(mesh)
-
-  renderer = new THREE.WebGLRenderer()
-  renderer.setPixelRatio(window.devicePixelRatio)
-
-  container.appendChild(renderer.domElement)
-
-  onWindowResize()
+const uniforms = {
+  u_time: { type: 'f', value: 1.0 },
+  u_resolution: { type: 'v2', value: new THREE.Vector2() },
+  u_noise: { type: 't', value: texture },
 }
 
-function onWindowResize(event) {
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  uniforms.u_resolution.value.x = renderer.domElement.width
-  uniforms.u_resolution.value.y = renderer.domElement.height
-}
+uniforms.u_resolution.value.x = renderer.domElement.width
+uniforms.u_resolution.value.y = renderer.domElement.height
 
-function animate(delta) {
+const material = new THREE.ShaderMaterial({
+  uniforms,
+  vertexShader: document.getElementById('vertexShader').textContent,
+  fragmentShader: document.getElementById('fragmentShader').textContent })
+
+material.extensions.derivatives = true
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+void function animate(delta) {
   requestAnimationFrame(animate)
-  render(delta)
-}
-
-function render(delta) {
   uniforms.u_time.value = -10000 + delta * 0.0005
   renderer.render(scene, camera)
-}
+}()
--- src/45-shaders/textured-blocks/main.js
+++ src/45-shaders/textured-blocks/main.js
@@ -1,85 +1,42 @@
 // https://threejs.org/examples/webgl_shader2.html
 import * as THREE from '/node_modules/three127/build/three.module.js'
+import { camera, scene, renderer, clock } from '/utils/scene.js'
 
-let camera, scene, renderer, clock
+camera.position.set(0, 0, 2)
 
-let uniforms1, uniforms2
+const geometry = new THREE.BoxGeometry(0.75, 0.75, 0.75)
 
-init()
-animate()
-
-function init() {
-  const container = document.getElementById('container')
-
-  camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 3000)
-  camera.position.z = 4
-
-  scene = new THREE.Scene()
-
-  clock = new THREE.Clock()
-
-  const geometry = new THREE.BoxGeometry(0.75, 0.75, 0.75)
-
-  uniforms1 = {
-    'time': { value: 1.0 }
-  }
-
-  uniforms2 = {
-    'time': { value: 1.0 },
-    'colorTexture': { value: new THREE.TextureLoader().load('textures/disturb.jpg') }
-  }
-
-  uniforms2.colorTexture.value.wrapS = uniforms2.colorTexture.value.wrapT = THREE.RepeatWrapping
-
-  const params = [
-    ['fragment_shader1', uniforms1],
-    ['fragment_shader2', uniforms2],
-    ['fragment_shader3', uniforms1],
-    ['fragment_shader4', uniforms1]
-  ]
-
-  for (let i = 0; i < params.length; i ++) {
-
-    const material = new THREE.ShaderMaterial({
-
-      uniforms: params[i][1],
-      vertexShader: document.getElementById('vertexShader').textContent,
-      fragmentShader: document.getElementById(params[i][0]).textContent
-
-    })
-
-    const mesh = new THREE.Mesh(geometry, material)
-    mesh.position.x = i - (params.length - 1) / 2
-    mesh.position.y = i % 2 - 0.5
-    scene.add(mesh)
-
-  }
-
-  renderer = new THREE.WebGLRenderer()
-  renderer.setPixelRatio(window.devicePixelRatio)
-  container.appendChild(renderer.domElement)
-
-  onWindowResize()
-
-  window.addEventListener('resize', onWindowResize)
-
+const uniforms1 = {
+  'time': { value: 1.0 }
 }
 
-function onWindowResize() {
-
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-
-  renderer.setSize(window.innerWidth, window.innerHeight)
-
+const uniforms2 = {
+  'time': { value: 1.0 },
+  'colorTexture': { value: new THREE.TextureLoader().load('textures/disturb.jpg') }
 }
 
-function animate() {
+uniforms2.colorTexture.value.wrapS = uniforms2.colorTexture.value.wrapT = THREE.RepeatWrapping
+
+const params = [
+  ['fragment_shader2', uniforms2],
+  ['fragment_shader3', uniforms1],
+]
+
+for (let i = 0; i < params.length; i ++) {
+  const material = new THREE.ShaderMaterial({
+    uniforms: params[i][1],
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById(params[i][0]).textContent
+  })
+
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.position.x = i - (params.length - 1) / 2
+  mesh.position.y = i % 2 - 0.5
+  scene.add(mesh)
+}
+
+void function animate() {
   requestAnimationFrame(animate)
-  render()
-}
-
-function render() {
   const delta = clock.getDelta()
 
   uniforms1.time.value += delta * 5
@@ -91,4 +48,4 @@ function render() {
     object.rotation.x += delta * 0.5 * (i % 2 ? - 1 : 1)
   }
   renderer.render(scene, camera)
-}
+}()
