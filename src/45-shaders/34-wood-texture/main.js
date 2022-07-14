4949bcaec5a96b6f08804745d33aef00ecab5eb7
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-14 21:38:54 +0200

    org

--- src/45-shaders/34-wood-texture/main.js
+++ src/45-shaders/34-wood-texture/main.js
@@ -1,37 +1,5 @@
-const vertexShader = /* glsl */`
-  varying vec3 vPosition;
-
-  void main() {
-    vPosition = position;
-
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-  }
-`
-
-const fragmentShader = /* glsl */`
-  uniform vec3 u_LightColor;
-  uniform vec3 u_DarkColor;
-  uniform float u_Frequency;
-  uniform float u_NoiseScale;
-  uniform float u_RingScale;
-  uniform float u_Contrast;
-
-  varying vec3 vPosition;
-
-  #include <noise>
-
-  void main(){
-    float n = snoise( vPosition );
-    float ring = fract( u_Frequency * vPosition.z + u_NoiseScale * n );
-    ring *= u_Contrast * ( 1.0 - ring );
-
-    // Adjust ring smoothness and shape, and add some noise
-    float lerp = pow( ring, u_RingScale ) + n;
-    vec3 color = mix(u_DarkColor, u_LightColor, lerp);
-
-    gl_FragColor = vec4(color, 1.0);
-  }
-`
+/* global THREE */
+import { vertexShader, fragmentShader } from './shader.js'
 
 const scene = new THREE.Scene()
 const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
