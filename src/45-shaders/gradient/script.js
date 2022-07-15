bfbda21c67867f7be974891cf7f5bdc1512d7e6e
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-16 00:51:15 +0200

    new examples

---
+++ src/45-shaders/gradient/script.js
@@ -0,0 +1,14 @@
+import * as THREE from '/node_modules/three127/build/three.module.js'
+import { camera, scene, renderer } from '/utils/scene.js'
+import { material } from './shader.js'
+
+const geometry = new THREE.PlaneGeometry(10, 10)
+const plane = new THREE.Mesh(geometry, material)
+scene.add(plane)
+
+/* LOOP */
+
+void function render() {
+  requestAnimationFrame(render)
+  renderer.render(scene, camera)
+}()
---
+++ src/45-shaders/gradient/shader.js
@@ -0,0 +1,15 @@
+import * as THREE from '/node_modules/three127/build/three.module.js'
+
+const fragmentShader = /* glsl */`
+  uniform vec2 resolution; // Uniform variables must be declared here first
+
+  void main() {
+    vec2 pos = gl_FragCoord.xy / resolution.xy; // normalize coordinates to -1 to 1
+    gl_FragColor = vec4(1.0, pos.x, pos.y, 1.0);   // create gradient
+  }
+`
+
+const uniforms = {}
+uniforms.resolution = { type: 'v2', value: new THREE.Vector2(window.innerWidth, window.innerHeight) }
+
+export const material = new THREE.ShaderMaterial({ uniforms, fragmentShader })
