ac601c5ba2f89479ea1996fbc7abb74bd6a5266b
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-18 17:14:18 +0200

    m

--- src/90-examples/ProceduralTerrain_Part3/src/main.js
+++ src/90-examples/ProceduralTerrain_Part3/src/main.js
@@ -1,38 +1,23 @@
 import * as THREE from '/node_modules/three127/build/three.module.js'
 import { scene, camera, renderer, clock } from '/utils/scene.js'
 import { moveCamera } from '/utils/player.js'
-
+import { createSunLight } from '/utils/light.js'
 import { TerrainChunkManager } from './terrain.js'
 
-function initLights() {
-  let light = new THREE.DirectionalLight(0x808080, 1, 100)
-  light.position.set(-100, 100, -100)
-  light.target.position.set(0, 0, 0)
-  light.castShadow = false
-  scene.add(light)
-
-  light = new THREE.DirectionalLight(0x404040, 1.5, 100)
-  light.position.set(100, 100, -100)
-  light.target.position.set(0, 0, 0)
-  light.castShadow = false
-  scene.add(light)
-}
-
-initLights()
+scene.add(createSunLight())
 scene.background = new THREE.Color(0xbfd1e5)
+camera.position.y = 50
 
-camera.position.set(475, 75, 900)
-
-const entities = {}
-entities._terrain = new TerrainChunkManager(camera)
+const terrain = new TerrainChunkManager(camera)
 
 /* LOOP */
 
 void function loop() {
   requestAnimationFrame(loop)
   const delta = clock.getDelta()
+
   moveCamera(camera, delta, 100)
-  for (const k in entities)
-    entities[k].Update(delta)
+  terrain.Update(delta)
+
   renderer.render(scene, camera)
 }()
