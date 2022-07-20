374f3b7c0eb678b0d54c509e4f109c286c7b329c
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-20 19:04:20 +0200

    pomalo

--- src/90-examples/cannon-vozilo-teren/main.js
+++ src/90-examples/cannon-vozilo-teren/main.js
@@ -1,8 +1,9 @@
 /* global THREE, CANNON */
+// import * as THREE from '/node_modules/three127/build/three.module.js'
 import JoyStick from '/classes/JoyStick.js'
+import { createVisual } from './cannon-helpers.js'
 
 let scene, camera, renderer, world, vehicle, dt, light, lightOffset
-let particleGeo, particleMaterial, settings
 
 scene = new THREE.Scene()
 scene.background = new THREE.Color(0xaaaaff)
@@ -52,187 +53,6 @@ function onMove(forward, turn) {
   vehicle.setSteeringValue(steer, 1)
 }
 
-function shape2Mesh(body, castShadow, receiveShadow, material) {
-  const obj = new THREE.Object3D()
-  let index = 0
-
-  body.shapes.forEach (shape => {
-    let mesh
-    let geometry
-    let v0, v1, v2
-
-    switch (shape.type) {
-      case CANNON.Shape.types.SPHERE:
-        const sphere_geometry = new THREE.SphereGeometry(shape.radius, 8, 8)
-        mesh = new THREE.Mesh(sphere_geometry, material)
-        break
-
-      case CANNON.Shape.types.PARTICLE:
-        mesh = new THREE.Mesh(particleGeo, particleMaterial)
-        const s = settings
-        mesh.scale.set(s.particleSize, s.particleSize, s.particleSize)
-        break
-
-      case CANNON.Shape.types.PLANE:
-        geometry = new THREE.PlaneGeometry(10, 10, 4, 4)
-        mesh = new THREE.Object3D()
-        const submesh = new THREE.Object3D()
-        const ground = new THREE.Mesh(geometry, material)
-        ground.scale.set(100, 100, 100)
-        submesh.add(ground)
-
-        mesh.add(submesh)
-        break
-
-      case CANNON.Shape.types.BOX:
-        const box_geometry = new THREE.BoxGeometry(shape.halfExtents.x * 2,
-          shape.halfExtents.y * 2,
-          shape.halfExtents.z * 2)
-        mesh = new THREE.Mesh(box_geometry, material)
-        break
-
-      case CANNON.Shape.types.CONVEXPOLYHEDRON:
-        const geo = new THREE.Geometry()
-
-        // Add vertices
-        shape.vertices.forEach(v => {
-          geo.vertices.push(new THREE.Vector3(v.x, v.y, v.z))
-        })
-
-        shape.faces.forEach(face => {
-          // add triangles
-          const a = face[0]
-          for (let j = 1; j < face.length - 1; j++) {
-            const b = face[j]
-            const c = face[j + 1]
-            geo.faces.push(new THREE.Face3(a, b, c))
-          }
-        })
-        geo.computeBoundingSphere()
-        geo.computeFaceNormals()
-        mesh = new THREE.Mesh(geo, material)
-        break
-
-      case CANNON.Shape.types.HEIGHTFIELD:
-        geometry = new THREE.Geometry()
-
-        v0 = new CANNON.Vec3()
-        v1 = new CANNON.Vec3()
-        v2 = new CANNON.Vec3()
-        for (let xi = 0; xi < shape.data.length - 1; xi++)
-          for (let yi = 0; yi < shape.data[xi].length - 1; yi++)
-            for (let k = 0; k < 2; k++) {
-              shape.getConvexTrianglePillar(xi, yi, k === 0)
-              v0.copy(shape.pillarConvex.vertices[0])
-              v1.copy(shape.pillarConvex.vertices[1])
-              v2.copy(shape.pillarConvex.vertices[2])
-              v0.vadd(shape.pillarOffset, v0)
-              v1.vadd(shape.pillarOffset, v1)
-              v2.vadd(shape.pillarOffset, v2)
-              geometry.vertices.push(
-                new THREE.Vector3(v0.x, v0.y, v0.z),
-                new THREE.Vector3(v1.x, v1.y, v1.z),
-                new THREE.Vector3(v2.x, v2.y, v2.z)
-              )
-              const i = geometry.vertices.length - 3
-              geometry.faces.push(new THREE.Face3(i, i + 1, i + 2))
-            }
-
-        geometry.computeBoundingSphere()
-        geometry.computeFaceNormals()
-        mesh = new THREE.Mesh(geometry, material)
-        break
-
-      case CANNON.Shape.types.TRIMESH:
-        geometry = new THREE.Geometry()
-
-        v0 = new CANNON.Vec3()
-        v1 = new CANNON.Vec3()
-        v2 = new CANNON.Vec3()
-        for (let i = 0; i < shape.indices.length / 3; i++) {
-          shape.getTriangleVertices(i, v0, v1, v2)
-          geometry.vertices.push(
-            new THREE.Vector3(v0.x, v0.y, v0.z),
-            new THREE.Vector3(v1.x, v1.y, v1.z),
-            new THREE.Vector3(v2.x, v2.y, v2.z)
-          )
-          const j = geometry.vertices.length - 3
-          geometry.faces.push(new THREE.Face3(j, j + 1, j + 2))
-        }
-        geometry.computeBoundingSphere()
-        geometry.computeFaceNormals()
-        mesh = new THREE.Mesh(geometry, MutationRecordaterial)
-        break
-
-      default:
-        throw 'Visual type not recognized: ' + shape.type
-    }
-
-    mesh.receiveShadow = receiveShadow
-    mesh.castShadow = castShadow
-
-    mesh.traverse(child => {
-      if (child.isMesh) {
-        child.castShadow = castShadow
-        child.receiveShadow = receiveShadow
-      }
-    })
-
-    const o = body.shapeOffsets[index]
-    const q = body.shapeOrientations[index++]
-    mesh.position.set(o.x, o.y, o.z)
-    mesh.quaternion.set(q.x, q.y, q.z, q.w)
-
-    obj.add(mesh)
-  })
-
-  return obj
-}
-
-function addVisual(body, color = 0x888888, name = 'mesh', castShadow = true, receiveShadow = true, material = new THREE.MeshLambertMaterial({ color })) {
-  body.name = name
-  if (!settings) {
-    settings = {
-      stepFrequency: 60,
-      quatNormalizeSkip: 2,
-      quatNormalizeFast: true,
-      gx: 0,
-      gy: 0,
-      gz: 0,
-      iterations: 3,
-      tolerance: 0.0001,
-      k: 1e6,
-      d: 3,
-      scene: 0,
-      paused: false,
-      rendermode: 'solid',
-      constraints: false,
-      contacts: false,  // Contact points
-      cm2contact: false, // center of mass to contact points
-      normals: false, // contact normals
-      axes: false, // "local" frame axes
-      particleSize: 0.1,
-      shadows: false,
-      aabbs: false,
-      profiling: false,
-      maxSubSteps: 3
-    }
-    particleGeo = new THREE.SphereGeometry(1, 16, 8)
-    particleMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 })
-  }
-  // What geometry should be used?
-  let mesh
-  if (body instanceof CANNON.Body) mesh = shape2Mesh(body, castShadow, receiveShadow, material)
-
-  if (mesh) {
-    // Add body
-    body.threemesh = mesh
-    mesh.castShadow = castShadow
-    mesh.receiveShadow = receiveShadow
-    scene.add(mesh)
-  }
-}
-
 function initPhysics() {
   world = new CANNON.World()
 
@@ -256,7 +76,7 @@ function initPhysics() {
   const chassisBody = new CANNON.Body({ mass: 150, material: groundMaterial })
   chassisBody.addShape(chassisShape)
   chassisBody.position.set(0, 4, 0)
-  addVisual(chassisBody, 0x0000aa, 'car')
+  scene.add(createVisual(chassisBody, 0x0000aa, 'car'))
   light.target = chassisBody.threemesh
   lightOffset = chassisBody.threemesh.position.clone().sub(light.position)
 
@@ -306,7 +126,7 @@ function initPhysics() {
     q.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), Math.PI / 2)
     wheelBody.addShape(cylinderShape, new CANNON.Vec3(), q)
     wheelBodies.push(wheelBody)
-    addVisual(wheelBody, 0x111111, 'wheel')
+    scene.add(createVisual(wheelBody, 0x111111, 'wheel'))
   })
 
   world.addEventListener('postStep', () => {
@@ -339,7 +159,7 @@ function initPhysics() {
   hfBody.position.set(-sizeX * hfShape.elementSize / 2, -4, sizeY * hfShape.elementSize / 2)
   hfBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
   world.add(hfBody)
-  addVisual(hfBody, 0x00aa00, 'landscape')
+  scene.add(createVisual(hfBody, 0x00aa00, 'landscape'))
 }
 
 /* LOOP */
