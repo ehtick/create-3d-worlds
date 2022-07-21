ce9ded1f52dd05112211e1b8700e8d6d645c3476
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-21 13:03:16 +0200

    utils

--- src/90-examples/cannon-car/cannon-utils.js
+++ src/90-examples/cannon-car/cannon-utils.js
@@ -4,6 +4,10 @@ import { createFloor } from '/utils/ground.js'
 
 const phongMaterial = new THREE.MeshPhongMaterial()
 
+const wheelMaterial = new CANNON.Material()
+wheelMaterial.friction = 0.25
+wheelMaterial.restitution = 0.25
+
 export function createObstacles() {
   const obstacles = []
   for (let i = 0; i < 100; i++) {
@@ -33,6 +37,102 @@ export function createGround({ size = 100 } = {}) {
   const body = new CANNON.Body({ mass: 0, material: groundMaterial })
   body.addShape(groundShape)
   body.position.set(0, -1, 0)
+  mesh.body = body
+  return mesh
+}
+
+export function createChaseCam() {
+  const chaseCam = new THREE.Object3D()
+  chaseCam.position.set(0, 0, 0)
+  const pivot = new THREE.Object3D()
+  pivot.position.set(0, 2, 4)
+  chaseCam.add(pivot)
+  return chaseCam
+}
+
+export function createCar() {
+  const carBodyGeometry = new THREE.BoxGeometry(1, 1, 2)
+  const car = new THREE.Mesh(carBodyGeometry, phongMaterial)
+  car.position.y = 3
+  car.castShadow = true
+  const carBodyShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 1))
+  const body = new CANNON.Body({ mass: 1 })
+  body.addShape(carBodyShape)
+  body.position.x = car.position.x
+  body.position.y = car.position.y
+  body.position.z = car.position.z
+  car.body = body
+  return car
+}
+
+export function createFrontLeftWheel() {
+  const geometry = new THREE.CylinderGeometry(0.33, 0.33, 0.2)
+  geometry.rotateZ(Math.PI / 2)
+  const mesh = new THREE.Mesh(geometry, phongMaterial)
+  mesh.position.x = -1
+  mesh.position.y = 3
+  mesh.position.z = -1
+  mesh.castShadow = true
+  const wheelLFShape = new CANNON.Sphere(0.33)
+  const body = new CANNON.Body({ mass: 1, material: wheelMaterial })
+  body.addShape(wheelLFShape)
+  body.position.x = mesh.position.x
+  body.position.y = mesh.position.y
+  body.position.z = mesh.position.z
+  mesh.body = body
+  return mesh
+}
+
+export function createFrontRightWheel() {
+  const geometry = new THREE.CylinderGeometry(0.33, 0.33, 0.2)
+  geometry.rotateZ(Math.PI / 2)
+  const mesh = new THREE.Mesh(geometry, phongMaterial)
+  mesh.position.y = 3
+  mesh.position.x = 1
+  mesh.position.z = -1
+  mesh.castShadow = true
+  const wheelRFShape = new CANNON.Sphere(0.33)
+  const body = new CANNON.Body({ mass: 1, material: wheelMaterial })
+  body.addShape(wheelRFShape)
+  body.position.x = mesh.position.x
+  body.position.y = mesh.position.y
+  body.position.z = mesh.position.z
+  mesh.body = body
+  return mesh
+}
+
+export function createBackLeftWheel() {
+  const geometry = new THREE.CylinderGeometry(0.4, 0.4, 0.33)
+  geometry.rotateZ(Math.PI / 2)
+  const mesh = new THREE.Mesh(geometry, phongMaterial)
+  mesh.position.y = 3
+  mesh.position.x = -1
+  mesh.position.z = 1
+  mesh.castShadow = true
+  const wheelLBShape = new CANNON.Sphere(0.4)
+  const body = new CANNON.Body({ mass: 1, material: wheelMaterial })
+  body.addShape(wheelLBShape)
+  body.position.x = mesh.position.x
+  body.position.y = mesh.position.y
+  body.position.z = mesh.position.z
+  mesh.body = body
+  return mesh
+}
+
+export function createBackRightWheel() {
+  const geometry = new THREE.CylinderGeometry(0.4, 0.4, 0.33)
+  geometry.rotateZ(Math.PI / 2)
+  const mesh = new THREE.Mesh(geometry, phongMaterial)
+  mesh.position.y = 3
+  mesh.position.x = 1
+  mesh.position.z = 1
+  mesh.castShadow = true
+  const wheelRBShape = new CANNON.Sphere(0.4)
+  const body = new CANNON.Body({ mass: 1, material: wheelMaterial })
+  body.addShape(wheelRBShape)
+  body.position.x = mesh.position.x
+  body.position.y = mesh.position.y
+  body.position.z = mesh.position.z
   mesh.body = body
   return mesh
 }
--- src/90-examples/cannon-car/main.js
+++ src/90-examples/cannon-car/main.js
@@ -3,19 +3,16 @@ import * as CANNON from './cannon-es.js'
 import { scene, camera, renderer, clock } from '/utils/scene.js'
 import { initLights } from '/utils/light.js'
 import keyboard from '/classes/Keyboard.js'
-import { createObstacles, createGround } from './cannon-utils.js'
+import { createObstacles, createGround, createCar, createChaseCam, createFrontLeftWheel, createFrontRightWheel, createBackLeftWheel, createBackRightWheel } from './cannon-utils.js'
 
 initLights()
 
-const chaseCam = new THREE.Object3D()
-chaseCam.position.set(0, 0, 0)
-const chaseCamPivot = new THREE.Object3D()
-chaseCamPivot.position.set(0, 2, 4)
-chaseCam.add(chaseCamPivot)
+const chaseCam = createChaseCam()
 
 const phongMaterial = new THREE.MeshPhongMaterial()
 const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
+
 const wheelMaterial = new CANNON.Material()
 wheelMaterial.friction = 0.25
 wheelMaterial.restitution = 0.25
@@ -29,112 +26,46 @@ add(createGround({ size: 100 }))
 
 createObstacles().forEach(add)
 
-const carBodyGeometry = new THREE.BoxGeometry(1, 1, 2)
-const car = new THREE.Mesh(carBodyGeometry, phongMaterial)
-car.position.y = 3
-car.castShadow = true
-scene.add(car)
+const car = createCar()
 car.add(chaseCam)
-const carBodyShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 1))
-const carBody = new CANNON.Body({ mass: 1 })
-carBody.addShape(carBodyShape)
-carBody.position.x = car.position.x
-carBody.position.y = car.position.y
-carBody.position.z = car.position.z
-world.addBody(carBody)
+add(car)
 
-// front left wheel
-const wheelLFGeometry = new THREE.CylinderGeometry(0.33, 0.33, 0.2)
-wheelLFGeometry.rotateZ(Math.PI / 2)
-const wheelLF = new THREE.Mesh(wheelLFGeometry, phongMaterial)
-wheelLF.position.x = -1
-wheelLF.position.y = 3
-wheelLF.position.z = -1
-wheelLF.castShadow = true
-scene.add(wheelLF)
-const wheelLFShape = new CANNON.Sphere(0.33)
-const wheelLFBody = new CANNON.Body({ mass: 1, material: wheelMaterial })
-wheelLFBody.addShape(wheelLFShape)
-wheelLFBody.position.x = wheelLF.position.x
-wheelLFBody.position.y = wheelLF.position.y
-wheelLFBody.position.z = wheelLF.position.z
-world.addBody(wheelLFBody)
+const wheelLF = createFrontLeftWheel()
+add(wheelLF)
 
-// front right wheel
-const wheelRFGeometry = new THREE.CylinderGeometry(0.33, 0.33, 0.2)
-wheelRFGeometry.rotateZ(Math.PI / 2)
-const wheelRF = new THREE.Mesh(wheelRFGeometry, phongMaterial)
-wheelRF.position.y = 3
-wheelRF.position.x = 1
-wheelRF.position.z = -1
-wheelRF.castShadow = true
-scene.add(wheelRF)
-const wheelRFShape = new CANNON.Sphere(0.33)
-const wheelRFBody = new CANNON.Body({ mass: 1, material: wheelMaterial })
-wheelRFBody.addShape(wheelRFShape)
-wheelRFBody.position.x = wheelRF.position.x
-wheelRFBody.position.y = wheelRF.position.y
-wheelRFBody.position.z = wheelRF.position.z
-world.addBody(wheelRFBody)
+const wheelRF = createFrontRightWheel()
+add(wheelRF)
 
-// back left wheel
-const wheelLBGeometry = new THREE.CylinderGeometry(0.4, 0.4, 0.33)
-wheelLBGeometry.rotateZ(Math.PI / 2)
-const wheelLB = new THREE.Mesh(wheelLBGeometry, phongMaterial)
-wheelLB.position.y = 3
-wheelLB.position.x = -1
-wheelLB.position.z = 1
-wheelLB.castShadow = true
-scene.add(wheelLB)
-const wheelLBShape = new CANNON.Sphere(0.4)
-const wheelLBBody = new CANNON.Body({ mass: 1, material: wheelMaterial })
-wheelLBBody.addShape(wheelLBShape)
-wheelLBBody.position.x = wheelLB.position.x
-wheelLBBody.position.y = wheelLB.position.y
-wheelLBBody.position.z = wheelLB.position.z
-world.addBody(wheelLBBody)
+const wheelLB = createBackLeftWheel()
+add(wheelLB)
 
-// back right wheel
-const wheelRBGeometry = new THREE.CylinderGeometry(0.4, 0.4, 0.33)
-wheelRBGeometry.rotateZ(Math.PI / 2)
-const wheelRB = new THREE.Mesh(wheelRBGeometry, phongMaterial)
-wheelRB.position.y = 3
-wheelRB.position.x = 1
-wheelRB.position.z = 1
-wheelRB.castShadow = true
-scene.add(wheelRB)
-const wheelRBShape = new CANNON.Sphere(0.4)
-const wheelRBBody = new CANNON.Body({ mass: 1, material: wheelMaterial })
-wheelRBBody.addShape(wheelRBShape)
-wheelRBBody.position.x = wheelRB.position.x
-wheelRBBody.position.y = wheelRB.position.y
-wheelRBBody.position.z = wheelRB.position.z
-world.addBody(wheelRBBody)
-const leftFrontAxis = new CANNON.Vec3(1, 0, 0)
-const rightFrontAxis = new CANNON.Vec3(1, 0, 0)
-const leftBackAxis = new CANNON.Vec3(1, 0, 0)
-const rightBackAxis = new CANNON.Vec3(1, 0, 0)
-const constraintLF = new CANNON.HingeConstraint(carBody, wheelLFBody, {
+const wheelRB = createBackRightWheel()
+add(wheelRB)
+
+const constraintLF = new CANNON.HingeConstraint(car.body, wheelLF.body, {
   pivotA: new CANNON.Vec3(-1, -0.5, -1),
-  axisA: leftFrontAxis,
+  axisA: new CANNON.Vec3(1, 0, 0),
   maxForce: 0.99,
 })
 world.addConstraint(constraintLF)
-const constraintRF = new CANNON.HingeConstraint(carBody, wheelRFBody, {
+
+const constraintRF = new CANNON.HingeConstraint(car.body, wheelRF.body, {
   pivotA: new CANNON.Vec3(1, -0.5, -1),
-  axisA: rightFrontAxis,
+  axisA: new CANNON.Vec3(1, 0, 0),
   maxForce: 0.99,
 })
 world.addConstraint(constraintRF)
-const constraintLB = new CANNON.HingeConstraint(carBody, wheelLBBody, {
+
+const constraintLB = new CANNON.HingeConstraint(car.body, wheelLB.body, {
   pivotA: new CANNON.Vec3(-1, -0.5, 1),
-  axisA: leftBackAxis,
+  axisA: new CANNON.Vec3(1, 0, 0),
   maxForce: 0.99,
 })
 world.addConstraint(constraintLB)
-const constraintRB = new CANNON.HingeConstraint(carBody, wheelRBBody, {
+
+const constraintRB = new CANNON.HingeConstraint(car.body, wheelRB.body, {
   pivotA: new CANNON.Vec3(1, -0.5, 1),
-  axisA: rightBackAxis,
+  axisA: new CANNON.Vec3(1, 0, 0),
   maxForce: 0.99,
 })
 world.addConstraint(constraintRB)
@@ -143,27 +74,25 @@ world.addConstraint(constraintRB)
 constraintLB.enableMotor()
 constraintRB.enableMotor()
 let forwardVelocity = 0
-let rightVelocity = 0
+let sideVelocity = 0
 
 /* LOOP */
 
 const v = new THREE.Vector3()
 let thrusting = false
 
+const physicMeshes = [car, wheelLF, wheelRF, wheelLB, wheelRB]
+
+const updateCar = () => physicMeshes.forEach(mesh => {
+  mesh.position.copy(mesh.body.position)
+  mesh.quaternion.copy(mesh.body.quaternion)
+})
+
 void function loop() {
   requestAnimationFrame(loop)
   const delta = clock.getDelta()
   world.step(delta)
-  car.position.copy(carBody.position)
-  car.quaternion.copy(carBody.quaternion)
-  wheelLF.position.copy(wheelLFBody.position)
-  wheelLF.quaternion.copy(wheelLFBody.quaternion)
-  wheelRF.position.copy(wheelRFBody.position)
-  wheelRF.quaternion.copy(wheelRFBody.quaternion)
-  wheelLB.position.copy(wheelLBBody.position)
-  wheelLB.quaternion.copy(wheelLBBody.quaternion)
-  wheelRB.position.copy(wheelRBBody.position)
-  wheelRB.quaternion.copy(wheelRBBody.quaternion)
+  updateCar()
   thrusting = false
 
   if (keyboard.up) {
@@ -175,10 +104,10 @@ void function loop() {
     thrusting = true
   }
   if (keyboard.left)
-    if (rightVelocity > -1.0) rightVelocity -= 0.1
+    if (sideVelocity > -1.0) sideVelocity -= 0.1
 
   if (keyboard.right)
-    if (rightVelocity < 1.0) rightVelocity += 0.1
+    if (sideVelocity < 1.0) sideVelocity += 0.1
 
   if (keyboard.pressed.Space) {
     if (forwardVelocity > 0)
@@ -195,10 +124,10 @@ void function loop() {
   }
   constraintLB.setMotorSpeed(forwardVelocity)
   constraintRB.setMotorSpeed(forwardVelocity)
-  constraintLF.axisA.z = rightVelocity
-  constraintRF.axisA.z = rightVelocity
+  constraintLF.axisA.z = sideVelocity
+  constraintRF.axisA.z = sideVelocity
   camera.lookAt(car.position)
-  chaseCamPivot.getWorldPosition(v)
+  chaseCam.children[0].getWorldPosition(v) // pivot
 
   if (v.y < 1) v.y = 1
 
