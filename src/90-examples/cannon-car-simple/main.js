8fb5f72caa32af02b9bd14d2b9d05eb0937d19e8
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-22 14:41:15 +0200

    pomalo

--- src/90-examples/cannon-car-simple/main.js
+++ src/90-examples/cannon-car-simple/main.js
@@ -1,26 +1,14 @@
-let container = document.querySelector('body'),
-  w = container.clientWidth,
-  h = container.clientHeight,
-  scene = new THREE.Scene(),
-  camera = new THREE.PerspectiveCamera(75, w / h, 0.001, 100),
-  renderConfig = { antialias: true, alpha: true },
-  renderer = new THREE.WebGLRenderer(renderConfig)
+import * as THREE from '/node_modules/three127/build/three.module.js'
+import * as CANNON from '/libs/cannon-es.js'
+import { scene, camera, renderer } from '/utils/scene.js'
+import CannonDebugRenderer from '/libs/cannonDebugRenderer.js'
+import keyboard from '/classes/Keyboard.js'
+
 camera.position.set(0, 1, -10)
 camera.lookAt(0, 0, 0)
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(w, h)
-container.appendChild(renderer.domElement)
 
-window.addEventListener('resize', () => {
-  w = container.clientWidth
-  h = container.clientHeight
-  camera.aspect = w / h
-  camera.updateProjectionMatrix()
-  renderer.setSize(w, h)
-})
-
-var geometry = new THREE.PlaneGeometry(10, 10, 10)
-var material = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+let geometry = new THREE.PlaneGeometry(100, 100, 10)
+let material = new THREE.MeshBasicMaterial({ color: 0x999999, side: THREE.DoubleSide })
 const plane = new THREE.Mesh(geometry, material)
 plane.rotation.x = Math.PI / 2
 scene.add(plane)
@@ -29,17 +17,13 @@ const sunlight = new THREE.DirectionalLight(0xffffff, 1.0)
 sunlight.position.set(-10, 10, 0)
 scene.add(sunlight)
 
-/**
-* Physics
-**/
-
 const world = new CANNON.World()
 world.broadphase = new CANNON.SAPBroadphase(world)
 world.gravity.set(0, -10, 0)
 world.defaultContactMaterial.friction = 0
 
-const groundMaterial = new CANNON.Material('groundMaterial')
-const wheelMaterial = new CANNON.Material('wheelMaterial')
+const groundMaterial = new CANNON.Material()
+const wheelMaterial = new CANNON.Material()
 const wheelGroundContactMaterial = new CANNON.ContactMaterial(wheelMaterial, groundMaterial, {
   friction: 0.3,
   restitution: 0,
@@ -47,21 +31,20 @@ const wheelGroundContactMaterial = new CANNON.ContactMaterial(wheelMaterial, gro
 })
 
 world.addContactMaterial(wheelGroundContactMaterial)
+const cannonDebugRenderer = new CannonDebugRenderer(scene, world)
 
-// car physics body
+// car
 const chassisShape = new CANNON.Box(new CANNON.Vec3(1, 0.3, 2))
 const chassisBody = new CANNON.Body({ mass: 150 })
 chassisBody.addShape(chassisShape)
 chassisBody.position.set(0, 0.2, 0)
 chassisBody.angularVelocity.set(0, 0, 0) // initial velocity
 
-// car visual body
-var geometry = new THREE.BoxGeometry(2, 0.6, 4) // double chasis shape
-var material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide })
-const box = new THREE.Mesh(geometry, material)
-scene.add(box)
+geometry = new THREE.BoxGeometry(2, 0.6, 4)
+material = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+const chassis = new THREE.Mesh(geometry, material)
+scene.add(chassis)
 
-// parent vehicle object
 const vehicle = new CANNON.RaycastVehicle({
   chassisBody,
   indexRightAxis: 0, // x
@@ -69,8 +52,7 @@ const vehicle = new CANNON.RaycastVehicle({
   indexForwardAxis: 2, // z
 })
 
-// wheel options
-const options = {
+const wheelOptions = {
   radius: 0.3,
   directionLocal: new CANNON.Vec3(0, -1, 0),
   suspensionStiffness: 45,
@@ -88,23 +70,22 @@ const options = {
 }
 
 const axlewidth = 0.7
-options.chassisConnectionPointLocal.set(axlewidth, 0, -1)
-vehicle.addWheel(options)
+wheelOptions.chassisConnectionPointLocal.set(axlewidth, 0, -1)
+vehicle.addWheel(wheelOptions)
 
-options.chassisConnectionPointLocal.set(-axlewidth, 0, -1)
-vehicle.addWheel(options)
+wheelOptions.chassisConnectionPointLocal.set(-axlewidth, 0, -1)
+vehicle.addWheel(wheelOptions)
 
-options.chassisConnectionPointLocal.set(axlewidth, 0, 1)
-vehicle.addWheel(options)
+wheelOptions.chassisConnectionPointLocal.set(axlewidth, 0, 1)
+vehicle.addWheel(wheelOptions)
 
-options.chassisConnectionPointLocal.set(-axlewidth, 0, 1)
-vehicle.addWheel(options)
+wheelOptions.chassisConnectionPointLocal.set(-axlewidth, 0, 1)
+vehicle.addWheel(wheelOptions)
 
 vehicle.addToWorld(world)
 
-// car wheels
-const wheelBodies = [],
-  wheelVisuals = []
+const wheelBodies = [], wheelVisuals = []
+
 vehicle.wheelInfos.forEach(wheel => {
   const shape = new CANNON.Cylinder(wheel.radius, wheel.radius, wheel.radius / 2, 20)
   const body = new CANNON.Body({ mass: 1, material: wheelMaterial })
@@ -112,11 +93,9 @@ vehicle.wheelInfos.forEach(wheel => {
   q.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), Math.PI / 2)
   body.addShape(shape, new CANNON.Vec3(), q)
   wheelBodies.push(body)
-  // wheel visual body
   const geometry = new THREE.CylinderGeometry(wheel.radius, wheel.radius, 0.4, 32)
   const material = new THREE.MeshPhongMaterial({
-    color: 0xd0901d,
-    emissive: 0xaa0000,
+    color: 0x444444,
     side: THREE.DoubleSide,
     flatShading: true,
   })
@@ -126,15 +105,12 @@ vehicle.wheelInfos.forEach(wheel => {
   scene.add(cylinder)
 })
 
-// update the wheels to match the physics
 world.addEventListener('postStep', () => {
   for (let i = 0; i < vehicle.wheelInfos.length; i++) {
     vehicle.updateWheelTransform(i)
     const t = vehicle.wheelInfos[i].worldTransform
-    // update wheel physics
     wheelBodies[i].position.copy(t.position)
     wheelBodies[i].quaternion.copy(t.quaternion)
-    // update wheel visuals
     wheelVisuals[i].position.copy(t.position)
     wheelVisuals[i].quaternion.copy(t.quaternion)
   }
@@ -142,65 +118,63 @@ world.addEventListener('postStep', () => {
 
 const q = plane.quaternion
 const planeBody = new CANNON.Body({
-  mass: 0, // mass = 0 makes the body static
+  mass: 0, // makes body static
   material: groundMaterial,
   shape: new CANNON.Plane(),
   quaternion: new CANNON.Quaternion(-q._x, q._y, q._z, q._w)
 })
-world.add(planeBody)
 
-/**
-* Main
-**/
+world.addBody(planeBody)
 
-function updatePhysics() {
-  world.step(1 / 60)
-  // update the chassis position
-  box.position.copy(chassisBody.position)
-  box.quaternion.copy(chassisBody.quaternion)
-}
+/** LOOP **/
 
-function render() {
-  requestAnimationFrame(render)
-  renderer.render(scene, camera)
-  updatePhysics()
-}
+function handleInput() {
+  const brakeForce = 5
+  const engineForce = 800
+  const maxSteerVal = 0.5
 
-function navigate(e) {
-  if (e.type != 'keydown' && e.type != 'keyup') return
-  const keyup = e.type == 'keyup'
-  vehicle.setBrake(0, 0)
-  vehicle.setBrake(0, 1)
-  vehicle.setBrake(0, 2)
-  vehicle.setBrake(0, 3)
+  if (!keyboard.keyPressed) {
+    vehicle.applyEngineForce(0, 2)
+    vehicle.applyEngineForce(0, 3)
+    return
+  }
 
-  const engineForce = 800,
-    maxSteerVal = 0.3
-  switch (e.keyCode) {
+  if (keyboard.pressed.Space) {
+    vehicle.setBrake(brakeForce, 0)
+    vehicle.setBrake(brakeForce, 1)
+    vehicle.setBrake(brakeForce, 2)
+    vehicle.setBrake(brakeForce, 3)
+  }
 
-    case 38: // forward
-      vehicle.applyEngineForce(keyup ? 0 : -engineForce, 2)
-      vehicle.applyEngineForce(keyup ? 0 : -engineForce, 3)
-      break
+  if (keyboard.up) {
+    vehicle.applyEngineForce(-engineForce, 2)
+    vehicle.applyEngineForce(-engineForce, 3)
+  }
 
-    case 40: // backward
-      vehicle.applyEngineForce(keyup ? 0 : engineForce, 2)
-      vehicle.applyEngineForce(keyup ? 0 : engineForce, 3)
-      break
+  if (keyboard.down) {
+    vehicle.applyEngineForce(engineForce, 2)
+    vehicle.applyEngineForce(engineForce, 3)
+  }
 
-    case 39: // right
-      vehicle.setSteeringValue(keyup ? 0 : -maxSteerVal, 2)
-      vehicle.setSteeringValue(keyup ? 0 : -maxSteerVal, 3)
-      break
+  if (keyboard.left) {
+    vehicle.setSteeringValue(maxSteerVal, 2)
+    vehicle.setSteeringValue(maxSteerVal, 3)
+  }
 
-    case 37: // left
-      vehicle.setSteeringValue(keyup ? 0 : maxSteerVal, 2)
-      vehicle.setSteeringValue(keyup ? 0 : maxSteerVal, 3)
-      break
+  if (keyboard.right) {
+    vehicle.setSteeringValue(-maxSteerVal, 2)
+    vehicle.setSteeringValue(-maxSteerVal, 3)
   }
 }
 
-window.addEventListener('keydown', navigate)
-window.addEventListener('keyup', navigate)
+void function render() {
+  requestAnimationFrame(render)
+  cannonDebugRenderer.update()
 
-render()
+  handleInput()
+  world.step(1 / 60)
+  chassis.position.copy(chassisBody.position)
+  chassis.quaternion.copy(chassisBody.quaternion)
+
+  renderer.render(scene, camera)
+}()
