35721b6199d7f9fe0bda5a5d52f968cd337ad422
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-22 23:05:13 +0200

    colors

--- src/90-examples/cannon-car/utils.js
+++ src/90-examples/cannon-car/utils.js
@@ -1,23 +1,31 @@
 import * as THREE from '/node_modules/three127/build/three.module.js'
 import * as CANNON from '/libs/cannon-es.js'
 import { createFloor } from '/utils/ground.js'
+import { randomInRange } from '/utils/helpers.js'
 
-const phongMaterial = new THREE.MeshPhongMaterial()
+const colors = [0xc2b280, 0xF2D16B, 0xedc9af, 0xfffacd, 0xF3CCAA]
 
 export function createObstacles() {
-  const radiusTop = 0, radiusBottom = 3, height = .5, radialSegments = 5
+  const radiusTop = 0
   const obstacles = []
 
   for (let i = 0; i < 100; i++) {
+    const radiusBottom = randomInRange(2, 3, true)
+    const height = randomInRange(radiusBottom * .1, radiusBottom * .4)
+    const radialSegments = radiusBottom * 5
+
     const geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments)
+    const phongMaterial = new THREE.MeshPhongMaterial({ color: colors[i % colors.length] })
     const mesh = new THREE.Mesh(geometry, phongMaterial)
     mesh.position.x = Math.random() * 100 - 50
     mesh.position.y = 0.5
     mesh.position.z = Math.random() * 100 - 50
-    const cylinderShape = new CANNON.Cylinder(radiusTop + 0.01, radiusBottom, height, radialSegments)
+
+    const shape = new CANNON.Cylinder(radiusTop + 0.01, radiusBottom, height, radialSegments)
     const body = new CANNON.Body({ mass: 0 })
-    body.addShape(cylinderShape, new CANNON.Vec3())
+    body.addShape(shape, new CANNON.Vec3())
     body.position.copy(mesh.position)
+
     mesh.body = body
     obstacles.push(mesh)
   }
@@ -25,7 +33,7 @@ export function createObstacles() {
 }
 
 export function createGround({ size = 100 } = {}) {
-  const mesh = createFloor({ size })
+  const mesh = createFloor({ size, color: colors[1] })
   const material = new CANNON.Material()
   material.friction = 0.25
   material.restitution = 0.25
