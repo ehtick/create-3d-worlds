d995b11eb73b1f7305715a91576054d3f0c2dfc6
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-23 13:11:28 +0200

    m

--- src/90-examples/globe/main.js
+++ src/90-examples/globe/main.js
@@ -1,21 +1,14 @@
 import * as THREE from '/node_modules/three127/build/three.module.js'
-import { OrbitControls } from '/node_modules/three127/examples/jsm/controls/OrbitControls.js'
-import { scene, camera, renderer } from '/utils/scene.js'
+import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
+import { createSunLight } from '/utils/light.js'
 
 const textureLoader = new THREE.TextureLoader()
 
-const ambientLight = new THREE.AmbientLight(0x444444)
-scene.add(ambientLight)
+scene.add(createSunLight())
 
-const light = new THREE.DirectionalLight(0xffffff, 2)
-light.position.set(0, 0, 3)
+const controls = createOrbitControls()
 
-scene.add(light)
-
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.screenSpacePanning = true
-
-const sphereGeometry = new THREE.SphereGeometry(3, 720, 360)
+const geometry = new THREE.SphereGeometry(3, 720, 360)
 const material = new THREE.MeshStandardMaterial()
 const texture = textureLoader.load('img/worldColour.5400x2700.jpg')
 texture.anisotropy = renderer.capabilities.getMaxAnisotropy()
@@ -24,7 +17,7 @@ material.map = texture
 material.displacementMap = textureLoader.load('img/world.5400x2700.jpg')
 material.displacementScale = 0.2
 
-const sphere = new THREE.Mesh(sphereGeometry, material)
+const sphere = new THREE.Mesh(geometry, material)
 sphere.rotation.y = -Math.PI / 2
 sphere.castShadow = sphere.receiveShadow = true
 scene.add(sphere)
