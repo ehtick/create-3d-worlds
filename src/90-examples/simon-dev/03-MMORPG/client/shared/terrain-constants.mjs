c3155ff9216dc3a65835296af17dd298f539d52c
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-24 15:33:20 +0200

    lint servr

--- src/90-examples/simon-dev/03-MMORPG/client/shared/terrain-constants.mjs
+++ src/90-examples/simon-dev/03-MMORPG/client/shared/terrain-constants.mjs
@@ -1,23 +1,22 @@
 
-
 export const terrain_constants = (function() {
-  const QT_MIN_CELL_SIZE = 100;
+  const QT_MIN_CELL_SIZE = 100
   // const QT_MIN_CELL_RESOLUTION = 24;
-  const QT_MIN_CELL_RESOLUTION = 16;
+  const QT_MIN_CELL_RESOLUTION = 16
   // const QT_MIN_CELL_RESOLUTION = 4;
-  const PLANET_RADIUS = 8000.0;
+  const PLANET_RADIUS = 8000.0
 
-  const NOISE_HEIGHT = 800.0;
+  const NOISE_HEIGHT = 800.0
   // const NOISE_HEIGHT = 0.0;
-  const NOISE_SCALE = 1800.0;
+  const NOISE_SCALE = 1800.0
 
   return {
-    QT_MIN_CELL_SIZE: QT_MIN_CELL_SIZE,
-    QT_MIN_CELL_RESOLUTION: QT_MIN_CELL_RESOLUTION,
-    PLANET_RADIUS: PLANET_RADIUS,
+    QT_MIN_CELL_SIZE,
+    QT_MIN_CELL_RESOLUTION,
+    PLANET_RADIUS,
 
-    NOISE_HEIGHT: NOISE_HEIGHT,
-    NOISE_SCALE: NOISE_SCALE,
+    NOISE_HEIGHT,
+    NOISE_SCALE,
     NOISE_PARAMS: {
       octaves: 10,
       persistence: 0.5,
@@ -28,4 +27,4 @@ export const terrain_constants = (function() {
       seed: 1
     },
   }
-})();
+})()
