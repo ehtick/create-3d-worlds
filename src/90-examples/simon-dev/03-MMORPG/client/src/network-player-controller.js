e3daff693222257c53b8174184bafb534e4aabfa
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-24 22:42:57 +0200

    mmm

--- src/90-examples/simon-dev/03-MMORPG/client/src/network-player-controller.js
+++ src/90-examples/simon-dev/03-MMORPG/client/src/network-player-controller.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.127/build/three.module.js'
+import * as THREE from '/node_modules/three127/build/three.module.js'
 
 import { entity } from './entity.js'
 
--- src/90-examples/simon-dev/03-MMORPG/client/src/particle-system.js
+++ src/90-examples/simon-dev/03-MMORPG/client/src/particle-system.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.127/build/three.module.js'
+import * as THREE from '/node_modules/three127/build/three.module.js'
 import { scene, camera } from '/utils/scene.js'
 
 const vertexShader = `
