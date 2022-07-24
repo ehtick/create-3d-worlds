fcd80b9663e5694463bc2f11e3d1bfcaeb38020c
Damjan Pavlica <damjan.pavlica@origo.is>
2022-07-24 15:14:35 +0200

    org

--- src/90-examples/simon-dev/01-procedural-terrain/main.js
+++ src/90-examples/simon-dev/01-procedural-terrain/main.js
@@ -2,7 +2,7 @@ import * as THREE from '/node_modules/three127/build/three.module.js'
 import { scene, camera, renderer, clock } from '/utils/scene.js'
 import { moveCamera } from '/utils/player.js'
 import { createSunLight } from '/utils/light.js'
-import { TerrainChunkManager } from './src/terrain.js'
+import { TerrainChunkManager } from '../terrain/terrain.js'
 
 scene.add(createSunLight())
 scene.background = new THREE.Color(0xbfd1e5)
--- src/90-examples/simon-dev/terrain/noise.js
+++ src/90-examples/simon-dev/terrain/noise.js
@@ -1,7 +1,7 @@
 import 'https://cdn.jsdelivr.net/npm/simplex-noise@2.4.0/simplex-noise.js'
 import perlin from 'https://cdn.jsdelivr.net/gh/mikechambers/es6-perlin-module/perlin.js'
 
-import { math } from '../../utils/math.js'
+import { math } from '../utils/math.js'
 
 class _PerlinWrapper {
   noise2D(x, y) {
