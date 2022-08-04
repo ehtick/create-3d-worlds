da17c22f02c6675a03ea38e7cc0c5b0b58468250
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-05 00:53:23 +0200

    new example

---
+++ src/25-shaders/toon/main.js
@@ -0,0 +1,71 @@
+let scene, renderer, camera, lightmodel, teapotMaterial
+let angle = 0
+let pointLight
+
+init()
+animate()
+
+function init() {
+  const width = window.innerWidth
+  const height = window.innerHeight
+
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  })
+  renderer.setSize(width, height)
+  renderer.setClearColor(0x888888)
+  document.body.appendChild(renderer.domElement)
+
+  scene = new THREE.Scene()
+
+  camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000)
+  camera.position.z = 200
+  camera.lookAt(new THREE.Vector3(0, 0, 0))
+
+  const controls = new THREE.OrbitControls(camera, renderer.domElement)
+
+  pointLight = new THREE.PointLight(0xffffff)
+  // pointLight.position.set(0, 300, 200);
+  scene.add(pointLight)
+  const ambientLight = new THREE.AmbientLight(0x111111)
+  scene.add(ambientLight)
+
+  lightmodel = new THREE.Mesh(new THREE.SphereGeometry(3, 16, 16), new THREE.MeshBasicMaterial({
+    color: 0xffff00
+  }))
+  scene.add(lightmodel)
+  // ////////////////////////////////////////////////////////////////////
+  teapotMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+      lightpos: {
+        type: 'v3',
+        value: new THREE.Vector3(0, 30, 20)
+      }
+    },
+    vertexShader: document.getElementById('myVertexShader').textContent,
+    fragmentShader: document.getElementById('myFragmentShader').textContent
+  })
+
+  const geometry = new THREE.TeapotGeometry()
+  const teapot = new THREE.Mesh(geometry, teapotMaterial)
+  teapot.scale.set(.21, .21, .21)
+  teapot.position.set(50, 0, 0)
+  const teapot2 = teapot.clone()
+  teapot2.position.set(-50, 0, 50)
+
+  scene.add(teapot)
+  scene.add(teapot2)
+}
+
+/* LOOP */
+
+function animate() {
+  angle += 0.01
+  pointLight.position.set(40 * Math.cos(angle), 75, 40 * Math.sin(angle))
+
+  teapotMaterial.uniforms.lightpos.value.copy(pointLight.position)
+
+  lightmodel.position.copy(pointLight.position)
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
