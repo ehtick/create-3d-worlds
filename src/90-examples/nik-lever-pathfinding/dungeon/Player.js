8af5f65a8c6920afcb56145266cc8be72e9a3080
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-06 21:57:23 +0200

    m

--- src/90-examples/nik-lever-pathfinding/dungeon/Player.js
+++ src/90-examples/nik-lever-pathfinding/dungeon/Player.js
@@ -5,40 +5,33 @@ class Player {
   constructor(options) {
     this.assetsPath = options.assetsPath
     this.name = options.name || 'Player'
-
     this.animations = {}
-
     scene.add(options.object)
 
     this.object = options.object
     this.pathLines = new THREE.Object3D()
     this.pathColor = new THREE.Color(0xFFFFFF)
     this.nodeRadius = (options.nodeRadius) ? options.nodeRadius : 0.2
-
     scene.add(this.pathLines)
 
     this.npc = options.npc
-
     if (this.npc) this.dead = false
 
     this.speed = options.speed
     this.app = options.app
-
     this.pathfinder = options.pathfinder
     this.navMeshGroup = this.pathfinder.getGroup('dungeon', this.object.position)
 
     const { clip } = options
-    const self = this
-
-    const pt = this.object.position.clone()
-    pt.z += 10
-    this.object.lookAt(pt)
+    const point = this.object.position.clone()
+    point.z += 10
+    this.object.lookAt(point)
 
     if (options.anims) {
       // Use this option to crop a single animation into multiple clips
       this.mixer = new THREE.AnimationMixer(options.object)
       options.anims.forEach(anim => {
-        self.animations[anim.name] = THREE.AnimationUtils.subclip(clip, anim.name, anim.start, anim.end)
+        this.animations[anim.name] = THREE.AnimationUtils.subclip(clip, anim.name, anim.start, anim.end)
       })
     }
 
@@ -46,27 +39,25 @@ class Player {
       // Use this option to set multiple animations directly
       this.mixer = new THREE.AnimationMixer(options.object)
       options.animations.forEach(animation => {
-        self.animations[animation.name.toLowerCase()] = animation
+        this.animations[animation.name.toLowerCase()] = animation
       })
     }
   }
 
-  newPath(pt) {
-    if (!pt) return
+  newPath(point) {
+    if (!point) return
     const player = this.object
 
     if (this.pathfinder === undefined) {
-      this.calculatedPath = [pt.clone()]
+      this.calculatedPath = [point.clone()]
       this.setTargetDirection()
       return
     }
 
-    // Calculate a path to the target and store it
-    this.calculatedPath = this.pathfinder.findPath(player.position, pt, 'dungeon', this.navMeshGroup)
+    this.calculatedPath = this.pathfinder.findPath(player.position, point, 'dungeon', this.navMeshGroup)
 
     if (this.calculatedPath && this.calculatedPath.length) {
       this.action = 'walk'
-
       this.setTargetDirection()
     } else {
       this.action = 'idle'
@@ -76,16 +67,15 @@ class Player {
 
   setTargetDirection() {
     const player = this.object
-    const pt = this.calculatedPath[0].clone()
-    pt.y = player.position.y
+    const point = this.calculatedPath[0].clone()
+    point.y = player.position.y
     const quaternion = player.quaternion.clone()
-    player.lookAt(pt)
+    player.lookAt(point)
     this.quaternion = player.quaternion.clone()
     player.quaternion.copy(quaternion)
   }
 
   set action(name) {
-    // Make a copy of the clip if this is a remote player
     if (this.actionName == name.toLowerCase()) return
 
     const clip = this.animations[name.toLowerCase()]
@@ -108,14 +98,11 @@ class Player {
   update(dt) {
     const { speed } = this
     const player = this.object
-
     if (this.mixer) this.mixer.update(dt)
 
     if (this.calculatedPath && this.calculatedPath.length) {
       const targetPosition = this.calculatedPath[0]
-
       const vel = targetPosition.clone().sub(player.position)
-
       let pathLegComplete = (vel.lengthSq() < 0.01)
 
       if (!pathLegComplete) {
@@ -127,13 +114,12 @@ class Player {
         player.position.add(vel.multiplyScalar(dt * speed))
         // Get distance after moving, if greater then we've overshot and this leg is complete
         const newDistanceSq = player.position.distanceToSquared(targetPosition)
-        pathLegComplete = (newDistanceSq > prevDistanceSq)
+        pathLegComplete = newDistanceSq > prevDistanceSq
       }
 
       if (pathLegComplete) {
-        // Remove node from the path we calculated
         this.calculatedPath.shift()
-        if (this.calculatedPath.length == 0)
+        if (!this.calculatedPath.length)
           if (this.npc)
             this.newPath(this.app.randomWaypoint)
           else {
@@ -142,7 +128,6 @@ class Player {
           }
         else
           this.setTargetDirection()
-
       }
     } else
     if (this.npc && !this.dead) this.newPath(this.app.randomWaypoint)
