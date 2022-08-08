3c3090f13da609fb0ea48ef81ae0a6d54e9c62a7
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-08 15:51:25 +0200

    m

--- src/90-examples/drag/main.js
+++ src/90-examples/drag/main.js
@@ -1,109 +1,43 @@
 import * as THREE from 'three'
 import { DragControls } from '/node_modules/three/examples/jsm/controls/DragControls.js'
-import {scene, renderer, camera} from '/utils/scene.js'
+import { scene, renderer, camera } from '/utils/scene.js'
 
-let controls, group
-let enableSelection = false
+const { randInt, randFloat } = THREE.MathUtils
 
 const objects = []
 
-const mouse = new THREE.Vector2(), raycaster = new THREE.Raycaster()
+camera.position.z = 800
+scene.add(new THREE.AmbientLight(0x505050))
 
-init()
+const light = new THREE.SpotLight(0xffffff, 1.5)
+light.position.set(0, 500, 2000)
+scene.add(light)
 
-function init() {
-  camera.position.z = 1000
-  scene.add(new THREE.AmbientLight(0x505050))
+const geometry = new THREE.BoxGeometry(40, 40, 40)
 
-  const light = new THREE.SpotLight(0xffffff, 1.5)
-  light.position.set(0, 500, 2000)
-  scene.add(light)
+for (let i = 0; i < 200; i ++) {
+  const object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }))
 
-  group = new THREE.Group()
-  scene.add(group)
+  object.position.x = randInt(-500, 500)
+  object.position.y = randInt(-300, 300)
+  object.position.z = randInt(-400, 400)
 
-  const geometry = new THREE.BoxGeometry(40, 40, 40)
+  object.scale.x = randFloat(1, 3)
+  object.scale.y = randFloat(1, 3)
+  object.scale.z = randFloat(1, 3)
 
-  for (let i = 0; i < 200; i ++) {
-    const object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }))
-
-    object.position.x = Math.random() * 1000 - 500
-    object.position.y = Math.random() * 600 - 300
-    object.position.z = Math.random() * 800 - 400
-
-    object.rotation.x = Math.random() * 2 * Math.PI
-    object.rotation.y = Math.random() * 2 * Math.PI
-    object.rotation.z = Math.random() * 2 * Math.PI
-
-    object.scale.x = Math.random() * 2 + 1
-    object.scale.y = Math.random() * 2 + 1
-    object.scale.z = Math.random() * 2 + 1
-
-    object.castShadow = true
-    object.receiveShadow = true
-
-    scene.add(object)
-
-    objects.push(object)
-  }
-
-  controls = new DragControls([... objects], camera, renderer.domElement)
-  controls.addEventListener('drag', render)
-
-  document.addEventListener('click', onClick)
-  window.addEventListener('keydown', onKeyDown)
-  window.addEventListener('keyup', onKeyUp)
-
-  render()
+  scene.add(object)
+  objects.push(object)
 }
 
-function onKeyDown(event) {
-  enableSelection = (event.keyCode === 16)
-}
+const controls = new DragControls(objects, camera, renderer.domElement)
 
-function onKeyUp() {
-  enableSelection = false
-}
-
-function onClick(event) {
-  event.preventDefault()
-
-  if (enableSelection === true) {
-
-    const draggableObjects = controls.getObjects()
-    draggableObjects.length = 0
-
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1
-
-    raycaster.setFromCamera(mouse, camera)
-
-    const intersections = raycaster.intersectObjects(objects, true)
-
-    if (intersections.length > 0) {
-      const { object } = intersections[0]
-
-      if (group.children.includes(object) === true) {
-        object.material.emissive.set(0x000000)
-        scene.attach(object)
-      } else {
-        object.material.emissive.set(0xaaaaaa)
-        group.attach(object)
-      }
-
-      controls.transformGroup = true
-      draggableObjects.push(group)
-    }
-
-    if (group.children.length === 0) {
-      controls.transformGroup = false
-      draggableObjects.push(...objects)
-    }
-  }
-
-  render()
-}
+render()
 
 function render() {
   renderer.render(scene, camera)
 }
+
+/* EVENT */
+
+controls.addEventListener('drag', render)
