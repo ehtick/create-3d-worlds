c728c63764f7509f76dd17807dd110d816696578
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-09 00:25:27 +0200

    del

--- src/90-examples/model-lookat-cursor/main.js
+++ src/90-examples/model-lookat-cursor/main.js
@@ -4,10 +4,10 @@ import { scene, camera, renderer, clock } from '/utils/scene.js'
 import { initLights } from '/utils/light.js'
 import { createFloor } from '/utils/ground.js'
 
-let model, neck, waist, mixer, idle
+let model, neck, spine, mixer, idle
 
 initLights()
-camera.position.set(0, 7, 19)
+camera.position.set(0, 1.5, 3)
 
 const loader = new GLTFLoader()
 const texture = new THREE.TextureLoader().load('stacy.jpg')
@@ -20,7 +20,7 @@ const stacy_mtl = new THREE.MeshPhongMaterial({
 
 loader.load('stacy_lightweight.glb', gltf => {
   model = gltf.scene
-  const fileAnimations = gltf.animations
+  const { animations } = gltf
 
   model.traverse(o => {
     if (o.isMesh) {
@@ -31,15 +31,14 @@ loader.load('stacy_lightweight.glb', gltf => {
     if (o.isBone && o.name === 'mixamorigNeck')
       neck = o
     if (o.isBone && o.name === 'mixamorigSpine')
-      waist = o
+      spine = o
   })
 
-  model.scale.set(7, 7, 7)
   scene.add(model)
 
   mixer = new THREE.AnimationMixer(model)
-
-  const idleAnim = THREE.AnimationClip.findByName(fileAnimations, 'idle')
+  const idleAnim = THREE.AnimationClip.findByName(animations, 'idle')
+  console.log(idleAnim)
   idleAnim.tracks.splice(3, 3)
   idleAnim.tracks.splice(9, 3)
   idle = mixer.clipAction(idleAnim)
@@ -49,64 +48,47 @@ loader.load('stacy_lightweight.glb', gltf => {
 const floor = createFloor({ color: 0xeeeeee })
 scene.add(floor)
 
-document.addEventListener('mousemove', e => {
-  const mousecoords = getMousePos(e)
-  if (neck && waist) {
-    moveJoint(mousecoords, neck, 50)
-    moveJoint(mousecoords, waist, 30)
-  }
-})
+const getMousePos = e => ({ x: e.clientX, y: e.clientY })
 
-function getMousePos(e) {
-  return { x: e.clientX, y: e.clientY }
+function getMouseDegrees(x, y, degreeLimit) {
+  let dx = 0,
+    dy = 0
+
+  const screen = { x: window.innerWidth, y: window.innerHeight }
+  // 1. If cursor is in the left half of screen
+  if (x <= screen.x / 2) {
+    // 2. Get the difference between middle of screen and cursor position
+    const xdiff = screen.x / 2 - x
+    // 3. Find the percentage of that difference
+    const xPercentage = xdiff / (screen.x / 2) * 100
+    // 4. Convert that to a percentage of the maximum rotation we allow for the neck
+    dx = degreeLimit * xPercentage / 100 * -1
+  }
+
+  if (x >= screen.x / 2) {
+    const xdiff = x - screen.x / 2
+    const xPercentage = xdiff / (screen.x / 2) * 100
+    dx = degreeLimit * xPercentage / 100
+  }
+
+  if (y <= screen.y / 2) {
+    const ydiff = screen.y / 2 - y
+    const yPercentage = ydiff / (screen.y / 2) * 100
+    dy = degreeLimit * 0.5 * yPercentage / 100 * -1
+  }
+
+  if (y >= screen.y / 2) {
+    const ydiff = y - screen.y / 2
+    const yPercentage = ydiff / (screen.y / 2) * 100
+    dy = degreeLimit * yPercentage / 100
+  }
+  return { x: dx, y: dy }
 }
 
 function moveJoint(mouse, joint, degreeLimit) {
   const degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit)
   joint.rotation.y = THREE.Math.degToRad(degrees.x)
   joint.rotation.x = THREE.Math.degToRad(degrees.y)
-}
-
-function getMouseDegrees(x, y, degreeLimit) {
-  let dx = 0,
-    dy = 0,
-    xdiff,
-    xPercentage,
-    ydiff,
-    yPercentage
-
-  const w = { x: window.innerWidth, y: window.innerHeight }
-  // Left (Rotates neck left between 0 and -degreeLimit)
-  // 1. If cursor is in the left half of screen
-  if (x <= w.x / 2) {
-    // 2. Get the difference between middle of screen and cursor position
-    xdiff = w.x / 2 - x
-    // 3. Find the percentage of that difference (percentage toward edge of screen)
-    xPercentage = xdiff / (w.x / 2) * 100
-    // 4. Convert that to a percentage of the maximum rotation we allow for the neck
-    dx = degreeLimit * xPercentage / 100 * -1
-  }
-
-  // Right (Rotates neck right between 0 and degreeLimit)
-  if (x >= w.x / 2) {
-    xdiff = x - w.x / 2
-    xPercentage = xdiff / (w.x / 2) * 100
-    dx = degreeLimit * xPercentage / 100
-  }
-  // Up (Rotates neck up between 0 and -degreeLimit)
-  if (y <= w.y / 2) {
-    ydiff = w.y / 2 - y
-    yPercentage = ydiff / (w.y / 2) * 100
-    // Note that I cut degreeLimit in half when she looks up
-    dy = degreeLimit * 0.5 * yPercentage / 100 * -1
-  }
-  // Down (Rotates neck down between 0 and degreeLimit)
-  if (y >= w.y / 2) {
-    ydiff = y - w.y / 2
-    yPercentage = ydiff / (w.y / 2) * 100
-    dy = degreeLimit * yPercentage / 100
-  }
-  return { x: dx, y: dy }
 }
 
 /* LOOP */
@@ -116,3 +98,14 @@ void function update() {
   renderer.render(scene, camera)
   requestAnimationFrame(update)
 }()
+
+/* EVENTS */
+
+document.addEventListener('mousemove', e => {
+  const mousecoords = getMousePos(e)
+  if (neck && spine) {
+    moveJoint(mousecoords, neck, 60)
+    moveJoint(mousecoords, spine, 40)
+  }
+})
+
