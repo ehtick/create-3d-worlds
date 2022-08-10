e0af4a34f8d32259518eba6fe09e196375a04de6
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-10 10:31:25 +0200

    m

--- src/45-terrain/landscape/main.js
+++ src/45-terrain/landscape/main.js
@@ -1,108 +1,32 @@
-import {
-  Scene, PerspectiveCamera, WebGLRenderer, Fog, SpotLight, HemisphereLight,
-} from 'three'
-
+import { Fog } from 'three'
+import { scene, camera, renderer } from '/utils/scene.js'
 import Landscape from './Landscape.js'
 import { size, count, speed } from './config.js'
+import { initLights } from '/utils/light.js'
 
-const W = window.innerWidth
-const H = window.innerHeight
+scene.fog = new Fog(0x100820, size, size * 2)
 
-const canvas = document.getElementById('canvas')
-const dpr = window.devicePixelRatio
-const antialias = true
+initLights()
 
-class Sketch {
-  constructor() {
-    this.canvas = canvas
-    this.dpr = dpr
-    this.antialias = antialias
-    this.radId = null
-    this.init()
-  }
+camera.position.set(0, 1.8, 3)
 
-  createCamera() {
-    this.camera = new PerspectiveCamera(
-      50,
-      W / H,
-      .1,
-      size * (count - 2),
-    )
-    this.camera.position.set(0, 1.8, 3)
-  }
+const segments = []
+for (let i = 0; i < count; i++) {
+  const landscape = new Landscape(scene)
+  landscape.container.position.z = -1 * i * size
 
-  createLight() {
-    this.light = new SpotLight(0xD90FFF, .75)
-    this.light.penumbra = 1
-    this.light.angle = .5
-    this.light.position.set(0, 120, -size * 5 * count)
-
-    this.scene.add(
-      this.light,
-      new HemisphereLight(0xD90FFF, 0x1C1385, .25)
-    )
-  }
-
-  createSegments() {
-    this.segments = []
-    for (let i = 0; i < count; i++) {
-      const landscape = new Landscape(this.scene)
-      landscape.container.position.z = -1 * i * size
-
-      this.segments.push(landscape)
-      this.scene.add(landscape.container)
-    }
-  }
-
-  getContext() {
-    return this.canvas.getContext('webgl')
-  }
-
-  init() {
-    this.scene = new Scene()
-    this.scene.fog = new Fog(0x100820, size, size * 2)
-
-    this.scene.position.set(0, 0, 5)
-    this.scene.rotation.x = 0
-
-    this.createCamera()
-    this.createLight()
-    this.createSegments()
-
-    this.renderer = new WebGLRenderer({
-      canvas: this.canvas,
-      context: this.getContext(),
-      antialias: this.antialias,
-      alpha: true,
-      powerPreference: 'high-performance',
-    })
-    this.renderer.setSize(W, H)
-    this.renderer.setPixelRatio(this.dpr)
-  }
-
-  render() {
-    this.segments.forEach(({ container }) => {
-      if (container.position.z >= size)
-        container.position.z = -1 * size * (count - 1)
-      container.position.z += speed
-    })
-    this.renderer.render(this.scene, this.camera)
-  }
-
-  update() {
-    this.radId = requestAnimationFrame(() => this.update())
-    this.render()
-  }
-
-  start() {
-    this.update()
-  }
-
-  stop() {
-    cancelAnimationFrame(this.radId)
-  }
+  segments.push(landscape)
+  scene.add(landscape.container)
 }
 
-const sketch = new Sketch()
+/* LOOP */
 
-sketch.start()
+void function update() {
+  requestAnimationFrame(update)
+  segments.forEach(({ container }) => {
+    if (container.position.z >= size)
+      container.position.z = -1 * size * (count - 1)
+    container.position.z += speed
+  })
+  renderer.render(scene, camera)
+}()
