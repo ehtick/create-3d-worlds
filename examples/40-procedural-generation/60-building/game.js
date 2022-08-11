8d346a770b51a4c8abe4d0d55c742ff78f74f1d1
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-11 23:13:30 +0200

    del

--- examples/40-procedural-generation/60-building/game.js
+++ examples/40-procedural-generation/60-building/game.js
@@ -1,6 +1,9 @@
+import * as THREE from 'three'
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
+import { randomInRange } from '/utils/helpers.js'
 import { initLights, hemLight } from '/utils/light.js'
-import { createBuilding } from '/utils/city.js'
+// import { createBuilding } from '/utils/city.js'
+import { material } from '/utils/shaders/rectangles.js'
 
 initLights()
 hemLight()
@@ -8,6 +11,30 @@ hemLight()
 const controls = createOrbitControls()
 camera.position.set(0, 75, 75)
 renderer.setClearColor(0x070b34)
+
+const toonMaterial = new THREE.MeshToonMaterial({ color: 0x000000 })
+
+function createBuilding({
+  color = new THREE.Color(0x000000), bWidth = randomInRange(8, 20),
+  bHeight = randomInRange(bWidth * 2, bWidth * 4), rotY = 0,
+} = {}) {
+
+  const geometry = new THREE.BoxBufferGeometry(bWidth, bHeight, bWidth)
+
+  const colors = []
+  for (let i = 0, l = geometry.attributes.position.count; i < l; i ++)
+    colors.push(color.r, color.g, color.b)
+  geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
+
+  geometry.translate(0, bHeight * .5, 0)
+  if (rotY) geometry.rotateY(rotY)
+
+  const building = new THREE.Mesh(
+    geometry,
+    [material, material, toonMaterial, toonMaterial, material, material]
+  )
+  return building
+}
 
 scene.add(createBuilding())
 
--- utils/shaders/rectangles.js
+++ utils/shaders/rectangles.js
@@ -2,28 +2,23 @@ import * as THREE from 'three'
 
 const vertexShader = /* glsl */`
   varying vec2 vUv;
-  varying vec3 vPosition;
 
   void main() {	
     vUv = uv;
-    vPosition = position;
     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
   }
 `
 
 const fragmentShader = /* glsl */`
-  uniform float u_time;
   uniform vec3 u_color;
-
   varying vec2 vUv;
-  varying vec3 vPosition;
 
   float rect(vec2 pt, vec2 anchor, vec2 size, vec2 center){
     vec2 p = pt - center;
     vec2 halfsize = size/2.0;
-    float horz = step(-halfsize.x - anchor.x, p.x) - step(halfsize.x - anchor.x, p.x);
+    float horiz = step(-halfsize.x - anchor.x, p.x) - step(halfsize.x - anchor.x, p.x);
     float vert = step(-halfsize.y - anchor.y, p.y) - step(halfsize.y - anchor.y, p.y);
-    return horz*vert;
+    return horiz * vert;
   }
 
   mat2 getRotationMatrix(float theta){
@@ -38,21 +33,18 @@ const fragmentShader = /* glsl */`
 
   void main (void)
   {
-    float tilecount = 6.0;
+    float tilecount = 5.0;
     vec2 center = vec2(0.5);
-    vec2 pt = fract(vUv*tilecount) - center;
-    mat2 matr = getRotationMatrix(u_time);
-    mat2 mats = getScaleMatrix((sin(u_time)+1.0)/3.0 + 0.5);
-    pt = mats * matr * pt;
+    vec2 pt = fract(vUv * tilecount) - center;
     pt += center;
     vec3 color = u_color * rect(pt, vec2(0.0), vec2(0.3), center);
+
     gl_FragColor = vec4(color, 1.0); 
   }
 `
 
 export const uniforms = {
   u_color: { value: new THREE.Color(0xffff00) },
-  u_time: { value: 0.0 },
 }
 
 export const material = new THREE.ShaderMaterial({
