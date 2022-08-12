f69d2197b583151afc993de76e0f3b667a473b86
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-12 20:21:04 +0200

    m

--- examples/15-animation/22-terrain-waves/main.js
+++ examples/15-animation/22-terrain-waves/main.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three'
-import { scene, camera, renderer, clock, createOrbitControls } from '/utils/scene.js'
-import { randomInRange } from '/utils/helpers.js'
+import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
 import { waveTerrain } from '/utils/ground.js'
+import { SimplexNoise } from '/libs/SimplexNoise.js'
 
 camera.position.set(0, 20, 20)
 createOrbitControls()
+
+const simplex = new SimplexNoise()
 
 const geometry = new THREE.PlaneGeometry(100, 100, 100, 100)
 const material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true })
@@ -15,7 +17,8 @@ const { position } = geometry.attributes
 const vertex = new THREE.Vector3()
 for (let i = 0, l = position.count; i < l; i++) {
   vertex.fromBufferAttribute(position, i)
-  vertex.z += randomInRange(-1, 1)
+  const res = simplex.noise(vertex.x * .1, vertex.y * .1)
+  vertex.z = res * 1.5
   position.setXYZ(i, vertex.x, vertex.y, vertex.z)
 }
 
@@ -23,11 +26,13 @@ scene.add(mesh)
 
 /* LOOP */
 
+let time = 0
+
 void function render() {
   requestAnimationFrame(render)
 
-  const time = clock.getElapsedTime()
   waveTerrain(geometry, time)
+  time += .01
 
   renderer.render(scene, camera)
 }()
