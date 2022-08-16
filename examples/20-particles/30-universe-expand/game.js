8feca78359d9650b87d26c35816e2f137208502d
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-17 00:31:31 +0200

    pom

--- examples/20-particles/30-universe-expand/game.js
+++ examples/20-particles/30-universe-expand/game.js
@@ -3,12 +3,13 @@ import { TWEEN } from '/node_modules/three/examples/jsm/libs/tween.module.min.js
 import { scene, camera, renderer } from '/utils/scene.js'
 
 const { randInt, randFloat } = THREE.Math
+const textureLoader = new THREE.TextureLoader()
 
 camera.position.z = 750
 scene.background = new THREE.Color(0x000040)
 
 const material = new THREE.SpriteMaterial({
-  map: new THREE.CanvasTexture(generateTexture()),
+  map: textureLoader.load('/assets/particles/spark.png'),
   blending: THREE.AdditiveBlending
 })
 
@@ -20,25 +21,7 @@ for (let i = 0; i < 1000; i++) {
 
 /* FUNCTIONS */
 
-function generateTexture() {
-  const canvas = document.createElement('canvas')
-  canvas.width = canvas.height = 16
-  const halfSize = canvas.width / 2
-
-  const context = canvas.getContext('2d')
-  const gradient = context.createRadialGradient(halfSize, halfSize, 0, halfSize, halfSize, halfSize)
-  gradient.addColorStop(0, 'white')
-  gradient.addColorStop(0.2, 'white')
-  gradient.addColorStop(0.4, 'rgba(0,0,64,1)')
-  gradient.addColorStop(1, 'rgba(0,0,0,1)')
-  context.fillStyle = gradient
-  context.fillRect(0, 0, canvas.width, canvas.height)
-
-  return canvas
-}
-
 function tweenParticle(particle, delay = 0) {
-  particle.position.set(0, 0, 0)
   particle.scale.x = particle.scale.y = randFloat(16, 48)
 
   new TWEEN.Tween(particle.position)
@@ -48,20 +31,6 @@ function tweenParticle(particle, delay = 0) {
       y: randInt(-500, 500),
       z: randInt(-2000, 2000)
     }, 10000)
-    .start()
-
-  new TWEEN.Tween(particle.scale)
-    .delay(delay)
-    .to({
-      x: 0.01,
-      y: 0.01
-    }, 10000)
-    .start()
-
-  new TWEEN.Tween(particle)
-    .delay(delay)
-    .to({}, 10000)
-    .onComplete(() => tweenParticle(particle))
     .start()
 }
 
--- examples/20-particles/40-atom/game.js
+++ examples/20-particles/40-atom/game.js
@@ -1,17 +1,16 @@
 import * as THREE from 'three'
 import { scene, camera, renderer, clock, createOrbitControls } from '/utils/scene.js'
 
-const { randFloat } = THREE.Math
+const { randFloat, randFloatSpread } = THREE.Math
+const textureLoader = new THREE.TextureLoader()
+const controls = createOrbitControls()
 
-scene.background = new THREE.Color(0x000000)
-
-const totalParticles = 200
 const radiusRange = 50
 
-const controls = createOrbitControls()
 camera.position.set(0, 50, 150)
+scene.background = new THREE.Color(0x000000)
 
-const texture = new THREE.TextureLoader().load('images/spark.png')
+const texture = textureLoader.load('/assets/particles/spark.png')
 
 const particles = createParticles()
 scene.add(particles)
@@ -19,18 +18,18 @@ scene.add(particles)
 /* FUNCTIONS */
 
 function createParticles() {
-  const particles = new THREE.Object3D()
-  for (let i = 0; i < totalParticles; i++) {
+  const particles = new THREE.Group()
+  for (let i = 0; i < 200; i++) {
     const material = new THREE.SpriteMaterial({ map: texture, color: 0xffffff })
     const sprite = new THREE.Sprite(material)
     sprite.scale.set(32, 32, 1.0)
-    sprite.position.set(randFloat(-.5, .5), randFloat(-.5, .5), randFloat(-.5, .5))
+    sprite.position.set(randFloatSpread(10), randFloatSpread(10), randFloatSpread(10))
     sprite.position.setLength(radiusRange * randFloat(.9, 1))
-    sprite.material.color.setHSL(Math.random(), 0.9, 0.7)
-    sprite.material.blending = THREE.AdditiveBlending // glowing
+    material.color.setHSL(Math.random(), 0.9, 0.7)
+    material.blending = THREE.AdditiveBlending // glowing
 
     // assign custom properties
-    sprite.startPosition = sprite.position.clone()
+    sprite.initialPosition = sprite.position.clone()
     sprite.randomness = Math.random() + 1
 
     particles.add(sprite)
@@ -38,24 +37,24 @@ function createParticles() {
   return particles
 }
 
-function updateParticles() {
-  const time = 4 * clock.getElapsedTime()
+function updateParticles(time) {
   for (let i = 0; i < particles.children.length; i++) {
     const sprite = particles.children[i]
     // pulse away/towards center at individual rates
     const pulseFactor = Math.sin(sprite.randomness * time) * 0.1 + 0.9
-    sprite.position.x = sprite.startPosition.x * pulseFactor
-    sprite.position.y = sprite.startPosition.y * pulseFactor
-    sprite.position.z = sprite.startPosition.z * pulseFactor
+    sprite.position.x = sprite.initialPosition.x * pulseFactor
+    sprite.position.y = sprite.initialPosition.y * pulseFactor
+    sprite.position.z = sprite.initialPosition.z * pulseFactor
   }
   particles.rotation.y = time * 0.75
 }
 
 /* LOOP */
 
-void function animate() {
-  requestAnimationFrame(animate)
+void function loop() {
+  requestAnimationFrame(loop)
+  const time = 4 * clock.getElapsedTime()
   controls.update()
-  updateParticles()
+  updateParticles(time)
   renderer.render(scene, camera)
 }()
