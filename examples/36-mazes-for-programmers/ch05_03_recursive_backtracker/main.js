ebe895e0c4989fdd87d7579abe44f91c6a153604
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-18 17:25:25 +0200

    del

--- examples/36-mazes-for-programmers/ch05_03_recursive_backtracker/main.js
+++ examples/36-mazes-for-programmers/ch05_03_recursive_backtracker/main.js
@@ -1,8 +1,25 @@
 import Grid from '../mazes/Grid.js'
 import RecursiveBacktracker from '../mazes/algorithms/RecursiveBacktracker.js'
+import { scene, renderer, camera, createOrbitControls, hemLight } from '/utils/scene.js'
+import { createMazeMesh } from '/utils/mazes.js'
 
 const grid = new Grid(20)
 RecursiveBacktracker.on(grid)
 
-grid.init_distances()
-grid.draw()
+const matrix = grid.toMatrix()
+
+hemLight()
+
+camera.position.set(0, 7, 10)
+const controls = createOrbitControls()
+
+const maze = createMazeMesh({ matrix })
+scene.add(maze)
+
+/* LOOP */
+
+void function gameLoop() {
+  requestAnimationFrame(gameLoop)
+  controls.update()
+  renderer.render(scene, camera)
+}()
