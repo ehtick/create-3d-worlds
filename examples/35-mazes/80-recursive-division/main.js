0504aced2879dd29296eb37614e2a27bd5c5d530
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-24 00:28:54 +0200

    refact

--- examples/35-mazes/80-recursive-division/main.js
+++ examples/35-mazes/80-recursive-division/main.js
@@ -1,6 +1,6 @@
 import Grid from '/utils/mazes/Grid.js'
 import { scene, renderer, camera, createOrbitControls } from '/utils/scene.js'
-import RecursiveDivision from '/utils/mazes/algorithms/RecursiveDivision.js'
+import { recursiveDivision } from '/utils/mazes/algorithms.js'
 import { meshFromGrid } from '/utils/mazes.js'
 import { hemLight } from '/utils/light.js'
 
@@ -9,7 +9,7 @@ camera.position.set(0, 7, 10)
 const controls = createOrbitControls()
 
 const grid = new Grid(10)
-RecursiveDivision.on(grid)
+recursiveDivision(grid)
 
 const mesh = meshFromGrid(grid)
 scene.add(mesh)
--- utils/mazes/algorithms.js
+++ utils/mazes/algorithms.js
@@ -294,3 +294,49 @@ export function ellersMatrix(rows, columns) {
   ellers(grid)
   return grid.toMatrix()
 }
+
+/* RECURSIVE DIVISION */
+
+function divide(grid, row, column, height, width) {
+  if (height <= 1 || width <= 1 || height < 5 && width < 5 && Math.random() < 0.25) return
+
+  if (height > width)
+    divide_horizontally(grid, row, column, height, width)
+  else
+    divide_vertically(grid, row, column, height, width)
+}
+
+function divide_horizontally(grid, row, column, height, width) {
+  const divide_south_of = Math.floor(Math.random() * (height - 1))
+  const passage_at = Math.floor(Math.random() * width)
+
+  for (let x = 0; x < width; x += 1) {
+    if (passage_at == x) continue
+    const cell = grid.cell(row + divide_south_of, column + x)
+    cell.unlink(cell.south)
+  }
+
+  divide(grid, row, column, divide_south_of + 1, width)
+  divide(grid, row + divide_south_of + 1, column, height - divide_south_of - 1, width)
+}
+
+function divide_vertically(grid, row, column, height, width) {
+  const divide_east_of = Math.floor(Math.random() * (width - 1))
+  const passage_at = Math.floor(Math.random() * height)
+
+  for (let y = 0; y < height; y += 1) {
+    if (passage_at == y) continue
+    const cell = grid.cell(row + y, column + divide_east_of)
+    cell.unlink(cell.east)
+  }
+
+  divide(grid, row, column, height, divide_east_of + 1)
+  divide(grid, row, column + divide_east_of + 1, height, width - divide_east_of - 1)
+}
+
+export function recursiveDivision(grid) {
+  for (const cell of grid.each_cell())
+    cell.neighbors.forEach(c => cell.link(c, false)) // removing all interior walls
+
+  divide(grid, 0, 0, grid.rows, grid.columns)
+}
