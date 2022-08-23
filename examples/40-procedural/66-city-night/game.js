b202384df0739fea7c04ff4559722365ae173357
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-23 11:23:28 +0200

    m

--- examples/40-procedural/66-city-night/game.js
+++ examples/40-procedural/66-city-night/game.js
@@ -1,10 +1,10 @@
+import * as THREE from 'three'
 import { scene, camera, renderer, createOrbitControls, hemLight } from '/utils/scene.js'
 import { createFloor } from '/utils/ground.js'
-import { createLampposts, createCityLights } from '/utils/city.js'
-import { createCity } from '/utils/city.js'
+import { createLampposts, createCity, createCityLights } from '/utils/city.js'
 
 hemLight({ intensity: 1.25 })
-renderer.setClearColor(0x000000)
+scene.background = new THREE.Color(0x070b34)
 
 const numLampposts = 12 // max num of spotlights is 16
 const numCityLights = 16 - numLampposts
@@ -14,7 +14,6 @@ const numBuildings = 200
 
 camera.position.set(0, size * .3, size * .4)
 createOrbitControls()
-renderer.setClearColor(0x070b34)
 
 const floor = createFloor({ size: size * 1.1, color: 0x101018 })
 const lampposts = createLampposts({ size, numLampposts, circle: false })
