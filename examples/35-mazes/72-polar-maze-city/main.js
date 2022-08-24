bba74205f2569b975ac8ddd98d783dee23545c53
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-24 23:06:15 +0200

    m

--- examples/35-mazes/72-polar-maze-city/main.js
+++ examples/35-mazes/72-polar-maze-city/main.js
@@ -1,15 +1,17 @@
+import { OutlineEffect } from '/node_modules/three/examples/jsm/effects/OutlineEffect.js'
 import { polarMazeCity } from '/utils/mazes.js'
 import PolarGrid from '/utils/mazes/PolarGrid.js'
 import { recursiveBacktracker } from '/utils/mazes/algorithms.js'
-import { scene, renderer, camera, createOrbitControls } from '/utils/scene.js'
+import { scene, renderer, camera } from '/utils/scene.js'
 import { createSunLight, hemLight, ambLight } from '/utils/light.js'
-import { createGround, createHill } from '/utils/ground.js'
+import { createHill } from '/utils/ground.js'
 import Avatar from '/utils/classes/Avatar.js'
 
 const gridSize = 20
 const cellSize = 10
+const groundSize = gridSize * cellSize
 
-const hill = createHill(gridSize * cellSize * 2)
+const hill = createHill(groundSize * 2.05)
 scene.add(hill)
 
 hemLight({ intensity: .5 })
@@ -18,10 +20,7 @@ const sun = createSunLight()
 sun.position.set(50, 150, 200)
 scene.add(sun)
 
-// scene.add(createGround())
-
 // camera.position.set(0, 100, 150)
-// const controls = createOrbitControls()
 
 const grid = new PolarGrid(gridSize)
 recursiveBacktracker(grid)
@@ -30,12 +29,15 @@ const maze = polarMazeCity(grid)
 scene.add(maze)
 
 const player = new Avatar({ size: .5, scene, camera, solids: [maze, hill] })
-player.position.set(cellSize * .5, 0, -gridSize * cellSize)
+player.position.set(cellSize * .5, 0, -groundSize - cellSize)
+player.mesh.lookAt(0, 0, -groundSize * 2)
+
+const effect = new OutlineEffect(renderer, { defaultThickness: 0.003 })
 
 /* LOOP */
 
 void function gameLoop() {
   requestAnimationFrame(gameLoop)
   player.update()
-  renderer.render(scene, camera)
+  effect.render(scene, camera)
 }()
