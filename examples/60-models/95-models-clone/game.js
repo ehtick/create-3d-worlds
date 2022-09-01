6e56b89e578a3940d2fe071e3a3af30c5af5bb29
Damjan Pavlica <damjan.pavlica@origo.is>
2022-09-01 12:11:48 +0200

    clone models

--- examples/60-models/95-models-clone/game.js
+++ examples/60-models/95-models-clone/game.js
@@ -1,12 +1,15 @@
 import * as THREE from 'three'
-import { loadModel } from '/utils/loaders.js'
+import { FBXLoader } from '/node_modules/three/examples/jsm/loaders/FBXLoader.js'
+import * as SkeletonUtils from '/node_modules/three/examples/jsm/utils/SkeletonUtils.js'
+
+import { loadModel, loadFbxAnimations } from '/utils/loaders.js'
 import { scene, camera, renderer, clock, createOrbitControls } from '/utils/scene.js'
 import { randomInCircle } from '/utils/helpers.js'
 import { createGround } from '/utils/ground.js'
 import { createSun } from '/utils/light.js'
 
 const mixers = []
-
+const fbxLoader = new FBXLoader()
 const controls = createOrbitControls()
 camera.position.set(0, 20, 30)
 
@@ -14,6 +17,7 @@ const HORSES = 10
 const OGRES = 3
 const BIRDS = 10
 const HOUSES = 3
+const GIRLS = 4
 
 scene.add(createGround(), createSun())
 
@@ -36,6 +40,15 @@ function createMixer(mesh, animation) {
 
 const { mesh: towerModel } = await loadModel({ file: 'castle/wizard-isle/scene.gltf', size: 15 })
 scene.add(randomPos(towerModel))
+
+const girlModel = await fbxLoader.loadAsync('/assets/models/character/kachujin/Kachujin.fbx')
+const girlAnimations = await loadFbxAnimations(['Ginga', 'Au', 'Troca', 'Backflip'], 'character/kachujin/')
+for (let i = 0; i < GIRLS; i++) {
+  const girl = SkeletonUtils.clone(girlModel)
+  girl.scale.set(.01, .01, .01)
+  createMixer(girl, girlAnimations[i])
+  scene.add(randomPos(girl))
+}
 
 const { mesh: houseModel } = await loadModel({ file: 'building/medieval-house/house1-01.obj', mtl: 'building/medieval-house/house1-01.mtl' })
 for (let i = 0; i < HOUSES; i++) {
