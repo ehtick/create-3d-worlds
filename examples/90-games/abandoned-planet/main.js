56d2cd4aaebf9e0e2b02afb014f8cc6035f85ddb
Damjan Pavlica <damjan.pavlica@origo.is>
2022-09-08 16:16:33 +0200

    m

--- examples/90-games/abandoned-planet/main.js
+++ examples/90-games/abandoned-planet/main.js
@@ -1,6 +1,7 @@
 /* global THREE, CANNON */
 import CannonHelper from './CannonHelper.js'
 import JoyStick from './JoyStick.js'
+import { fromUrl } from './utils.js'
 
 const { randInt } = THREE.MathUtils
 const clock = new THREE.Clock()
@@ -86,61 +87,10 @@ loader.load('https://raw.githubusercontent.com/baronwatts/models/master/astronau
 
 // ===================================================== add Terrain
 const sizeX = 128, sizeY = 128, minHeight = 0, maxHeight = 60
-const img2matrix = function() {
-  return {
-    fromImage,
-    fromUrl
-  }
 
-  function fromImage(image, width, depth, minHeight, maxHeight) {
-    width |= 0
-    depth |= 0
-
-    let i, j
-    const matrix = []
-    const canvas = document.createElement('canvas'),
-      ctx = canvas.getContext('2d')
-    let imgData, pixel, channels = 4
-    const heightRange = maxHeight - minHeight
-    let heightData
-
-    canvas.width = width
-    canvas.height = depth
-
-    ctx.drawImage(image, 0, 0, width, depth)
-    imgData = ctx.getImageData(0, 0, width, depth).data
-
-    for (i = 0 | 0; i < depth; i = (i + 1) | 0) { // row
-      matrix.push([])
-
-      for (j = 0 | 0; j < width; j = (j + 1) | 0) { // col
-        pixel = i * depth + j
-        heightData = imgData[pixel * channels] / 255 * heightRange + minHeight
-        matrix[i].push(heightData)
-      }
-    }
-    return matrix
-  }
-
-  function fromUrl(url, width, depth, minHeight, maxHeight) {
-    return function() {
-      return new Promise((onFulfilled, onRejected) => {
-        const image = new Image()
-        image.crossOrigin = 'anonymous'
-        image.onload = function() {
-          const matrix = fromImage(image, width, depth, minHeight, maxHeight)
-          onFulfilled(matrix)
-        }
-        image.src = url
-      })
-    }
-  }
-}()
-
-// can add an array if things
 let check
 Promise.all([
-  img2matrix.fromUrl('https://upload.wikimedia.org/wikipedia/commons/5/57/Heightmap.png', sizeX, sizeY, minHeight, maxHeight)(),
+  fromUrl('/assets/heightmaps/wiki.png', sizeX, sizeY, minHeight, maxHeight)(),
 ]).then(data => {
   const matrix = data[0]
 
