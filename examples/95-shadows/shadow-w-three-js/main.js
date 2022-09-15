81de25aec33006925a16f85cd0fb6663c24b9b70
Damjan Pavlica <damjan.pavlica@origo.is>
2022-09-15 17:42:01 +0200

    m

--- examples/95-shadows/shadow-w-three-js/main.js
+++ examples/95-shadows/shadow-w-three-js/main.js
@@ -1,8 +1,7 @@
 import * as THREE from 'three'
 import { scene, renderer, camera, createOrbitControls } from '/utils/scene.js'
+import { createGround } from '/utils/ground.js'
 import { createBox } from '/utils/geometry.js'
-
-const { degToRad } = THREE.MathUtils
 
 const shadowLightRadius = 8
 
@@ -33,40 +32,30 @@ function createShadowLight() {
 
 const dirLight = createShadowLight()
 
-function addBlock(radius, degree) {
-  const cube = createBox({ height: 3, color: 'white', castShadow: true, receiveShadow: true })
+const radius = 5
 
-  cube.position.x = Math.cos(degToRad(degree)) * radius
-  cube.position.z = Math.sin(degToRad(degree)) * radius
+for (let degree = 0; degree <= 2 * Math.PI; degree += Math.PI / 6) {
+  const cube = createBox({ height: 3, color: 'white', castShadow: true, receiveShadow: true })
+  cube.position.x = Math.cos(degree) * radius
+  cube.position.z = Math.sin(degree) * radius
   cube.position.y = 0
-  cube.rotation.y = degToRad(-degree)
+  cube.rotation.y = -degree
   scene.add(cube)
 }
 
-for (let degree = 0; degree <= 360; degree += 30)
-  addBlock(5, degree)
-
-const planeSize = 20
-const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize)
-const planeMat = new THREE.MeshLambertMaterial({
-  color: 'lightgray',
-})
-const plane = new THREE.Mesh(planeGeo, planeMat)
-plane.receiveShadow = true
-plane.rotation.x = degToRad(-90)
+const plane = createGround({ size: 20 })
 scene.add(plane)
 
 /* LOOP */
 
-let shadowLightAngle = 0
+let lightAngle = 0
 
 void function loop(time) {
   time *= 0.001
-  shadowLightAngle += 0.3
-  const shadowLightPositionX = Math.cos(degToRad(shadowLightAngle)) * shadowLightRadius
-  const shadowLightPositionZ = Math.sin(degToRad(shadowLightAngle)) * shadowLightRadius
-  // senka je dobra kad je svetlo nisko
-  dirLight.position.set(shadowLightPositionX, 3, shadowLightPositionZ)
+  lightAngle += .003
+  const x = Math.cos(lightAngle) * shadowLightRadius
+  const z = Math.sin(lightAngle) * shadowLightRadius
+  dirLight.position.set(x, 3, z)
 
   renderer.render(scene, camera)
   requestAnimationFrame(loop)
