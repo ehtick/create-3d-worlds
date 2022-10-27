c80ee66bfb8aa4f0d8f37cf931746adfa5a95fe4
Damjan Pavlica <damjan.pavlica@origo.is>
2022-10-27 22:03:31 +0200

    m

--- examples/50-physics/race-game/vehicle.js
+++ examples/50-physics/race-game/vehicle.js
@@ -1,16 +1,16 @@
 /* global Ammo */
 
-const suspensionStiffness = [58.0, 45.0]
+const suspensionStiffness = 50
 const suspensionDamping = 4
 const suspensionCompression = 2.4
 const maxSuspensionTravelCm = 1500.0
 const maxSuspensionForce = 50000.0
-const CUBE_HALF_EXTENTS = [.96, 1.12]
-const suspensionRestLength = [1.1, 1.05]
+const CUBE_HALF_EXTENTS = 1
+const suspensionRestLength = 1.3
 const connectionHeight = 1.2
 
-const wheelRadius = [.36, .42]
-const wheelWidth = [.2, .5]
+const wheelRadius = .39
+const wheelWidth = .35
 const frictionSlip = 3.5
 const rearWheelFriction = 4.5
 
@@ -22,7 +22,7 @@ export function makeVehicle(c, physicsWorld, bodies) {
   const m_tuning = new Ammo.btVehicleTuning()
   const wheelAxleCS = new Ammo.btVector3(-1, 0, 0)
   const wheelDirectionCS0 = new Ammo.btVector3(0, -1, 0)
-  m_tuning.set_m_suspensionStiffness(suspensionStiffness[c])
+  m_tuning.set_m_suspensionStiffness(suspensionStiffness)
   m_tuning.set_m_suspensionCompression(suspensionCompression)
   m_tuning.set_m_suspensionDamping(suspensionDamping)
   m_tuning.set_m_maxSuspensionTravelCm(maxSuspensionTravelCm)
@@ -41,37 +41,37 @@ export function makeVehicle(c, physicsWorld, bodies) {
   let isFrontWheel = true
   const connectionPointCS0 = new Ammo.btVector3(0, 0, 0)
 
-  connectionPointCS0.setValue(CUBE_HALF_EXTENTS[c] - (0.3 * wheelWidth[c]), connectionHeight, 2 * CUBE_HALF_EXTENTS[c] - wheelRadius[c])
+  connectionPointCS0.setValue(CUBE_HALF_EXTENTS - (0.3 * wheelWidth), connectionHeight, 2 * CUBE_HALF_EXTENTS - wheelRadius)
 
-  vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength[c], wheelRadius[c], m_tuning, isFrontWheel)
+  vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength, wheelRadius, m_tuning, isFrontWheel)
 
-  connectionPointCS0.setValue(-CUBE_HALF_EXTENTS[c] + (0.3 * wheelWidth[c]), connectionHeight, 2 * CUBE_HALF_EXTENTS[c] - wheelRadius[c])
+  connectionPointCS0.setValue(-CUBE_HALF_EXTENTS + (0.3 * wheelWidth), connectionHeight, 2 * CUBE_HALF_EXTENTS - wheelRadius)
 
-  vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength[c], wheelRadius[c], m_tuning, isFrontWheel)
+  vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength, wheelRadius, m_tuning, isFrontWheel)
 
   isFrontWheel = true // for all wheel drive?
 
   m_tuning.set_m_frictionSlip(rearWheelFriction)
 
-  connectionPointCS0.setValue(-CUBE_HALF_EXTENTS[c] + (0.3 * wheelWidth[c]), connectionHeight, -2 * CUBE_HALF_EXTENTS[c] + wheelRadius[c])
+  connectionPointCS0.setValue(-CUBE_HALF_EXTENTS + (0.3 * wheelWidth), connectionHeight, -2 * CUBE_HALF_EXTENTS + wheelRadius)
 
-  vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength[c], wheelRadius[c], m_tuning, isFrontWheel)
+  vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength, wheelRadius, m_tuning, isFrontWheel)
 
-  connectionPointCS0.setValue(CUBE_HALF_EXTENTS[c] - (0.3 * wheelWidth[c]), connectionHeight, -2 * CUBE_HALF_EXTENTS[c] + wheelRadius[c])
+  connectionPointCS0.setValue(CUBE_HALF_EXTENTS - (0.3 * wheelWidth), connectionHeight, -2 * CUBE_HALF_EXTENTS + wheelRadius)
 
-  vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength[c], wheelRadius[c], m_tuning, isFrontWheel)
+  vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength, wheelRadius, m_tuning, isFrontWheel)
 
   // these last two of the six total wheels are for rendering
   // m_tuning.set_m_frictionSlip(rearWheelFriction)
 
   // isFrontWheel = true
-  // connectionPointCS0.setValue(-CUBE_HALF_EXTENTS[c] + (0.3 * wheelWidth[c]), connectionHeight, -2 * CUBE_HALF_EXTENTS[c] + wheelRadius[c])
+  // connectionPointCS0.setValue(-CUBE_HALF_EXTENTS + (0.3 * wheelWidth), connectionHeight, -2 * CUBE_HALF_EXTENTS + wheelRadius)
 
-  // vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength[c], wheelRadius[c], m_tuning, isFrontWheel)
+  // vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength, wheelRadius, m_tuning, isFrontWheel)
 
-  // connectionPointCS0.setValue(CUBE_HALF_EXTENTS[c] - (0.3 * wheelWidth[c]), connectionHeight, -2 * CUBE_HALF_EXTENTS[c] + wheelRadius[c])
+  // connectionPointCS0.setValue(CUBE_HALF_EXTENTS - (0.3 * wheelWidth), connectionHeight, -2 * CUBE_HALF_EXTENTS + wheelRadius)
 
-  // vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength[c], wheelRadius[c], m_tuning, isFrontWheel)
+  // vehicle.addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS, suspensionRestLength, wheelRadius, m_tuning, isFrontWheel)
 
   return vehicle
 }
