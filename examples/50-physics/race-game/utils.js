fe716cec92b114aa49daa325a74b99335395b6d3
Damjan Pavlica <damjan.pavlica@origo.is>
2022-10-30 12:45:03 +0100

    m

--- examples/50-physics/race-game/utils.js
+++ examples/50-physics/race-game/utils.js
@@ -30,6 +30,8 @@ function fixAngleRad(a) {
 }
 
 export function leaveDecals(tireMesh, worldModel, body, tires, scene) {
+  if (!tireMesh || !worldModel.children[0]) return
+
   const velocity = new THREE.Vector3(0, 0, 0)
   const dec = new Ammo.btVector3(0, 0, 0)
   const dec2 = new Ammo.btVector3(0, 0, 0)
@@ -39,77 +41,75 @@ export function leaveDecals(tireMesh, worldModel, body, tires, scene) {
   const r_d = new THREE.Euler(0, 0, 0, 'XYZ')
   const s_d = new THREE.Vector3(90, 90, 90)
 
-  if (tireMesh && worldModel.children[0]) {
-    const wheelRot = body.getWorldTransform().getBasis()
-    dec.setValue(-.2, 0, .2)
-    dec2.setValue(
-      wheelRot.getRow(0).x() * dec.x() + wheelRot.getRow(0).y() * dec.y() + wheelRot.getRow(0).z() * dec.z(),
-      wheelRot.getRow(1).x() * dec.x() + wheelRot.getRow(1).y() * dec.y() + wheelRot.getRow(1).z() * dec.z(),
-      wheelRot.getRow(2).x() * dec.x() + wheelRot.getRow(2).y() * dec.y() + wheelRot.getRow(2).z() * dec.z()
-    )
-    dec3.setValue(
-      dec2.x() + tireMesh.position.x,
-      dec2.y() + tireMesh.position.y,
-      dec2.z() + tireMesh.position.z
-    )
+  const wheelRot = body.getWorldTransform().getBasis()
+  dec.setValue(-.2, 0, .2)
+  dec2.setValue(
+    wheelRot.getRow(0).x() * dec.x() + wheelRot.getRow(0).y() * dec.y() + wheelRot.getRow(0).z() * dec.z(),
+    wheelRot.getRow(1).x() * dec.x() + wheelRot.getRow(1).y() * dec.y() + wheelRot.getRow(1).z() * dec.z(),
+    wheelRot.getRow(2).x() * dec.x() + wheelRot.getRow(2).y() * dec.y() + wheelRot.getRow(2).z() * dec.z()
+  )
+  dec3.setValue(
+    dec2.x() + tireMesh.position.x,
+    dec2.y() + tireMesh.position.y,
+    dec2.z() + tireMesh.position.z
+  )
 
-    p_d.set(dec3.x(), dec3.y(), dec3.z())
+  p_d.set(dec3.x(), dec3.y(), dec3.z())
 
-    velocity.x = p_d.x - oldCarPos.x
-    velocity.y = p_d.y - oldCarPos.y
-    velocity.z = p_d.z - oldCarPos.z
+  velocity.x = p_d.x - oldCarPos.x
+  velocity.y = p_d.y - oldCarPos.y
+  velocity.z = p_d.z - oldCarPos.z
 
-    oldCarPos.x = p_d.x
-    oldCarPos.y = p_d.y
-    oldCarPos.z = p_d.z
-    // angle from velocity
-    decRot = -fixAngleRad(Math.atan2(velocity.z, velocity.x) + Math.PI / 2)
+  oldCarPos.x = p_d.x
+  oldCarPos.y = p_d.y
+  oldCarPos.z = p_d.z
+  // angle from velocity
+  decRot = -fixAngleRad(Math.atan2(velocity.z, velocity.x) + Math.PI / 2)
 
-    r_d.set(0, decRot, 0)
-    if (velocity.length() > 2) {
-      velocity.x = 0
-      velocity.y = 0
-      velocity.z = 0
-    }
-    s_d.set(1, 1, velocity.length())
-    const material_d = decalMaterial.clone()
-    let md = new THREE.Mesh(new DecalGeometry(worldModel.children[0], p_d, r_d, s_d), material_d)
-    decals.push(md)
-    scene.add(md)
-
-    dec.setValue(.2, 0, .2)
-    dec2.setValue(
-      wheelRot.getRow(0).x() * dec.x() + wheelRot.getRow(0).y() * dec.y() + wheelRot.getRow(0).z() * dec.z(),
-      wheelRot.getRow(1).x() * dec.x() + wheelRot.getRow(1).y() * dec.y() + wheelRot.getRow(1).z() * dec.z(),
-      wheelRot.getRow(2).x() * dec.x() + wheelRot.getRow(2).y() * dec.y() + wheelRot.getRow(2).z() * dec.z()
-    )
-    dec3.setValue(
-      dec2.x() + tires[2].position.x,
-      dec2.y() + tires[2].position.y,
-      dec2.z() + tires[2].position.z
-    )
-    p_d.set(dec3.x(), dec3.y(), dec3.z())
-
-    velocity.x = p_d.x - oldCarPos2.x
-    velocity.y = p_d.y - oldCarPos2.y
-    velocity.z = p_d.z - oldCarPos2.z
-
-    oldCarPos2.x = p_d.x
-    oldCarPos2.y = p_d.y
-    oldCarPos2.z = p_d.z
-
-    decRot = -fixAngleRad(Math.atan2(velocity.z, velocity.x) + Math.PI / 2)
-    r_d.set(0, decRot, 0)
-
-    if (velocity.length() > 2) {
-      velocity.x = 0; velocity.y = 0; velocity.z = 0
-    }
-    s_d.set(1, 1, velocity.length())
-
-    md = new THREE.Mesh(new DecalGeometry(worldModel.children[0], p_d, r_d, s_d), material_d)
-    decals.push(md)
-    scene.add(md)
+  r_d.set(0, decRot, 0)
+  if (velocity.length() > 2) {
+    velocity.x = 0
+    velocity.y = 0
+    velocity.z = 0
   }
+  s_d.set(1, 1, velocity.length())
+  const material_d = decalMaterial.clone()
+  let md = new THREE.Mesh(new DecalGeometry(worldModel.children[0], p_d, r_d, s_d), material_d)
+  decals.push(md)
+  scene.add(md)
+
+  dec.setValue(.2, 0, .2)
+  dec2.setValue(
+    wheelRot.getRow(0).x() * dec.x() + wheelRot.getRow(0).y() * dec.y() + wheelRot.getRow(0).z() * dec.z(),
+    wheelRot.getRow(1).x() * dec.x() + wheelRot.getRow(1).y() * dec.y() + wheelRot.getRow(1).z() * dec.z(),
+    wheelRot.getRow(2).x() * dec.x() + wheelRot.getRow(2).y() * dec.y() + wheelRot.getRow(2).z() * dec.z()
+  )
+  dec3.setValue(
+    dec2.x() + tires[2].position.x,
+    dec2.y() + tires[2].position.y,
+    dec2.z() + tires[2].position.z
+  )
+  p_d.set(dec3.x(), dec3.y(), dec3.z())
+
+  velocity.x = p_d.x - oldCarPos2.x
+  velocity.y = p_d.y - oldCarPos2.y
+  velocity.z = p_d.z - oldCarPos2.z
+
+  oldCarPos2.x = p_d.x
+  oldCarPos2.y = p_d.y
+  oldCarPos2.z = p_d.z
+
+  decRot = -fixAngleRad(Math.atan2(velocity.z, velocity.x) + Math.PI / 2)
+  r_d.set(0, decRot, 0)
+
+  if (velocity.length() > 2) {
+    velocity.x = 0; velocity.y = 0; velocity.z = 0
+  }
+  s_d.set(1, 1, velocity.length())
+
+  md = new THREE.Mesh(new DecalGeometry(worldModel.children[0], p_d, r_d, s_d), material_d)
+  decals.push(md)
+  scene.add(md)
 }
 
 export function fadeDecals(scene) {
