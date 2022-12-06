334542a6bd67b941fb89990c300d362907a2105e
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-06 10:54:21 +0100

    pomalo

--- examples/50-physics/35-ball-shoot-wall/main.js
+++ examples/50-physics/35-ball-shoot-wall/main.js
@@ -3,6 +3,12 @@ import { AMMO, createBox, createBall, createWall, createPhysicsWorld, updateMesh
 import { scene, camera, renderer, clock, createOrbitControls } from '/utils/scene.js'
 import { createSun } from '/utils/light.js'
 import { normalizeMouse } from '/utils/helpers.js'
+import keyboard from '/utils/classes/Keyboard.js'
+
+const magnitude = document.getElementById('magnitude')
+const minMagnitude = 15
+const maxMagnitude = 30
+magnitude.value = minMagnitude
 
 createOrbitControls()
 camera.position.set(-7, 1.5, 0)
@@ -37,6 +43,9 @@ function addRigidBody(mesh) {
 
 void function loop() {
   requestAnimationFrame(loop)
+  if (keyboard.pressed.mouse && magnitude.value < maxMagnitude)
+    magnitude.value = parseFloat(magnitude.value) + .2
+
   const dt = clock.getDelta()
   physicsWorld.stepSimulation(dt, 10)
   rigidBodies.forEach(updateMesh)
@@ -47,12 +56,13 @@ void function loop() {
 
 const raycaster = new THREE.Raycaster()
 
-window.addEventListener('pointerdown', e => {
+window.addEventListener('pointerup', e => {
   const mouse = normalizeMouse(e)
   raycaster.setFromCamera(mouse, camera)
   const pos = new THREE.Vector3().copy(raycaster.ray.direction).add(raycaster.ray.origin)
   const ball = createBall({ radius: 0.4, mass: 5, pos })
   addRigidBody(ball)
-  pos.copy(raycaster.ray.direction).multiplyScalar(24)
+  pos.copy(raycaster.ray.direction).multiplyScalar(magnitude.value)
   ball.userData.body.setLinearVelocity(new AMMO.btVector3(pos.x, pos.y, pos.z))
+  magnitude.value = minMagnitude
 })
