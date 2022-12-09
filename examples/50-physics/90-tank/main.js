0bcc4b12a64778dff6cff2c5e8b510e4dda5f62f
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-09 14:38:10 +0100

    m

--- examples/50-physics/90-tank/main.js
+++ examples/50-physics/90-tank/main.js
@@ -2,9 +2,10 @@ import * as THREE from 'three'
 import { scene, camera, renderer, clock } from '/utils/scene.js'
 import { createSun } from '/utils/light.js'
 import { fadeDecals } from './decals.js'
-import { Car } from './Car.js'
 import { handleInput, updateTires } from './update-vehicle.js'
 import { createPhysicsWorld, updateMesh, chaseCam, createBox, findGround } from '/utils/physics.js'
+import { loadModel } from '/utils/loaders.js'
+import { makeVehicle } from './vehicle.js'
 
 scene.add(createSun({ position: [10, 50, 0] }))
 
@@ -13,6 +14,23 @@ const physicsWorld = createPhysicsWorld()
 const ground = createBox({ pos: new THREE.Vector3(0, -0.5, 0), width: 100, height: 1, depth: 100, friction: 2, color: 0x509f53 })
 scene.add(ground)
 physicsWorld.addRigidBody(ground.userData.body)
+
+class Car {
+  constructor() {
+    return (async() => {
+      const { mesh } = await loadModel({ file: 'racing/hummer.obj', mtl: 'racing/hummer.mtl', scale: .57 })
+      const { mesh: tireMesh } = await loadModel({ file: 'racing/hummerTire.obj', mtl: 'racing/hummerTire.mtl', scale: .57 })
+      const { vehicle, body } = makeVehicle(physicsWorld)
+
+      mesh.userData.body = body
+      this.mesh = mesh
+      this.vehicle = vehicle
+      this.tires = [...Array(4)].map(() => tireMesh.clone())
+
+      return this
+    })()
+  }
+}
 
 const car = await new Car({ objFile: 'hummer', tireFile: 'hummerTire', physicsWorld })
 scene.add(car.mesh, ...car.tires)
