6108d9b3848677b39618cbaa753ecc494e9183ae
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-14 13:04:38 +0100

    del sky

--- examples/50-physics/75-vehicle-jerome/main.js
+++ examples/50-physics/75-vehicle-jerome/main.js
@@ -232,38 +232,35 @@ function buildVehicleSkinVeyron(opt, onReady) {
     wheels: []
   }
 
-  const textureCube = new THREE.CubeTextureLoader()
-    .setPath('textures/')
-    .load(['posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg'])
   const s = 0.02
 
   const mlib = {
 
-    'Orange': new THREE.MeshLambertMaterial({ color: 0xff6600, envMap: textureCube, combine: THREE.MixOperation, reflectivity: 0.3 }),
-    'Blue': new THREE.MeshLambertMaterial({ color: 0x001133, envMap: textureCube, combine: THREE.MixOperation, reflectivity: 0.3 }),
-    'Red': new THREE.MeshLambertMaterial({ color: 0x660000, envMap: textureCube, combine: THREE.MixOperation, reflectivity: 0.25 }),
-    'Black': new THREE.MeshLambertMaterial({ color: 0x000000, envMap: textureCube, combine: THREE.MixOperation, reflectivity: 0.15 }),
-    'White': new THREE.MeshLambertMaterial({ color: 0xffffff, envMap: textureCube, combine: THREE.MixOperation, reflectivity: 0.25 }),
+    'Orange': new THREE.MeshLambertMaterial({ color: 0xff6600, combine: THREE.MixOperation, reflectivity: 0.3 }),
+    'Blue': new THREE.MeshLambertMaterial({ color: 0x001133, combine: THREE.MixOperation, reflectivity: 0.3 }),
+    'Red': new THREE.MeshLambertMaterial({ color: 0x660000, combine: THREE.MixOperation, reflectivity: 0.25 }),
+    'Black': new THREE.MeshLambertMaterial({ color: 0x000000, combine: THREE.MixOperation, reflectivity: 0.15 }),
+    'White': new THREE.MeshLambertMaterial({ color: 0xffffff, combine: THREE.MixOperation, reflectivity: 0.25 }),
 
-    'Carmine': new THREE.MeshPhongMaterial({ color: 0x770000, specular: 0xffaaaa, envMap: textureCube, combine: THREE.MultiplyOperation }),
-    'Gold': new THREE.MeshPhongMaterial({ color: 0xaa9944, specular: 0xbbaa99, shininess: 50, envMap: textureCube, combine: THREE.MultiplyOperation }),
-    'Bronze': new THREE.MeshPhongMaterial({ color: 0x150505, specular: 0xee6600, shininess: 10, envMap: textureCube, combine: THREE.MixOperation, reflectivity: 0.25 }),
-    'Chrome': new THREE.MeshPhongMaterial({ color: 0xffffff, specular: 0xffffff, envMap: textureCube, combine: THREE.MultiplyOperation }),
+    'Carmine': new THREE.MeshPhongMaterial({ color: 0x770000, specular: 0xffaaaa, combine: THREE.MultiplyOperation }),
+    'Gold': new THREE.MeshPhongMaterial({ color: 0xaa9944, specular: 0xbbaa99, shininess: 50, combine: THREE.MultiplyOperation }),
+    'Bronze': new THREE.MeshPhongMaterial({ color: 0x150505, specular: 0xee6600, shininess: 10, combine: THREE.MixOperation, reflectivity: 0.25 }),
+    'Chrome': new THREE.MeshPhongMaterial({ color: 0xffffff, specular: 0xffffff, combine: THREE.MultiplyOperation }),
 
-    'Orange metal': new THREE.MeshLambertMaterial({ color: 0xff6600, envMap: textureCube, combine: THREE.MultiplyOperation }),
-    'Blue metal': new THREE.MeshLambertMaterial({ color: 0x001133, envMap: textureCube, combine: THREE.MultiplyOperation }),
-    'Red metal': new THREE.MeshLambertMaterial({ color: 0x770000, envMap: textureCube, combine: THREE.MultiplyOperation }),
-    'Green metal': new THREE.MeshLambertMaterial({ color: 0x007711, envMap: textureCube, combine: THREE.MultiplyOperation }),
-    'Black metal': new THREE.MeshLambertMaterial({ color: 0x222222, envMap: textureCube, combine: THREE.MultiplyOperation }),
+    'Orange metal': new THREE.MeshLambertMaterial({ color: 0xff6600, combine: THREE.MultiplyOperation }),
+    'Blue metal': new THREE.MeshLambertMaterial({ color: 0x001133, combine: THREE.MultiplyOperation }),
+    'Red metal': new THREE.MeshLambertMaterial({ color: 0x770000, combine: THREE.MultiplyOperation }),
+    'Green metal': new THREE.MeshLambertMaterial({ color: 0x007711, combine: THREE.MultiplyOperation }),
+    'Black metal': new THREE.MeshLambertMaterial({ color: 0x222222, combine: THREE.MultiplyOperation }),
 
-    'Pure chrome': new THREE.MeshLambertMaterial({ color: 0xffffff, envMap: textureCube }),
-    'Dark chrome': new THREE.MeshLambertMaterial({ color: 0x444444, envMap: textureCube }),
-    'Darker chrome': new THREE.MeshLambertMaterial({ color: 0x222222, envMap: textureCube }),
+    'Pure chrome': new THREE.MeshLambertMaterial({ color: 0xffffff }),
+    'Dark chrome': new THREE.MeshLambertMaterial({ color: 0x444444 }),
+    'Darker chrome': new THREE.MeshLambertMaterial({ color: 0x222222 }),
 
-    'Black glass': new THREE.MeshLambertMaterial({ color: 0x101016, envMap: textureCube, opacity: 0.975, transparent: true }),
-    'Dark glass': new THREE.MeshLambertMaterial({ color: 0x101046, envMap: textureCube, opacity: 0.25, transparent: true }),
-    'Blue glass': new THREE.MeshLambertMaterial({ color: 0x668899, envMap: textureCube, opacity: 0.75, transparent: true }),
-    'Light glass': new THREE.MeshBasicMaterial({ color: 0x223344, envMap: textureCube, opacity: 0.25, transparent: true, combine: THREE.MixOperation, reflectivity: 0.25 }),
+    'Black glass': new THREE.MeshLambertMaterial({ color: 0x101016, opacity: 0.975, transparent: true }),
+    'Dark glass': new THREE.MeshLambertMaterial({ color: 0x101046, opacity: 0.25, transparent: true }),
+    'Blue glass': new THREE.MeshLambertMaterial({ color: 0x668899, opacity: 0.75, transparent: true }),
+    'Light glass': new THREE.MeshBasicMaterial({ color: 0x223344, opacity: 0.25, transparent: true, combine: THREE.MixOperation, reflectivity: 0.25 }),
 
     'Red glass': new THREE.MeshLambertMaterial({ color: 0xff0000, opacity: 0.75, transparent: true }),
     'Yellow glass': new THREE.MeshLambertMaterial({ color: 0xffffaa, opacity: 0.75, transparent: true }),
@@ -321,7 +318,6 @@ function buildVehicleSkinVeyron(opt, onReady) {
       wheelMaterial.materials[0] = new THREE.MeshLambertMaterial({
         color: 0xffffff,
         reflectivity: 0.75,
-        envMap: textureCube,
         combine: THREE.MixOperation,
       })
       wheelMaterial.materials[1] = new THREE.MeshLambertMaterial({
