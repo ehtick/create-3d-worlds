5ff7189b5a7825bbb437bbfa7c3cfc6989c63312
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-14 16:21:23 +0100

    lint

--- examples/50-physics/75-vehicle-jerome/libs/threex-ammoterrain.js
+++ examples/50-physics/75-vehicle-jerome/libs/threex-ammoterrain.js
@@ -1,192 +1,125 @@
-"use strict";
+'use strict'
 
 var THREEx = THREEx || {}
 
-THREEx.AmmoTerrain = function( terrainWidth, terrainDepth, terrainMinHeight, terrainMaxHeight, terrain3dWidth, terrain3dDepth){
-	var _this = this
-	
-	var heightData = generateHeightRocket( terrainWidth, terrainDepth, terrainMinHeight, terrainMaxHeight );
-	//////////////////////////////////////////////////////////////////////////////
-	//		Code Separator
-	//////////////////////////////////////////////////////////////////////////////
-        var geometry = new THREE.PlaneBufferGeometry( terrain3dWidth, terrain3dDepth, terrainWidth - 1, terrainDepth - 1 );
-        geometry.rotateX( -Math.PI / 2 );
-        var vertices = geometry.attributes.position.array;        
-        for ( var i = 0; i < vertices.length; i++ ) {
-                // j + 1 because it is the y component that we modify
-                vertices[ i*3 + 1 ] = heightData[ i ];                        
+THREEx.AmmoTerrain = function(terrainWidth, terrainDepth, terrainMinHeight, terrainMaxHeight, terrain3dWidth, terrain3dDepth) {
+  const _this = this
+
+  const heightData = generateHeightRocket(terrainWidth, terrainDepth, terrainMinHeight, terrainMaxHeight)
+
+  const geometry = new THREE.PlaneBufferGeometry(terrain3dWidth, terrain3dDepth, terrainWidth - 1, terrainDepth - 1)
+  geometry.rotateX(-Math.PI / 2)
+  const vertices = geometry.attributes.position.array
+  for (let i = 0; i < vertices.length; i++)
+  // j + 1 because it is the y component that we modify
+    vertices[i * 3 + 1] = heightData[i]
+
+  geometry.computeVertexNormals()
+  const material = new THREE.MeshLambertMaterial({
+    map: new THREE.TextureLoader().load('textures/grid.png', texture => {
+      texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+      texture.repeat.set(terrain3dWidth, terrain3dDepth)
+    }),
+    side: THREE.DoubleSide
+  })
+
+  const meshGround = new THREE.Mesh(geometry, material)
+  meshGround.receiveShadow = true
+
+  const groundShape = createTerrainShape(heightData)
+  const groundTransform = new Ammo.btTransform()
+  groundTransform.setIdentity()
+  // Shifts the terrain, since bullet re-centers it on its bounding box.
+  groundTransform.setOrigin(new Ammo.btVector3(0, (terrainMaxHeight + terrainMinHeight) / 2, 0))
+  const groundMass = 0
+  const groundLocalInertia = new Ammo.btVector3(0, 0, 0)
+  const groundMotionState = new Ammo.btDefaultMotionState(groundTransform)
+  const groundBody = new Ammo.btRigidBody(new Ammo.btRigidBodyConstructionInfo(groundMass, groundMotionState, groundShape, groundLocalInertia))
+
+  _this.object3d = meshGround
+  _this.body = groundBody
+
+  return
+
+  function createTerrainShape(heightData) {
+    // This parameter is not really used, since we are using PHY_FLOAT height data type and hence it is ignored
+    const heightScale = 1
+    // Up axis = 0 for X, 1 for Y, 2 for Z. Normally 1 = Y is used.
+    const upAxis = 1
+    // hdt, height data type. "PHY_FLOAT" is used. Possible values are "PHY_FLOAT", "PHY_UCHAR", "PHY_SHORT"
+    const hdt = 'PHY_FLOAT'
+    // Set this to your needs (inverts the triangles)
+    const flipQuadEdges = false
+    // Creates height data buffer in Ammo heap
+    const ammoHeightData = Ammo._malloc(4 * terrainWidth * terrainDepth)
+    // Copy the javascript height data array to the Ammo one.
+    let p = 0
+    let p2 = 0
+    for (let j = 0; j < terrainDepth; j++)
+      for (let i = 0; i < terrainWidth; i++) {
+        // write 32-bit float data to memory
+        Ammo.HEAPF32[ammoHeightData + p2 >> 2] = heightData[p]
+        p++
+        // 4 bytes/float
+        p2 += 4
+      }
+
+    // Creates the heightfield physics shape
+    const heightFieldShape = new Ammo.btHeightfieldTerrainShape(
+      terrainWidth,
+      terrainDepth,
+      ammoHeightData,
+      heightScale,
+      terrainMinHeight,
+      terrainMaxHeight,
+      upAxis,
+      hdt,
+      flipQuadEdges
+    )
+    // Set horizontal scale
+    const scaleX = terrain3dWidth / (terrainWidth - 1)
+    const scaleZ = terrain3dDepth / (terrainDepth - 1)
+    heightFieldShape.setLocalScaling(new Ammo.btVector3(scaleX, 1, scaleZ))
+    heightFieldShape.setMargin(0.05)
+    return heightFieldShape
+  }
+
+  function generateHeightRocket(width, depth, minHeight) {
+    const data = new Float32Array(width * depth)
+    const radiusX = 24 * 2
+    const radiusZ = 24 * 2
+    const radiusY = radiusX / 4
+
+    for (let index = 0, z = 0; z < depth; z++)
+      for (let x = 0; x < width; x++) {
+        let height = minHeight
+
+        if (x > width - radiusX) {
+          var delta = x - (width - radiusX)
+          var angle = Math.acos(delta / radiusX)
+          height = Math.max(height, minHeight - Math.sin(angle) * radiusY + radiusY)
         }
-        geometry.computeVertexNormals();
-        var material = new THREE.MeshLambertMaterial({
-		map: new THREE.TextureLoader().load("textures/grid.png", function onLoad(texture){
-			texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-			texture.repeat.set(terrain3dWidth,terrain3dDepth)
-			texture.anisotropy = renderer.getMaxAnisotropy()
-		}),
-		side : THREE.DoubleSide
-	});        
-        // var material = new THREE.MeshPhongMaterial( { color: 0xC7C7C7 } );
-        var meshGround = new THREE.Mesh( geometry, material );
-        meshGround.receiveShadow = true;
-
-	//////////////////////////////////////////////////////////////////////////////
-	//		Code Separator
-	//////////////////////////////////////////////////////////////////////////////
-        var groundShape = createTerrainShape( heightData );
-        var groundTransform = new Ammo.btTransform();
-        groundTransform.setIdentity();
-        // Shifts the terrain, since bullet re-centers it on its bounding box.
-        groundTransform.setOrigin( new Ammo.btVector3( 0, ( terrainMaxHeight + terrainMinHeight ) / 2, 0 ) );
-        var groundMass = 0;
-        var groundLocalInertia = new Ammo.btVector3( 0, 0, 0 );
-        var groundMotionState = new Ammo.btDefaultMotionState( groundTransform );
-        var groundBody = new Ammo.btRigidBody( new Ammo.btRigidBodyConstructionInfo( groundMass, groundMotionState, groundShape, groundLocalInertia ) );
-        // ammoWorld.physicsWorld.addRigidBody( groundBody );
-
-
-	_this.object3d = meshGround
-	_this.body = groundBody
-
-	// THREEx.AmmoControls.call( this, meshGround, {
-	// 	shape: groundShape,
-	// 	mass: 0,
-	// });
-	return
-	
-	//////////////////////////////////////////////////////////////////////////////
-	//		Code Separator
-	//////////////////////////////////////////////////////////////////////////////
-        function createTerrainShape(heightData) {
-                
-                // This parameter is not really used, since we are using PHY_FLOAT height data type and hence it is ignored
-                var heightScale = 1;
-                
-                // Up axis = 0 for X, 1 for Y, 2 for Z. Normally 1 = Y is used.
-                var upAxis = 1;
-                
-                // hdt, height data type. "PHY_FLOAT" is used. Possible values are "PHY_FLOAT", "PHY_UCHAR", "PHY_SHORT"
-                var hdt = "PHY_FLOAT";
-                
-                // Set this to your needs (inverts the triangles)
-                var flipQuadEdges = false;
-                
-                // Creates height data buffer in Ammo heap
-                var ammoHeightData = Ammo._malloc(4 * terrainWidth * terrainDepth);
-                
-                // Copy the javascript height data array to the Ammo one.
-                var p = 0;
-                var p2 = 0;
-                for ( var j = 0; j < terrainDepth; j++ ) {
-                        for ( var i = 0; i < terrainWidth; i++ ) {
-                                
-                                // write 32-bit float data to memory
-                                Ammo.HEAPF32[ ammoHeightData + p2 >> 2 ] = heightData[ p ];
-                                
-                                p++;
-                                
-                                // 4 bytes/float
-                                p2 += 4;
-                        }
-                }
-                
-                // Creates the heightfield physics shape
-                var heightFieldShape = new Ammo.btHeightfieldTerrainShape(
-                        terrainWidth,
-                        terrainDepth,
-                        ammoHeightData,
-                        heightScale,
-                        terrainMinHeight,
-                        terrainMaxHeight,
-                        upAxis,
-                        hdt,
-                        flipQuadEdges
-                );
-                
-                // Set horizontal scale
-                var scaleX = terrain3dWidth / ( terrainWidth - 1 );
-                var scaleZ = terrain3dDepth / ( terrainDepth - 1 );
-                heightFieldShape.setLocalScaling( new Ammo.btVector3( scaleX, 1, scaleZ ) );
-                
-                heightFieldShape.setMargin( 0.05 );
-                
-                return heightFieldShape;
-                
+        if (x < radiusX) {
+          var delta = radiusX - x
+          var angle = Math.acos(delta / radiusX)
+          height = Math.max(height, minHeight - Math.sin(angle) * radiusY + radiusY)
         }
-	
-	//////////////////////////////////////////////////////////////////////////////
-	//		Code Separator
-	//////////////////////////////////////////////////////////////////////////////
-        function generateHeightSinus( width, depth, minHeight, maxHeight ) {
-                var data = new Float32Array(width * depth);
-                
-                var heightRange = maxHeight - minHeight;
-                var halfWidth = width / 2;
-                var halfDepth = depth / 2;
-                var phaseMult = 12;
-                
-                for (var index = 0, z = 0; z < depth; z++ ) {
-                        for ( var x = 0; x < width; x++ ) {
-                                var radius = Math.sqrt(
-                                        Math.pow( ( x - halfWidth ) / halfWidth, 2.0 ) +
-                                        Math.pow( ( z - halfDepth ) / halfDepth, 2.0 )
-                                );
-                                
-                                var height = ( Math.sin( radius * phaseMult ) + 1 ) * 0.5 * heightRange + minHeight;
-                                data[ index++ ] = height;
-                        }
-                }
-                return data;
-        }	
-        function generateHeightFlat( width, depth, minHeight, maxHeight ) {
-                var data = new Float32Array(width * depth);
-                
-                for(var index = 0, z = 0; z < depth; z++ ) {
-                        for ( var x = 0; x < width; x++ ) {
-				var height = minHeight
-				
-                                data[ index++ ] = height;
-                        }
-                }
-                return data;
-        }		
-        function generateHeightRocket( width, depth, minHeight, maxHeight ) {
-                var data = new Float32Array(width * depth);
-		var radiusX = 24*2
-		var radiusZ = 24*2
-		var radiusY = radiusX/4
-		
-                for(var index = 0, z = 0; z < depth; z++ ) {
-                        for ( var x = 0; x < width; x++ ) {
-				var height = minHeight
-				
-				if( x > width-radiusX ){
-					var delta = x - (width-radiusX)
-					var angle = Math.acos(delta/radiusX)
-					height = Math.max(height, minHeight - Math.sin(angle)*radiusY + radiusY)
-				}
-				if( x < radiusX ){
-					var delta = radiusX - x
-					var angle = Math.acos(delta/radiusX)
-					height = Math.max(height, minHeight - Math.sin(angle)*radiusY + radiusY)
-				}
-				if( z > depth-radiusZ ){
-					var delta = z - (depth-radiusZ)
-					var angle = Math.acos(delta/radiusZ)
-					height = Math.max(height, minHeight - Math.sin(angle)*radiusY + radiusY)
-				}
-				if( z < radiusZ ){
-					var delta = radiusZ - z
-					var angle = Math.acos(delta/radiusZ)
-					height = Math.max(height, minHeight - Math.sin(angle)*radiusY + radiusY)
-				}
-				
-                                data[ index++ ] = height;
-                        }
-                }
-                return data;
-        }		
+        if (z > depth - radiusZ) {
+          var delta = z - (depth - radiusZ)
+          var angle = Math.acos(delta / radiusZ)
+          height = Math.max(height, minHeight - Math.sin(angle) * radiusY + radiusY)
+        }
+        if (z < radiusZ) {
+          var delta = radiusZ - z
+          var angle = Math.acos(delta / radiusZ)
+          height = Math.max(height, minHeight - Math.sin(angle) * radiusY + radiusY)
+        }
+        data[index++] = height
+      }
 
+    return data
+  }
 }
 
-THREEx.AmmoTerrain.prototype = Object.create( THREEx.AmmoControls.prototype );
-THREEx.AmmoTerrain.prototype.constructor = THREEx.AmmoControls;
+THREEx.AmmoTerrain.prototype = Object.create(THREEx.AmmoControls.prototype)
+THREEx.AmmoTerrain.prototype.constructor = THREEx.AmmoControls
--- examples/50-physics/75-vehicle-jerome/main.js
+++ examples/50-physics/75-vehicle-jerome/main.js
@@ -1,5 +1,47 @@
 /* global THREE */
 
+/* UTILS */
+
+const AmmoVehicleControls = function(object3d) {
+  this.object3d = object3d
+
+  this.offsetCamera = new THREE.Vector3(0, 2, -6).setLength(10)
+  this.lookatCamera = new THREE.Vector3(0, 0, +4)
+
+  this.tweenOffset = 0.1
+  this.tweenLookAt = 0.1
+
+  this._currentOffset = null
+  this._currentLookat = null
+}
+
+AmmoVehicleControls.prototype.update = function(ammoVehicle) {
+  const object3dVehicle = ammoVehicle.object3d.getObjectByName('chassis')
+
+  const offsetCamera = this.offsetCamera.clone()
+  object3dVehicle.localToWorld(offsetCamera)
+
+  if (this._currentOffset === null)
+    this._currentOffset = offsetCamera.clone()
+	 else
+    this._currentOffset.multiplyScalar(1 - this.tweenOffset)
+      .add(offsetCamera.clone().multiplyScalar(this.tweenOffset))
+
+  this.object3d.position.copy(this._currentOffset)
+
+  const lookatCamera = this.lookatCamera.clone()
+  object3dVehicle.localToWorld(lookatCamera)
+  if (this._currentLookat === null)
+    this._currentLookat = lookatCamera.clone()
+	 else
+    this._currentLookat.multiplyScalar(1 - this.tweenLookAt)
+      .add(lookatCamera.clone().multiplyScalar(this.tweenLookAt))
+
+  this.object3d.lookAt(this._currentLookat)
+}
+
+/* INIT */
+
 const renderer = new THREE.WebGLRenderer()
 renderer.setClearColor(new THREE.Color('black'), 1)
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -10,7 +52,7 @@ const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerH
 camera.position.z = 10
 
 const speedometer = document.getElementById('speedometer')
-const cameraControls = new THREEx.AmmoVehicleControls(camera)
+const cameraControls = new AmmoVehicleControls(camera)
 const ammoWorld = new THREEx.AmmoWorld()
 
 let light = new THREE.AmbientLight(0x202020)
