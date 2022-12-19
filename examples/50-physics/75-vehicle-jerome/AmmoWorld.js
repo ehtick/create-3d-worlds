e617ce0feb5da897391e7ff1d5c78842f64f4d40
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-19 13:24:03 +0100

    m

--- examples/50-physics/75-vehicle-jerome/AmmoWorld.js
+++ examples/50-physics/75-vehicle-jerome/AmmoWorld.js
@@ -8,19 +8,13 @@ export default class AmmoWorld {
     this.physicsWorld = createPhysicsWorld()
   }
 
+  add(ammoBody) {
+    this.physicsWorld.addRigidBody(ammoBody.mesh.userData.body)
+    this.ammoBodies.push(ammoBody.mesh)
+  }
+
   update() {
     this.physicsWorld.stepSimulation(clock.getDelta(), this.maxSteps)
-    this.ammoBodies.forEach(ammoBody => updateMesh(ammoBody.mesh))
-  }
-
-  add(ammoBody) {
-    this.physicsWorld.addRigidBody(ammoBody.body)
-    this.ammoBodies.push(ammoBody)
-  }
-
-  remove(ammoBody) {
-    if (!this.ammoBodies.contains(ammoBody)) return
-    this.physicsWorld.removeRigidBody(ammoBody.physicsWorld)
-    this.ammoBodies.splice(this.ammoBodies.indexOf(ammoBody), 1)
+    this.ammoBodies.forEach(updateMesh)
   }
 }
