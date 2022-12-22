7cf66c1580aa8ab886f1f6ec6d63009a2c15508e
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-22 10:25:57 +0100

    m

--- examples/50-physics/35-ball-shoot-wall/main.js
+++ examples/50-physics/35-ball-shoot-wall/main.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three'
-import { Ammo, createBox, createBall, createWall, createSideWall, createPhysicsWorld, updateMesh } from '/utils/physics.js'
+import { Ammo, createBox, createBall, createWall, createSideWall } from '/utils/physics.js'
 import { scene, camera, renderer, clock, createOrbitControls } from '/utils/scene.js'
 import { createSun } from '/utils/light.js'
 import { normalizeMouse } from '/utils/helpers.js'
 import keyboard from '/utils/classes/Keyboard.js'
+import PhysicsWorld from '/utils/classes/PhysicsWorld.js'
+
+const world = new PhysicsWorld()
 
 const impulse = document.getElementById('impulse')
 const minImpulse = impulse.value = 15
@@ -13,15 +16,11 @@ createOrbitControls()
 camera.position.set(-10, 1.5, 0)
 camera.lookAt(10, 0, 0)
 
-const rigidBodies = []
-
 const sun = createSun({ position: [-5, 10, 5] })
 scene.add(sun)
 
-const physicsWorld = createPhysicsWorld()
-
 const ground = createBox({ width: 40, height: 1, depth: 40, mass: 0, pos: { x: 0, y: -0.5, z: 0 }, color: 0xFFFFFF })
-addRigidBody(ground)
+world.add(ground, 0)
 
 const frontWall = createWall({ rows: 10, columns: 6, brickMass: 5, friction: 5, startX: -3.2 })
 const backWall = createWall({ rows: 10, columns: 6, brickMass: 5, friction: 5, startX: 2.2 })
@@ -29,18 +28,8 @@ const leftWall = createSideWall({ rows: 10, columns: 6, brickMass: 5, friction: 
 const rightWall = createSideWall({ rows: 10, columns: 6, brickMass: 5, friction: 5, startZ: 2.8 })
 
 ;[...frontWall, ...backWall, ...leftWall, ...rightWall].forEach(mesh => {
-  scene.add(mesh)
-  rigidBodies.push(mesh)
-  physicsWorld.addRigidBody(mesh.userData.body)
+  world.add(mesh)
 })
-
-/* FUNCTIONS */
-
-function addRigidBody(mesh) {
-  scene.add(mesh)
-  rigidBodies.push(mesh)
-  physicsWorld.addRigidBody(mesh.userData.body)
-}
 
 /* LOOP */
 
@@ -48,10 +37,8 @@ void function loop() {
   requestAnimationFrame(loop)
   if (keyboard.pressed.mouse && impulse.value < maxImpulse)
     impulse.value = parseFloat(impulse.value) + .2
-
   const dt = clock.getDelta()
-  physicsWorld.stepSimulation(dt, 10)
-  rigidBodies.forEach(updateMesh)
+  world.update(dt)
   renderer.render(scene, camera)
 }()
 
@@ -64,7 +51,7 @@ window.addEventListener('pointerup', e => {
   raycaster.setFromCamera(mouse, camera)
   const pos = new THREE.Vector3().copy(raycaster.ray.direction).add(raycaster.ray.origin)
   const ball = createBall({ radius: .4, mass: 5, pos })
-  addRigidBody(ball)
+  world.add(ball)
   pos.copy(raycaster.ray.direction).multiplyScalar(impulse.value)
   ball.userData.body.setLinearVelocity(new Ammo.btVector3(pos.x, pos.y, pos.z))
   impulse.value = minImpulse
