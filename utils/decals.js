a84842c3f8fc458905b4f69514c0f9980ce196e8
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-22 18:37:40 +0100

    m

--- utils/decals.js
+++ utils/decals.js
@@ -32,12 +32,7 @@ function fixAngleRad(a) {
 }
 
 export function leaveDecals({ ground, vehicle, body, wheelMeshes, scene }) {
-  const accelerating = keyboard.up || keyboard.down
-  const kmh = vehicle.getCurrentSpeedKmHour()
-
-  // accelerating && Math.abs(kmh) > 20
-  // vehicle.getWheelInfo(2).get_m_skidInfo() > 0
-  if (!keyboard.left && !keyboard.right) return
+  if (!keyboard.left && !keyboard.right || vehicle.getCurrentSpeedKmHour() < 30) return
 
   const groundMesh = ground?.children?.length ? ground.children[0] : ground
   const velocity = new THREE.Vector3(0, 0, 0)
