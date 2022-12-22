372910848d97e3ef9f06b6bcab457b1f07982857
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-22 11:18:18 +0100

    reuse

--- examples/50-physics/60-wave-terrain/main.js
+++ examples/50-physics/60-wave-terrain/main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { scene, camera, renderer, clock, createOrbitControls } from '/utils/scene.js'
 import { dirLight } from '/utils/light.js'
 import { generateSineWaveData, createTerrainFromData } from '/utils/ground.js'
-import { Ammo, createTerrainBodyFromData } from '/utils/physics.js'
+import { createTerrainBodyFromData } from '/utils/physics.js'
 import PhysicsWorld from '/utils/classes/PhysicsWorld.js'
 
 const world = new PhysicsWorld()
@@ -28,14 +28,8 @@ world.add(terrain, 0)
 /* FUNCTIONS */
 
 function generateObject() {
-  const numTypes = 3
-  const objectType = Math.ceil(Math.random() * numTypes)
-
-  let mesh = null
-  let shape = null
-
+  const randNum = Math.ceil(Math.random() * 3)
   const objectSize = 3
-  const margin = 0.05
 
   const rand1 = 1 + Math.random() * objectSize
   const rand2 = 1 + Math.random() * objectSize
@@ -44,38 +38,20 @@ function generateObject() {
   const color = Math.floor(Math.random() * (1 << 24))
   const material = new THREE.MeshPhongMaterial({ color })
 
-  switch (objectType) {
+  let mesh = null
+  switch (randNum) {
     case 1:
       mesh = new THREE.Mesh(new THREE.SphereGeometry(rand1, 20, 20), material)
-      shape = new Ammo.btSphereShape(rand1)
-      shape.setMargin(margin)
       break
     case 2:
       mesh = new THREE.Mesh(new THREE.BoxGeometry(rand1, rand2, rand3, 1, 1, 1), material)
-      shape = new Ammo.btBoxShape(new Ammo.btVector3(rand1 * 0.5, rand2 * 0.5, rand3 * 0.5))
-      shape.setMargin(margin)
       break
     case 3:
       mesh = new THREE.Mesh(new THREE.CylinderGeometry(rand1, rand1, rand2, 20, 1), material)
-      shape = new Ammo.btCylinderShape(new Ammo.btVector3(rand1, rand2 * 0.5, rand1))
-      shape.setMargin(margin)
       break
   }
 
   mesh.position.set((Math.random() - 0.5) * width * 0.6, maxHeight + objectSize + 2, (Math.random() - 0.5) * depth * 0.6)
-
-  const mass = objectSize * 5
-  const inertia = new Ammo.btVector3(0, 0, 0)
-  shape.calculateLocalInertia(mass, inertia)
-  const transform = new Ammo.btTransform()
-  transform.setOrigin(new Ammo.btVector3(...mesh.position))
-  const motionState = new Ammo.btDefaultMotionState(transform)
-  const rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, shape, inertia)
-  const body = new Ammo.btRigidBody(rbInfo)
-
-  mesh.userData.body = body
-  mesh.receiveShadow = mesh.castShadow = true
-
   world.add(mesh)
 }
 
