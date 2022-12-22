faa6944e45cd8d45325af0dd6d763fa20f88dc4a
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-22 01:36:33 +0100

    m

--- examples/50-physics/30-cannon-shoot-wall/main.js
+++ examples/50-physics/30-cannon-shoot-wall/main.js
@@ -1,17 +1,18 @@
 import * as THREE from 'three'
-import { Ammo, createGround, createBall, createWall } from '/utils/physics.js'
+import { Ammo, createWall } from '/utils/physics.js'
 import { scene, camera, renderer, clock } from '/utils/scene.js'
 import keyboard from '/utils/classes/Keyboard.js'
 import { createSun } from '/utils/light.js'
 import { loadModel } from '/utils/loaders.js'
 import PhysicsWorld from '/utils/classes/PhysicsWorld.js'
+import { createGround } from '/utils/ground.js'
+import { createSphere } from '/utils/geometry.js'
 
 const world = new PhysicsWorld()
 
-const minMagnitude = 15
-const maxMagnitude = 30
-const magnitude = document.getElementById('magnitude')
-magnitude.value = minMagnitude
+const impulse = document.getElementById('impulse')
+const minImpulse = impulse.value = 15
+const maxImpulse = 30
 
 camera.position.set(-2, 1.25, 0)
 camera.lookAt(10, 1, 0)
@@ -20,50 +21,49 @@ const sun = createSun({ position: [-5, 10, 5] })
 scene.add(sun)
 
 const ground = createGround({ size: 40, color: 0x509f53 })
-world.add(ground)
+world.add(ground, 0)
 
 const wall = createWall({ brickMass: 3, friction: 5 })
 wall.forEach(mesh => world.add(mesh))
 
 const { mesh: cannon } = await loadModel({ file: 'weapon/cannon/mortar/mortar.obj', mtl: 'weapon/cannon/mortar/mortar.mtl', size: 1, angle: Math.PI, shouldAdjustHeight: true })
 cannon.translateX(-5)
-// cannon.rotation.reorder('YZX') // 'YZX', 'ZXY', 'XZY', 'YXZ' and 'ZYX'.
 world.add(cannon)
 cannon.add(camera)
 
 /* FUNCTIONS */
 
 function shoot() {
+  const angle = cannon.rotation.y + Math.PI * .5
+  const x = impulse.value * Math.sin(angle)
+  const z = impulse.value * Math.cos(angle)
+
+  const distance = .7
+  const cannonTop = new THREE.Vector3(distance * Math.sin(angle), 0, distance * Math.cos(angle))
+
   const pos = cannon.position.clone()
   pos.y += 0.9
+  pos.add(cannonTop)
 
-  const angle = cannon.rotation.y + Math.PI * .5
-  const x = magnitude.value * Math.sin(angle)
-  const z = magnitude.value * Math.cos(angle)
+  const ball = createSphere({ r: .22, color: 0x202020 })
+  ball.position.copy(pos)
+  world.add(ball, 4)
 
-  const ballDistance = .7
-  const b = new THREE.Vector3(ballDistance * Math.sin(angle), 0, ballDistance * Math.cos(angle))
-  pos.add(b)
-  const ball = createBall({ radius: .22, mass: 4, pos })
-  world.add(ball)
-  ball.userData.body.setLinearVelocity(new Ammo.btVector3(x, magnitude.value * .2, z))
-  magnitude.value = minMagnitude
-  cannon.translateX(-.1)
+  ball.userData.body.setLinearVelocity(new Ammo.btVector3(x, impulse.value * .2, z))
+  cannon.userData.body.applyImpulse(new Ammo.btVector3(-1, 0, 0))
+  impulse.value = minImpulse
 }
 
-function handleInput(cannon, dt) {
-  // if (keyboard.up) cannon.translateX(dt * .5)
-  // if (keyboard.down) cannon.translateX(-dt * .5)
-  // if (keyboard.left) cannon.rotateY(dt * .25)
-  // if (keyboard.right) cannon.rotateY(-dt * .25)
+function handleInput(cannon) {
   const { body } = cannon.userData
+
   if (keyboard.up) body.applyImpulse(new Ammo.btVector3(.1, 0, 0))
   if (keyboard.down) body.applyImpulse(new Ammo.btVector3(-.1, 0, 0))
   if (keyboard.left) body.setAngularVelocity(new Ammo.btVector3(0, .5, 0))
   if (keyboard.right) body.setAngularVelocity(new Ammo.btVector3(0, -.5, 0))
 
-  if ((keyboard.space || keyboard.pressed.mouse) && magnitude.value < maxMagnitude)
-    magnitude.value = parseFloat(magnitude.value) + .2
+  if ((keyboard.space || keyboard.pressed.mouse) && impulse.value < maxImpulse)
+    impulse.value = parseFloat(impulse.value) + .2
 }
 
 /* LOOP */
@@ -71,7 +71,7 @@ function handleInput(cannon, dt) {
 void function loop() {
   requestAnimationFrame(loop)
   const dt = clock.getDelta()
-  handleInput(cannon, dt)
+  handleInput(cannon)
   world.update(dt)
   renderer.render(scene, camera)
 }()
