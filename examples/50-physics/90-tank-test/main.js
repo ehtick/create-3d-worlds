a608598f76590b800f73870a9ae28a6d6c8186a2
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-01 23:58:18 +0100

    fix model

--- examples/50-physics/90-tank-test/main.js
+++ examples/50-physics/90-tank-test/main.js
@@ -1,33 +1,24 @@
 import { scene, camera, renderer, clock } from '/utils/scene.js'
-import PhysicsWorld from '/utils/classes/PhysicsWorld.js'
-import Vehicle from '/utils/classes/Vehicle.js'
 import { loadModel } from '/utils/loaders.js'
-import { createGround } from '/utils/ground.js'
-import VehicleCamera from '/utils/classes/VehicleCamera.js'
 import { createSun } from '/utils/light.js'
-
-const world = new PhysicsWorld()
-const cameraControls = new VehicleCamera({ camera })
+import { createGround } from '/utils/ground.js'
 
 scene.add(createSun())
-world.add(createGround({ color: 0x509f53 }), 0)
+scene.add(createGround({ color: 0x509f53 }))
+
+camera.position.set(0, 2, 4)
 
 /* VEHICLE */
 
-const { mesh: chassisMesh } = await loadModel({ file: 'tank/steampunk/model.fbx', angle: Math.PI })
-chassisMesh.position.set(0, 2, 0)
+const { mesh } = await loadModel({ file: 'tank/a7v/model.fbx' })
+// mesh.position.set(0, 20, 0)
 
-const tank = new Vehicle({ physicsWorld: world.physicsWorld, chassisMesh })
-
-scene.add(chassisMesh)
+scene.add(mesh)
 
 /* LOOP */
 
 void function loop() {
   requestAnimationFrame(loop)
   const dt = clock.getDelta()
-  tank.update()
-  world.update(dt)
-  cameraControls.update(chassisMesh)
   renderer.render(scene, camera)
 }()
