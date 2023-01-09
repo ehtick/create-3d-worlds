c384d29935ac2d2fd23dcc42dca4276a74571ab1
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-09 12:52:51 +0100

    buildSimpleCastle

--- examples/50-physics/80-vehicle-tank/main.js
+++ examples/50-physics/80-vehicle-tank/main.js
@@ -4,17 +4,15 @@ import PhysicsWorld from '/utils/classes/PhysicsWorld.js'
 import Vehicle from '/utils/classes/Vehicle.js'
 import { loadModel } from '/utils/loaders.js'
 import { createGround } from '/utils/ground.js'
-import { createJumpBoard, createCrates, createWall, buildSimpleCastle } from '/utils/geometry.js'
+import { createJumpBoard, createCrates, buildSimpleCastle } from '/utils/geometry.js'
 import VehicleCamera from '/utils/classes/VehicleCamera.js'
 import { createSun } from '/utils/light.js'
 import { addTexture } from '/utils/helpers.js'
 
+const tankX = -20
+
 const world = new PhysicsWorld()
-const cameraControls = new VehicleCamera({
-  camera,
-  offsetCamera: new THREE.Vector3(0, 2, -6),
-  lookatCamera: new THREE.Vector3(0, 2, 4),
-})
+const cameraControls = new VehicleCamera({ camera, offsetCamera: new THREE.Vector3(0, 2, -6), lookatCamera: new THREE.Vector3(0, 2, 4) })
 
 scene.add(createSun())
 
@@ -22,13 +20,12 @@ const ground = createGround({ color: 0x509f53 })
 world.add(ground, 0)
 
 const jumpBoard = createJumpBoard()
+jumpBoard.position.x = tankX
 world.add(jumpBoard, 0)
 
-createCrates({ z: 10 }).forEach(mesh => world.add(mesh))
+createCrates({ x: tankX, z: 10 }).forEach(mesh => world.add(mesh))
 
-// createWall({ rows: 10, columns: 20, x: 16 }).forEach(mesh => world.add(mesh))
-
-// buildSimpleCastle().forEach(block => world.add(block, 2))
+buildSimpleCastle().forEach(block => world.add(block, 2))
 
 /* VEHICLE */
 
@@ -41,7 +38,7 @@ const wheelBack = { x: 1, y: .1, z: -1 }
 const { mesh: chassisMesh } = await loadModel({ file: 'tank/t-50/model.fbx' })
 addTexture({ mesh: chassisMesh, file: 'metal/metal01.jpg' })
 
-chassisMesh.position.set(0, 2, -20)
+chassisMesh.position.set(tankX, 2, -20)
 const tank = new Vehicle({ physicsWorld: world.physicsWorld, chassisMesh, wheelFront, wheelBack, maxEngineForce: 1000 })
 
 scene.add(chassisMesh)
