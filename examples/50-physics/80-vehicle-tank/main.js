a5c62dae8cb1d9e6aee09126c06edd521c2fb83c
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-09 11:11:02 +0100

    m

--- examples/50-physics/80-vehicle-tank/main.js
+++ examples/50-physics/80-vehicle-tank/main.js
@@ -28,21 +28,15 @@ createCrates({ z: 10 }).forEach(mesh => world.add(mesh))
 
 /* VEHICLE */
 
-const { mesh: chassisMesh } = await loadModel({ file: 'tank/a7v/model.fbx', angle: Math.PI })
-// const { mesh: chassisMesh } = await loadModel({ file: 'tank/mark-1/model.fbx' })
-// const { mesh: chassisMesh } = await loadModel({ file: 'tank/renault-ft/model.fbx' })
-// const { mesh: chassisMesh } = await loadModel({ file: 'tank/su-122/model.fbx', angle: Math.PI })
-// const { mesh: chassisMesh } = await loadModel({ file: 'tank/t-18-mc-1/model.fbx' })
-// const { mesh: chassisMesh } = await loadModel({ file: 'tank/t-50/model.fbx' })
-// const { mesh: chassisMesh } = await loadModel({ file: 'tank/tiger-1/model.fbx' })
-// const { mesh: chassisMesh } = await loadModel({ file: 'tank/warhammer/model.fbx', angle: Math.PI })
-// const { mesh: chassisMesh } = await loadModel({ file: 'tank/t-34.fbx' })
+// const wheelFront = { x: 1, y: .45, z: 1.7 }
+// const wheelBack = { x: 1, y: .45, z: -1 }
+// const { mesh: chassisMesh } = await loadModel({ file: 'tank/a7v/model.fbx', angle: Math.PI })
 
-chassisMesh.position.set(0, 4, -20)
+const wheelFront = { x: 1, y: .1, z: 1.7 }
+const wheelBack = { x: 1, y: .1, z: -1 }
+const { mesh: chassisMesh } = await loadModel({ file: 'tank/t-50/model.fbx' })
 
-const wheelFront = { x: 1, y: .4, z: 1.7 }
-const wheelBack = { x: 1, y: .4, z: -1 }
-
+chassisMesh.position.set(0, 2, -20)
 const tank = new Vehicle({ physicsWorld: world.physicsWorld, chassisMesh, wheelFront, wheelBack, maxEngineForce: 1000 })
 
 scene.add(chassisMesh) // , ...wheelMeshes
