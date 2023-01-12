c72c5b3ac249d20f48e55d544904046de547a36e
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-12 10:57:03 +0100

    LinearSpline

--- examples/20-particles/70-thrust/main.js
+++ examples/20-particles/70-thrust/main.js
@@ -1,40 +1,10 @@
 import * as THREE from 'three'
 import { scene, renderer, camera, clock, createOrbitControls } from '/utils/scene.js'
+import LinearSpline from '/utils/classes/LinearSpline.js'
 import { material } from '/utils/shaders/thrust.js'
 
 createOrbitControls()
 camera.position.z = 20
-
-class LinearSpline {
-  constructor(lerp) {
-    this.points = []
-    this.lerp = lerp
-  }
-
-  addPoint(t, d) {
-    this.points.push([t, d])
-  }
-
-  get(t) {
-    let p1 = 0
-
-    for (let i = 0; i < this.points.length; i++) {
-      if (this.points[i][0] >= t)
-        break
-      p1 = i
-    }
-
-    const p2 = Math.min(this.points.length - 1, p1 + 1)
-
-    if (p1 == p2) return this.points[p1][1]
-
-    return this.lerp(
-      (t - this.points[p1][0]) / (this.points[p2][0] - this.points[p1][0]),
-      this.points[p1][1],
-      this.points[p2][1]
-    )
-  }
-}
 
 let particles = []
 
