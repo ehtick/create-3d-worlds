0987a35995be56dd75042589f5a79142321a8c36
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-14 21:41:33 +0100

    locomotive

--- examples/80-scenes/locomotive/main.js
+++ examples/80-scenes/locomotive/main.js
@@ -2,7 +2,6 @@ import * as THREE from 'three'
 import { scene, renderer, camera, clock, createOrbitControls } from '/utils/scene.js'
 import { createSun } from '/utils/light.js'
 import { createGround } from '/utils/ground.js'
-import { createLocomotive } from '/utils/geometry/shapes.js'
 import Thrust from '/utils/classes/Thrust.js'
 import { followPath, createEllipse } from '/utils/path.js'
 import { loadModel } from '/utils/loaders.js'
@@ -14,19 +13,18 @@ scene.add(createSun())
 scene.add(createGround({ size: 50 }))
 
 const outerLine = createEllipse({ xRadius: 30, yRadius: 15 })
-const innerLine = createEllipse({ xRadius: 29.6, yRadius: 14.6 })
+const innerLine = createEllipse({ xRadius: 29.4, yRadius: 14.4 })
 scene.add(outerLine, innerLine)
 
-const { mesh: locomotive } = await loadModel({ file: 'vehicle/train/locomotive-lowpoly/parovoZ1.fbx', shouldCenter: true })
-// const locomotive = createLocomotive()
+const { mesh: locomotive, mixer } = await loadModel({ file: 'vehicle/train/toy-locomotive/scene.gltf', angle: Math.PI, axis: [0, 1, 0] })
 
 const thrust = new Thrust()
-thrust.mesh.rotateX(Math.PI).translateZ(-2).translateY(-2)
-locomotive.add(thrust.mesh)
 
 const group = new THREE.Group()
+locomotive.position.set(-.3, -.25, 0)
+thrust.mesh.rotateX(Math.PI).translateY(-1.5).translateZ(-1.5)
 group.add(locomotive)
-locomotive.translateX(-.2)
+group.add(thrust.mesh)
 scene.add(group)
 
 /* LOOP */
@@ -36,6 +34,7 @@ void function loop() {
   const delta = clock.getDelta()
   const elapsedTime = clock.getElapsedTime()
   followPath({ path: outerLine.userData.path, mesh: group, elapsedTime, speed: .025 })
-  thrust.update(delta, { velocity: [0, -14, 5] })
+  thrust.update(delta * .5, { velocity: [0, -14, 5] })
+  mixer.update(delta * 15)
   renderer.render(scene, camera)
 }()
