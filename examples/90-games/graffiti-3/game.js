13f6cd5597e6b146dd9422dc211cc6ca84ab5dd6
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-21 20:36:44 +0100

    m

--- examples/90-games/graffiti-3/game.js
+++ examples/90-games/graffiti-3/game.js
@@ -1,15 +1,11 @@
 // http://jsfiddle.net/0couqp27/9/
-import * as THREE from 'three'
-import { scene, renderer, camera, clock, createOrbitControls } from '/utils/scene.js'
 
-createOrbitControls()
-
-document.fonts.onloadingdone = function() {
-  const ctx = document.getElementById('c').getContext('2d')
+window.onload = function() {
+  const canvas = document.getElementById('c')
+  const ctx = canvas.getContext('2d')
 
   ctx.font = '48px Lobster'
   ctx.fillStyle = 'teal'
   ctx.textBaseline = 'top'
   ctx.fillText('This is the Lobster font', 0, 10)
 }
-
