e3e21d269c68ab44ccca8eb58cfe318b169c7ed2
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-21 18:28:42 +0100

    m

--- examples/90-games/graffiti-2/game.js
+++ examples/90-games/graffiti-2/game.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { scene, renderer, camera, clock, createOrbitControls } from '/utils/scene.js'
+import { scene, renderer, camera, createOrbitControls } from '/utils/scene.js'
 
 createOrbitControls()
 
@@ -9,61 +9,59 @@ const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.setScalar(1)
 scene.add(light, new THREE.AmbientLight(0xffffff, 0.25))
 
-const c = document.createElement('canvas')
-c.width = 256
-c.height = 128
-const ctx = c.getContext('2d')
+const canvas = document.createElement('canvas')
+canvas.width = 256
+canvas.height = 128
+const ctx = canvas.getContext('2d')
 ctx.fillStyle = 'rgba(255, 255, 255, 0)'
-ctx.fillRect(0, 0, c.width, c.height)
+ctx.fillRect(0, 0, canvas.width, canvas.height)
 ctx.textAlign = 'center'
 ctx.textBaseline = 'middle'
 ctx.fillStyle = 'magenta'
 ctx.font = 'bold 36px Arial'
 const text = 'I love Three.js'
-ctx.fillText(text, c.width * 0.5, c.height * 0.5)
+ctx.fillText(text, canvas.width * 0.5, canvas.height * 0.5)
 ctx.strokeStyle = 'red'
-ctx.strokeText(text, c.width * 0.5, c.height * 0.5)
-const tex = new THREE.CanvasTexture(c)
-tex.offset.y = 0.25
+ctx.strokeText(text, canvas.width * 0.5, canvas.height * 0.5)
+const texture = new THREE.CanvasTexture(canvas)
+texture.offset.y = 0.25
 
 const u = {
-  time: { value: 0 },
-  textTex: { value: tex }
+  y: { value: 0 },
+  graffiti: { value: texture }
 }
 
 const g = new THREE.CylinderGeometry(2, 2, 4.5, 36, 1, true)
 g.rotateY(Math.PI)
 
-const m = new THREE.MeshLambertMaterial({
+const material = new THREE.MeshLambertMaterial({
   color: 0x7f7f64,
-  map: new THREE.TextureLoader().load('https://threejs.org/examples/textures/floors/FloorsCheckerboard_S_Diffuse.jpg', tex => {
-    tex.wrapS = THREE.RepeatWrapping
-    tex.wrapT = THREE.RepeatWrapping
-    tex.repeat.set(3, 1)
+  map: new THREE.TextureLoader().load('https://threejs.org/examples/textures/floors/FloorsCheckerboard_S_Diffuse.jpg', texture => {
+    texture.wrapS = THREE.RepeatWrapping
+    texture.wrapT = THREE.RepeatWrapping
+    texture.repeat.set(3, 1)
   }),
   side: THREE.DoubleSide,
   onBeforeCompile: shader => {
-    shader.uniforms.time = u.time
-    shader.uniforms.textTex = u.textTex
+    shader.uniforms.graffiti = u.graffiti
     shader.fragmentShader = `
-        uniform float time;
-        uniform sampler2D textTex;
+        uniform float y;
+        uniform sampler2D graffiti;
       ${shader.fragmentShader}
     `.replace(
     '#include <map_fragment>',
     `#include <map_fragment>
-        vec4 textCol = texture(textTex, (vUv * 2. - 0.5) + vec2(-2., sin(time) * 0.25));
+        vec4 textCol = texture(graffiti, (vUv * 2. - 0.5) + vec2(-2., y));
         vec3 col = mix(diffuseColor.rgb, textCol.rgb, textCol.a);
         diffuseColor = vec4( col, opacity );
       `
-  )}
+  )
+  }
 })
-m.defines = { 'USE_UV': '' }
-const o = new THREE.Mesh(g, m)
-scene.add(o)
+
+const mesh = new THREE.Mesh(g, material)
+scene.add(mesh)
 
 renderer.setAnimationLoop(() => {
-  const time = clock.getElapsedTime()
-  u.time.value = time
   renderer.render(scene, camera)
 })
