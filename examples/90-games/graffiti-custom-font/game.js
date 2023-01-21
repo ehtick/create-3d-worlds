e9246c6510e1b30fe3e5697164250c953fb90064
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-21 21:28:54 +0100

    custom fonts

---
+++ examples/90-games/graffiti-custom-font/game.js
@@ -0,0 +1,19 @@
+import * as THREE from 'three'
+import { scene, renderer, camera, createOrbitControls } from '/utils/scene.js'
+import { createGraffitiTexture } from '/utils/city.js'
+
+createOrbitControls()
+
+await new Promise(resolve => window.addEventListener('load', resolve)) // window.onload
+
+const geometry	= new THREE.BoxGeometry(2, 2, 2)
+const material	= new THREE.MeshBasicMaterial({
+  map: createGraffitiTexture({ background: '#dddddd', font: '24px Lobster', color: 'teal' })
+})
+const mesh	= new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+void function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}()
