ab00c16e3ba02332a5993ef0c50ab7e723505118
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-21 20:04:19 +0100

    m

--- examples/90-games/graffiti-2/game.js
+++ examples/90-games/graffiti-2/game.js
@@ -7,9 +7,23 @@ createOrbitControls()
 scene.add(createSun())
 
 const uniforms = {
-  y: { value: 0 },
+  y: { value: 0.0 },
   graffiti: { value: createGraffitiTexture({ stroke: 'red' }) }
 }
+
+const getFragmentShader = defaultFragmentShader => /* glsl */`
+uniform float y;
+uniform sampler2D graffiti;
+
+${defaultFragmentShader}
+
+`.replace('#include <map_fragment>', `
+  #include <map_fragment>
+  
+  vec4 textCol = texture(graffiti, (vUv * 2. - 0.5) + vec2(-2., y));
+  vec3 col = mix(diffuseColor.rgb, textCol.rgb, textCol.a);
+  diffuseColor = vec4( col, opacity );
+`)
 
 const material = new THREE.MeshLambertMaterial({
   map: new THREE.TextureLoader().load('https://threejs.org/examples/textures/floors/FloorsCheckerboard_S_Diffuse.jpg', texture => {
@@ -17,19 +31,9 @@ const material = new THREE.MeshLambertMaterial({
     texture.repeat.set(3, 1)
   }),
   onBeforeCompile: shader => {
+    shader.uniforms.y = uniforms.y
     shader.uniforms.graffiti = uniforms.graffiti
-    shader.fragmentShader = `
-        uniform float y;
-        uniform sampler2D graffiti;
-      ${shader.fragmentShader}
-    `.replace(
-    '#include <map_fragment>',
-    `#include <map_fragment>
-        vec4 textCol = texture(graffiti, (vUv * 2. - 0.5) + vec2(-2., y));
-        vec3 col = mix(diffuseColor.rgb, textCol.rgb, textCol.a);
-        diffuseColor = vec4( col, opacity );
-      `
-  )
+    shader.fragmentShader = getFragmentShader(shader.fragmentShader)
   }
 })
 
