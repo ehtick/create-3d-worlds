4c251a48b8ad1931b41ac55943d25eedde1c7e84
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-21 20:53:52 +0100

    m

--- examples/90-games/graffiti-3/game.js
+++ examples/90-games/graffiti-3/game.js
@@ -1,11 +1,11 @@
 // http://jsfiddle.net/0couqp27/9/
 
-window.onload = function() {
-  const canvas = document.getElementById('c')
-  const ctx = canvas.getContext('2d')
+await new Promise(resolve => window.addEventListener('load', resolve)) // window.onload
 
-  ctx.font = '48px Lobster'
-  ctx.fillStyle = 'teal'
-  ctx.textBaseline = 'top'
-  ctx.fillText('This is the Lobster font', 0, 10)
-}
+const canvas = document.getElementById('c')
+const ctx = canvas.getContext('2d')
+
+ctx.font = '48px Lobster'
+ctx.fillStyle = 'teal'
+ctx.textBaseline = 'top'
+ctx.fillText('This is the Lobster font', 0, 10)
