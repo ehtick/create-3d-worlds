b8fc71abfdce0b9602c758d76d438c38f6534635
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-23 17:21:31 +0100

    yieldRandomCoord

--- examples/40-procedural/69-city-graffiti/main.js
+++ examples/40-procedural/69-city-graffiti/main.js
@@ -1,8 +1,11 @@
+import * as THREE from 'three'
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
 import { createFloor } from '/utils/ground.js'
 import { createGraffitiBuilding } from '/utils/city.js'
 import { createSun } from '/utils/light.js'
 import { shuffle } from '/utils/helpers.js'
+
+const { randFloatSpread } = THREE.MathUtils
 
 createOrbitControls()
 
@@ -16,17 +19,31 @@ scene.add(createSun({ position: [50, 100, 50] }))
 const floor = createFloor({ size: size * 1.1 })
 scene.add(floor)
 
-const fieldWidth = 20
-const coords = []
+function getRandomCoords(mapSize = 400, fieldSize = 20) {
+  const coords = []
+  for (let i = -mapSize * .5; i < mapSize * .5; i += fieldSize)
+    for (let j = -mapSize * .5; j < mapSize * .5; j += fieldSize)
+      coords.push([i, j])
 
-for (let i = -size * .5; i < size * .5; i += fieldWidth)
-  for (let j = -size * .5; j < size * .5; j += fieldWidth)
-    coords.push([i, j])
+  shuffle(coords)
+  return coords
+}
 
-shuffle(coords)
+function* yieldRandomCoord(mapSize = 400, fieldSize = 20, offset = fieldSize * .5) {
+  const coords = getRandomCoords(mapSize, fieldSize)
+
+  for (let i = 0; i < 100; i++) {
+    const [x, z] = coords[i]
+    const xOffset = randFloatSpread(offset), zOffset = randFloatSpread(offset)
+    yield [x + xOffset, z + zOffset]
+  }
+}
+
+const fieldSize = 20
+const coords = yieldRandomCoord(size, fieldSize)
 
 for (let i = 0; i < 100; i++) {
-  const [x, z] = coords[i]
+  const [x, z] = coords.next().value
   const building = await createGraffitiBuilding({ x, z })
   scene.add(building)
 }
