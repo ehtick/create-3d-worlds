002cd4ed74d1ba857dc13b5ab18999c50fd1e25f
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-24 17:22:17 +0100

    fix

--- examples/40-procedural/66-city-texture/main.js
+++ examples/40-procedural/66-city-texture/main.js
@@ -1,8 +1,9 @@
 // http://learningthreejs.com/blog/2013/08/02/how-to-do-a-procedural-city-in-100lines/
 import * as THREE from 'three'
-import { scene, camera, renderer, createOrbitControls, hemLight } from '/utils/scene.js'
+import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
 import { createFloor } from '/utils/ground.js'
 import { createCity, createBuildingTexture } from '/utils/city.js'
+import { hemLight } from '/utils/light.js'
 
 const mapSize = 2000
 const numBuildings = 10000
@@ -16,7 +17,7 @@ scene.fog = new THREE.FogExp2(0xd0e0f0, 0.0025)
 renderer.setClearColor(0x7ec0ee)
 
 scene.add(createFloor({ size: mapSize }))
-scene.add(createCity({ numBuildings, mapSize, rotateEvery: 2, enlargeEvery: 10, map: createBuildingTexture(), colorParams: { colorful: .035, max: 1 } }))
+scene.add(createCity({ mapSize, numBuildings, rotateEvery: 2, enlargeEvery: 10, map: createBuildingTexture(), colorParams: { colorful: .035, max: 1 } }))
 
 /* INIT */
 
