419d1ac214a93fe5366df2025ac0f799c60efd5d
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-24 12:24:02 +0100

    m

--- examples/40-procedural/65-city-empty-center/game.js
+++ examples/40-procedural/65-city-empty-center/game.js
@@ -1,22 +1,27 @@
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
-import { createFloor } from '/utils/ground.js'
+import { createFloor, createGround } from '/utils/ground.js'
 import { createNightCity } from '/utils/city.js'
 import { createTrees } from '/utils/geometry/trees.js'
 
 const mapSize = 400
+const center = 50
 const numBuildings = 300
 
 camera.position.set(0, mapSize * .3, mapSize * .4)
 createOrbitControls()
 renderer.setClearColor(0x070b34)
 
-const floor = createFloor({ size: mapSize * 1.1, color: 0x101018 })
+const floor = createFloor({ size: mapSize * 1.1 })
+scene.add(floor)
 
-const city = createNightCity({ mapSize, numBuildings, emptyCenter: 50 })
+const city = createNightCity({ mapSize, numBuildings, emptyCenter: center })
+scene.add(city)
 
-scene.add(floor, city)
+const ground = createGround({ size: center * 1.5, circle: false })
+ground.translateY(.1)
+scene.add(ground)
 
-scene.add(createTrees({ mapSize: 60, n: 30 }))
+scene.add(createTrees({ mapSize: center * 1.25, n: 30 }))
 
 /* LOOP */
 
