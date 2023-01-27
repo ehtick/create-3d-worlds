1bdc1af5dec6e7aa54719effb532d6941e0d609c
Damjan Pavlica <damjan.pavlica@origo.is>
2023-01-27 10:46:55 +0100

    del

--- utils/fsm/Savo.js
+++ utils/fsm/Savo.js
@@ -2,6 +2,8 @@ import Player from '/utils/fsm/Player.js'
 import { createBox } from '/utils/geometry.js'
 import { camera } from '/utils/scene.js'
 
+const PI_HALF = Math.PI * .5
+
 export default class Savo extends Player {
   constructor({ speed, size = 2, ...params } = {}) {
     const mesh = createBox({ size })
@@ -11,6 +13,19 @@ export default class Savo extends Player {
     this.speed = speed || this.size * 3
     this.maxVelocityY = .2
     this.minVelocityY = -this.maxVelocityY
+
+    this.mouseSensitivity = .002
+    camera.rotation.set(0, 0, 0)
+    mesh.add(camera)
+
+    document.addEventListener('mousemove', e => this.onMouseMove(e))
+  }
+
+  onMouseMove(e) {
+    this.mesh.rotation.y -= e.movementX * this.mouseSensitivity
+
+    camera.rotation.x -= e.movementY * this.mouseSensitivity
+    camera.rotation.x = Math.max(-PI_HALF, Math.min(PI_HALF, camera.rotation.x))
   }
 
   update(delta) {
