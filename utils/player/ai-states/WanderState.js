d20a8907cb9b78c8f6ed069698e3165bb143206c
Damjan Pavlica <damjan.pavlica@origo.is>
2023-02-08 11:17:23 +0100

    TWEEN

--- utils/player/ai-states/WanderState.js
+++ utils/player/ai-states/WanderState.js
@@ -1,21 +1,25 @@
 import * as THREE from 'three'
+import { TWEEN } from '/node_modules/three/examples/jsm/libs/tween.module.min.js'
 import WalkState from '../states/WalkState.js'
 
 const { randFloat } = THREE.MathUtils
 
-export default class WanderState extends WalkState {
-  // enter(oldState, oldAction) {
-  //   this.turn()
-  // }
+const rotate = mesh => new TWEEN.Tween(mesh.rotation)
+  .to({ y: randFloat(-1, 1) })
+  .start()
 
-  turn() {
+export default class WanderState extends WalkState {
+  enter(oldState, oldAction) {
+    super.enter(oldState, oldAction)
     this.player.mesh.rotateY(randFloat(-1, 1))
   }
 
   update(delta) {
     if (Math.random() > 0.995)
-      this.turn()
+      rotate(this.player.mesh)
+
     this.keyboard.pressed.ArrowUp = true
+    TWEEN.update(delta)
     super.update(delta)
   }
 }
