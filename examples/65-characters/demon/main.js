70cef76f17984d594d62809b5a7687800facce88
Damjan Pavlica <damjan.pavlica@origo.is>
2023-02-12 18:21:51 +0100

    loadDemon

--- examples/65-characters/demon/main.js
+++ examples/65-characters/demon/main.js
@@ -2,16 +2,14 @@ import Player from '/utils/player/Player.js'
 import { scene, renderer, camera, createOrbitControls, clock } from '/utils/scene.js'
 import { createSun } from '/utils/light.js'
 import { createFloor } from '/utils/ground.js'
-import { loadModel, loadFbxAnimations } from '/utils/loaders.js'
-import { demonAnimations } from '/data/animations.js'
+import { loadDemon } from '/utils/loaders.js'
 
 scene.add(createSun())
 
 scene.add(createFloor({ size: 100 }))
 
-const { mesh } = await loadModel({ file: 'character/demon/demon.fbx', angle: Math.PI, fixColors: true })
-const animations = await loadFbxAnimations(demonAnimations, 'character/demon/')
-const player = new Player({ mesh, animations, animDict: demonAnimations })
+const { mesh, animations, animDict } = await loadDemon()
+const player = new Player({ mesh, animations, animDict })
 
 scene.add(mesh)
 
