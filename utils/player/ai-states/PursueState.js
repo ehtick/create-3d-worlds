9c58ffb0baf16437760f70be1c1fd1b6df33277c
Damjan Pavlica <damjan.pavlica@origo.is>
2023-02-19 20:35:50 +0100

    m

--- utils/player/ai-states/PursueState.js
+++ utils/player/ai-states/PursueState.js
@@ -1,10 +1,6 @@
 import WalkState from '../states/WalkState.js'
 
 export default class PursueState extends WalkState {
-  enter(oldState, oldAction) {
-    super.enter(oldState, oldAction)
-  }
-
   update(delta) {
     const { player } = this
 
