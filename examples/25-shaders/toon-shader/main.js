9f15990c98e416c39e5798789f23232f7f9b0ce0
Damjan Pavlica <damjan.pavlica@origo.is>
2023-02-22 16:50:33 +0100

    reuse sun

--- examples/25-shaders/toon-shader/main.js
+++ examples/25-shaders/toon-shader/main.js
@@ -2,18 +2,14 @@ import * as THREE from 'three'
 import { TeapotGeometry } from '/node_modules/three/examples/jsm/geometries/TeapotGeometry.js'
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
 import { material } from '/utils/shaders/cartoon.js'
+import { createSun } from '/utils/light.js'
 
 createOrbitControls()
 
 let time = 0
 
-const light = new THREE.PointLight(0xffffff)
-const lightContainer = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 16, 16),
-  new THREE.MeshBasicMaterial({ color: 0xffff00 })
-)
-scene.add(lightContainer)
-lightContainer.add(light)
+const sun = createSun()
+scene.add(sun)
 
 const teapot = new THREE.Mesh(new TeapotGeometry(2), material)
 scene.add(teapot)
@@ -24,8 +20,8 @@ teapot.material.uniforms.uMaterialColor.value = new THREE.Color('crimson')
 
 void function animate() {
   requestAnimationFrame(animate)
-  lightContainer.position.set(40 * Math.cos(time), 75, 40 * Math.sin(time))
-  teapot.material.uniforms.uLightPos.value = lightContainer.position
+  sun.position.set(40 * Math.cos(time), 75, 40 * Math.sin(time))
+  teapot.material.uniforms.uLightPos.value = sun.position
   time += 0.01
 
   renderer.render(scene, camera)
