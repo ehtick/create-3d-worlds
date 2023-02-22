cad02585258f4c956d71e9d36c5488b68ed2d2c1
Damjan Pavlica <damjan.pavlica@origo.is>
2023-02-22 17:27:50 +0100

    m

--- examples/65-characters/ww2/soviet-partisan/main.js
+++ examples/65-characters/ww2/soviet-partisan/main.js
@@ -13,7 +13,7 @@ const { mesh, animations, animDict } = await loadSovietPartisan()
 const player = new Player({ mesh, animations, animDict })
 scene.add(mesh)
 
-const { mesh: weapon } = await loadModel({ file: 'weapon/mg-42/untitled.fbx', scale: .4 })
+const { mesh: weapon } = await loadModel({ file: 'weapon/mg-42/model.fbx', scale: .4 })
 player.addWeapon(weapon)
 
 const controls = createOrbitControls()
