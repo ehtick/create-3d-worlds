ca2278fdac39d653440018ac33a74ed8ff50104a
Damjan Pavlica <damjan.pavlica@origo.is>
2023-02-24 15:48:25 +0100

    Nazi Nazi Nazi

---
+++ utils/player/ai-characters/Nazi.js
@@ -0,0 +1,26 @@
+import Player from '/utils/player/Player.js'
+import AI from '/utils/player/AI.js'
+import { loadModel } from '/utils/loaders.js'
+import { naziAnimations } from '/data/animations.js'
+
+/* LOADING */
+
+const { mesh, animations, animDict } = await loadModel({ file: 'nazi.fbx', animDict: naziAnimations, prefix: 'character/nazi/', angle: Math.PI, fixColors: true, size: 1.8 })
+
+const { mesh: rifle } = await loadModel({ file: 'weapon/rifle.fbx', scale: 1.33, angle: Math.PI })
+
+/* EXTENDED CLASSES */
+
+const sharedProps = { mesh, animations, animDict, rifle, attackStyle: 'LOOP' }
+
+export class NaziPlayer extends Player {
+  constructor(props = {}) {
+    super({ ...sharedProps, ...props })
+  }
+}
+
+export class NaziAI extends AI {
+  constructor(props = {}) {
+    super({ ...sharedProps, basicState: 'wander', attackDistance: 10, ...props })
+  }
+}
