626e95915f6ebdb8404d2d9de64a08759a7a953c
Damjan Pavlica <damjan.pavlica@origo.is>
2023-02-24 23:58:33 +0100

    animDict

--- utils/characters/ww2/Partisan.js
+++ utils/characters/ww2/Partisan.js
@@ -1,17 +1,26 @@
 import Player from '/utils/player/Player.js'
 import AI from '/utils/player/AI.js'
 import { loadModel } from '/utils/loaders.js'
-import { partisanAnimations } from '/data/animations.js'
+
+export const animDict = {
+  idle: 'Rifle Idle',
+  walk: 'Walk With Rifle',
+  jump: 'Jump Forward',
+  attack: 'Firing Rifle',
+  special: 'Toss Grenade',
+  pain: 'Reaction',
+  death: 'Dying',
+}
 
 /* LOADING */
 
-const { mesh, animations } = await loadModel({ file: 'partisan.fbx', angle: Math.PI, animDict: partisanAnimations, prefix: 'character/soldier/', fixColors: true, size: 1.8 })
+const { mesh, animations } = await loadModel({ file: 'partisan.fbx', angle: Math.PI, animDict, prefix: 'character/soldier/', fixColors: true, size: 1.8 })
 
 const { mesh: rifle } = await loadModel({ file: 'weapon/rifle-berthier/model.fbx', scale: .60, angle: Math.PI })
 
 /* EXTENDED CLASSES */
 
-const sharedProps = { mesh, animations, animDict: partisanAnimations, rifle }
+const sharedProps = { mesh, animations, animDict, rifle }
 
 export class PartisanPlayer extends Player {
   constructor(props = {}) {
--- utils/characters/ww2/PartisanLowpoly.js
+++ utils/characters/ww2/PartisanLowpoly.js
@@ -1,17 +1,17 @@
 import Player from '/utils/player/Player.js'
 import AI from '/utils/player/AI.js'
 import { loadModel } from '/utils/loaders.js'
-import { partisanAnimations } from '/data/animations.js'
+import { animDict } from './Partisan.js'
 
 /* LOADING */
 
-const { mesh, animations } = await loadModel({ file: 'partisan-lowpoly.fbx', angle: Math.PI, animDict: partisanAnimations, prefix: 'character/soldier/', fixColors: true, size: 1.8 })
+const { mesh, animations } = await loadModel({ file: 'partisan-lowpoly.fbx', angle: Math.PI, animDict, prefix: 'character/soldier/', fixColors: true, size: 1.8 })
 
 const { mesh: rifle } = await loadModel({ file: 'weapon/rifle.fbx', scale: 1.25, angle: Math.PI })
 
 /* EXTENDED CLASSES */
 
-const sharedProps = { mesh, animations, animDict: partisanAnimations, rifle }
+const sharedProps = { mesh, animations, animDict, rifle }
 
 export class PartisanLowpolyPlayer extends Player {
   constructor(props = {}) {
