dd11f637e43b1a1ec237b49c2d4643b2a3d0467a
Damjan Pavlica <damjan.pavlica@origo.is>
2023-02-28 09:54:37 +0100

    remove start pursue

--- utils/player/ai-states/PursueState.js
+++ utils/player/ai-states/PursueState.js
@@ -6,21 +6,15 @@ const { randInt } = MathUtils
 export default class PursueState extends RunState {
 
   enter(oldState, oldAction) {
-    // this.actions.pursue = this.actions.run
     super.enter(oldState, oldAction)
     this.input.run = this.input.up = true
-
     this.player.randomizeAction()
-    this.startPursue = randInt(300, 600)
-    this.i = 0
   }
 
   update(delta) {
     const { player } = this
 
     player.lookAtTarget()
-
-    if (Date.now() - this.last < this.startPursue) return
 
     player.updateMove(delta, false)
 
