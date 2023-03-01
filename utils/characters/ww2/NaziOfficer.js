652830d0583f04f5e7a29eaef84a46b74762426f
Damjan Pavlica <damjan.pavlica@origo.is>
2023-03-01 11:00:32 +0100

    reuse all nazi

--- utils/characters/ww2/NaziOfficer.js
+++ utils/characters/ww2/NaziOfficer.js
@@ -14,7 +14,7 @@ export const animDict = {
 
 /* LOADING */
 
-const { mesh, animations } = await loadModel({ file: 'nazi-officer.fbx', prefix: 'character/nazi/', animDict, angle: Math.PI, fixColors: true, size: 2 })
+const { mesh, animations } = await loadModel({ file: 'nazi-officer.fbx', prefix: 'character/soldier/', animDict, angle: Math.PI, fixColors: true, size: 2 })
 
 const { mesh: pistol } = await loadModel({ file: 'weapon/luger/model.fbx', scale: .18 })
 
@@ -25,12 +25,14 @@ const sharedProps = { mesh, animations, animDict, pistol, speed: 1.9 }
 export class NaziOfficerPlayer extends Player {
   constructor(props = {}) {
     super({ ...sharedProps, ...props })
+    this.mesh.translateY(.1)
   }
 }
 
 export class NaziOfficerAI extends AI {
   constructor(props = {}) {
     super({ ...sharedProps, attackDistance: 7, ...props })
+    this.mesh.translateY(.1)
   }
 
   lookAtTarget() {
