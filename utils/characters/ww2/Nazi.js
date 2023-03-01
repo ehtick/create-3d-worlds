661b8914c642993b6b99d44883f9090faea88591
Damjan Pavlica <damjan.pavlica@origo.is>
2023-03-01 11:14:57 +0100

    del nazi Crouch

--- utils/characters/ww2/Nazi.js
+++ utils/characters/ww2/Nazi.js
@@ -5,7 +5,7 @@ import { loadModel } from '/utils/loaders.js'
 const animDict = {
   idle: 'Rifle Aiming Idle',
   walk: 'Walk With Rifle',
-  run: 'Rifle Run',
+  run: 'Crouched Run',
   attack: 'Firing Rifle',
   attack2: 'Fire Rifle Crouch',
   pain: 'Hit Reaction',
