449a22f4d27e081cd5e85d9e69595542f6453ea1
Damjan Pavlica <damjan.pavlica@origo.is>
2023-03-08 15:36:53 +0100

    m

--- examples/20-particles/flame-thrower/game.js
+++ examples/20-particles/flame-thrower/game.js
@@ -1,11 +1,11 @@
 import * as THREE from 'three'
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
-import { Stars } from '/utils/classes/Particles.js'
+import Particles from '/utils/classes/Particles.js'
 
 createOrbitControls()
 scene.background = new THREE.Color(0x000000)
 
-const particles = new Stars({ file: 'fire.png', size: 10, num: 100, minRange: .5, maxRange: 1, color: 0xffffff })
+const particles = new Particles({ file: 'fire.png', size: 10, num: 100, minRange: .5, maxRange: 1, color: 0xffffff })
 particles.mesh.rotateX(Math.PI)
 scene.add(particles.mesh)
 
