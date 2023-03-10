e5e64a01c48d343ae611808f83dfbc49154e9edb
Damjan Pavlica <damjan.pavlica@origo.is>
2023-03-10 15:40:51 +0100

    m

--- examples/20-particles/rain/game.js
+++ examples/20-particles/rain/game.js
@@ -5,7 +5,7 @@ import { Rain } from '/utils/classes/Particles.js'
 hemLight()
 renderer.setClearColor(0x000000)
 
-const particles = new Rain({ num: 1000, opacity: .7, color: 0x9999ff, size: 1, minRadius: 5, maxRadius: 100 })
+const particles = new Rain({ color: 0x9999ff, size: 1, minRadius: 5, maxRadius: 100 })
 scene.add(particles.mesh)
 
 /* LOOP */
