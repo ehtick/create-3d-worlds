8970235f82e5da111a78be4e8f2989e1a547e0d6
Damjan Pavlica <damjan.pavlica@origo.is>
2023-03-18 00:22:45 +0100

    tlo

--- examples/35-mazes/pyramid/main.js
+++ examples/35-mazes/pyramid/main.js
@@ -1,9 +1,18 @@
+import * as THREE from 'three'
+
 import { scene, renderer, camera, createOrbitControls } from '/utils/scene.js'
 import { pyramidFromMatrix, putInMaze } from '/utils/mazes.js'
 import { huntAndKillMatrix } from '/utils/mazes/algorithms.js'
 import Avatar from '/utils/player/Avatar.js'
 import { hemLight } from '/utils/light.js'
 import { createGround } from '/utils/ground.js'
+import { material } from '/utils/shaders/marble.js'
+
+const geometry = new THREE.PlaneGeometry(75, 75)
+const plane = new THREE.Mesh(geometry, material)
+plane.translateY(.01)
+plane.rotateX(-Math.PI * 0.5)
+scene.add(plane)
 
 const size = 3
 
